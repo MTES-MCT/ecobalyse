d9f6d749f3d7394b3df3d29373e900b5edcaa9ae
Nicolas Perriault <nicolas@perriault.net>
2021-09-01 12:14:32 +0200

    Integrate first waste data.

--- src/Data/Product.elm
+++ src/Data/Product.elm
@@ -8,25 +8,26 @@ type alias Product =
     { id : String
     , name : String
     , defaultMass : Float
+    , waste : Float
     }
 
 
 choices : List Product
 choices =
-    [ { id = "1", name = "Cape", defaultMass = 0.95 }
-    , { id = "2", name = "Châle", defaultMass = 0.11 }
-    , { id = "3", name = "Chemisier", defaultMass = 0.25 }
-    , { id = "4", name = "Débardeur", defaultMass = 0.17 }
-    , { id = "5", name = "Echarpe", defaultMass = 0.11 }
-    , { id = "6", name = "Gilet", defaultMass = 0.5 }
-    , { id = "7", name = "Jean", defaultMass = 0.45 }
-    , { id = "8", name = "Jupe", defaultMass = 0.3 }
-    , { id = "9", name = "Manteau", defaultMass = 0.95 }
-    , { id = "10", name = "Pantalon", defaultMass = 0.45 }
-    , { id = "11", name = "Pull", defaultMass = 0.5 }
-    , { id = "12", name = "Robe", defaultMass = 0.3 }
-    , { id = "13", name = "T-shirt", defaultMass = 0.17 }
-    , { id = "14", name = "Veste", defaultMass = 0.95 }
+    [ { id = "1", name = "Cape", defaultMass = 0.95, waste = 0.2 }
+    , { id = "2", name = "Châle", defaultMass = 0.11, waste = 0.1 }
+    , { id = "3", name = "Chemisier", defaultMass = 0.25, waste = 0.2 }
+    , { id = "4", name = "Débardeur", defaultMass = 0.17, waste = 0.15 }
+    , { id = "5", name = "Echarpe", defaultMass = 0.11, waste = 0.1 }
+    , { id = "6", name = "Gilet", defaultMass = 0.5, waste = 0.2 }
+    , { id = "7", name = "Jean", defaultMass = 0.45, waste = 0.22 }
+    , { id = "8", name = "Jupe", defaultMass = 0.3, waste = 0.2 }
+    , { id = "9", name = "Manteau", defaultMass = 0.95, waste = 0.2 }
+    , { id = "10", name = "Pantalon", defaultMass = 0.45, waste = 0.2 }
+    , { id = "11", name = "Pull", defaultMass = 0.5, waste = 0.2 }
+    , { id = "12", name = "Robe", defaultMass = 0.3, waste = 0.2 }
+    , { id = "13", name = "T-shirt", defaultMass = 0.17, waste = 0.15 }
+    , { id = "14", name = "Veste", defaultMass = 0.95, waste = 0.2 }
     ]
 
 
@@ -37,15 +38,16 @@ findById id =
 
 tShirt : Product
 tShirt =
-    { id = "13", name = "T-shirt", defaultMass = 0.17 }
+    { id = "13", name = "T-shirt", defaultMass = 0.17, waste = 0.15 }
 
 
 decode : Decoder Product
 decode =
-    Decode.map3 Product
+    Decode.map4 Product
         (Decode.field "id" Decode.string)
         (Decode.field "name" Decode.string)
         (Decode.field "defaultMass" Decode.float)
+        (Decode.field "waste" Decode.float)
 
 
 encode : Product -> Encode.Value
@@ -54,4 +56,5 @@ encode v =
         [ ( "id", Encode.string v.id )
         , ( "name", Encode.string v.name )
         , ( "defaultMass", Encode.float v.defaultMass )
+        , ( "waste", Encode.float v.waste )
         ]
