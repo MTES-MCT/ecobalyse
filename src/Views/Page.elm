e1cc43559ae6f705c852d4faf89d83b06bf882bf
Nicolas Perriault <nicolas@perriault.net>
2021-09-10 16:11:05 +0200

    Update sponsorship logos & footer layout.

--- src/Views/Page.elm
+++ src/Views/Page.elm
@@ -30,7 +30,7 @@ frame config ( title, content ) =
             [ div [ class "container" ]
                 [ text "Ce site n'est pas encore opérationnel… Work in progress." ]
             ]
-        , div [ class "bg-light", style "min-height" "53vh" ]
+        , div [ class "bg-light", style "min-height" "52vh" ]
             [ main_ [ class "container py-5" ] content
             ]
         , pageFooter config
@@ -71,38 +71,61 @@ navbar { activePage } =
 pageFooter : Config -> Html msg
 pageFooter { activePage } =
     footer
-        [ class "bg-dark text-light py-5" ]
+        [ class "bg-dark text-light py-5 fs-7" ]
         [ div [ class "container" ]
-            [ div [ class "row" ]
-                [ div [ class "col-sm-6" ]
+            [ div [ class "row d-flex align-items-center" ]
+                [ div [ class "col" ]
                     [ h3 [] [ text "wikicarbone" ]
                     , menuLinks activePage
                         |> List.map (\l -> li [] [ l ])
                         |> ul []
                     , p [ class "mb-0" ]
                         [ text "Un produit "
-                        , a [ href "https://beta.gouv.fr/", class "text-light" ] [ text "beta.gouv.fr" ]
+                        , a [ href "https://beta.gouv.fr/", class "text-light", rel "noopener noreferrer", target "_blank" ]
+                            [ text "beta.gouv.fr" ]
                         ]
                     ]
-                , div [ class "col-sm-6 text-center text-sm-end" ]
-                    [ a [ href "https://www.ecologique-solidaire.gouv.fr/fabrique-numerique", rel "noopener noreferrer", target "_blank" ]
-                        [ img
-                            [ src "img/logo-fabriquenumerique.svg"
-                            , alt "La Fabrique Numérique"
-                            , attribute "height" "200"
-                            , attribute "width" "200"
-                            ]
-                            []
+                , a
+                    [ href "https://www.ecologique-solidaire.gouv.fr/"
+                    , rel "noopener noreferrer"
+                    , target "_blank"
+                    , class "col text-center bg-light px-3 m-3"
+                    ]
+                    [ img
+                        [ src "img/logo_mte.svg"
+                        , alt "Ministère de la transition écologique et solidaire"
+                        , attribute "width" "200"
+                        , attribute "height" "200"
                         ]
-                    , a [ href "https://www.ecologique-solidaire.gouv.fr/", rel "noopener noreferrer", target "_blank" ]
-                        [ img
-                            [ src "img/MTES_MCTRCT.svg"
-                            , alt "Ministère de la transition écologique et solidaire, Ministère de la Cohésion des territoires et des Relations avec les collectivités territoriales"
-                            , attribute "width" "90"
-                            , attribute "height" "200"
-                            ]
-                            []
+                        []
+                    ]
+                , a
+                    [ href "https://www.cohesion-territoires.gouv.fr/"
+                    , rel "noopener noreferrer"
+                    , target "_blank"
+                    , class "col text-center bg-light px-3 m-3"
+                    ]
+                    [ img
+                        [ src "img/logo_mct.svg"
+                        , alt "Ministère de la Cohésion des territoires et des Relations avec les collectivités territoriales"
+                        , attribute "width" "200"
+                        , attribute "height" "200"
                         ]
+                        []
+                    ]
+                , a
+                    [ href "https://www.ecologique-solidaire.gouv.fr/fabrique-numerique"
+                    , rel "noopener noreferrer"
+                    , target "_blank"
+                    , class "col text-center px-3 py-2"
+                    ]
+                    [ img
+                        [ src "img/logo-fabriquenumerique.svg"
+                        , alt "La Fabrique Numérique"
+                        , attribute "width" "200"
+                        , attribute "height" "200"
+                        ]
+                        []
                     ]
                 ]
             ]
