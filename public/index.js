a2f36f3a1dce300ef82ca1c5ff659322dd40736f
Nicolas Perriault <nicolas@perriault.net>
2021-09-23 10:28:59 +0200

    WiP parcel bundler.

---
+++ public/index.js
@@ -0,0 +1,39 @@
+import { Elm } from "../src/Main.elm";
+
+// The localStorage key to use to store serialized session data
+const storeKey = "store";
+
+const app = Elm.Main.init({
+  flags: {
+    clientUrl: location.origin + location.pathname,
+    rawStore: localStorage[storeKey] || "",
+  },
+});
+
+app.ports.saveStore.subscribe((rawStore) => {
+  localStorage[storeKey] = rawStore;
+});
+
+app.ports.scrollTo.subscribe((pos) => {
+  window.scrollTo(pos.x, pos.y);
+});
+
+app.ports.copyToClipboard.subscribe((text) => {
+  navigator.clipboard.writeText(text).then(
+    function () {},
+    function (err) {
+      alert("Votre navigateur ne supporte pas la copie automatique; vous pouvez copier l'adresse manuellement");
+    }
+  );
+});
+
+// Ensure session is refreshed when it changes in another tab/window
+window.addEventListener(
+  "storage",
+  (event) => {
+    if (event.storageArea === localStorage && event.key === storeKey) {
+      app.ports.storeChanged.send(event.newValue);
+    }
+  },
+  false
+);
