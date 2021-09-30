7b17a3827d769ded7e6cd1b599889758d82dcb65
Nicolas Perriault <nicolas@perriault.net>
2021-09-30 22:21:46 +0200

    Add tests.

--- tests/Data/SimulatorTest.elm
+++ tests/Data/SimulatorTest.elm
@@ -1,6 +1,6 @@
 module Data.SimulatorTest exposing (..)
 
-import Data.Inputs as Inputs exposing (Inputs)
+import Data.Inputs exposing (..)
 import Data.Simulator as Simulator
 import Expect exposing (Expectation)
 import Route exposing (Route(..))
@@ -21,26 +21,51 @@ suite : Test
 suite =
     describe "Data.Simulator"
         [ describe "compute"
-            [ Inputs.tShirtCotonFrance
-                |> expectCo2 5.26
-                |> asTest "should compute co2 score for tShirtCotonFrance"
-            , Inputs.tShirtCotonEurope
-                |> expectCo2 8.33
-                |> asTest "should compute co2 score for tShirtCotonEurope"
-            , Inputs.tShirtCotonAsie
-                |> expectCo2 9.73
-                |> asTest "should compute co2 score for tShirtCotonAsie"
-            , Inputs.jupeCircuitAsie
-                |> expectCo2 33.55
-                |> asTest "should compute co2 score for jupeCircuitAsie"
-            , Inputs.manteauCircuitEurope
-                |> expectCo2 2071.72
-                |> asTest "should compute co2 score for manteauCircuitEurope"
-            , Inputs.pantalonCircuitEurope
-                |> expectCo2 25.49
-                |> asTest "should compute co2 score for pantalonCircuitEurope"
-            , Inputs.robeCircuitBangladesh
-                |> expectCo2 39.72
-                |> asTest "should compute co2 score for robeCircuitBangladesh"
+            [ describe "default dyeing weighting"
+                [ tShirtCotonFrance
+                    |> expectCo2 5.26
+                    |> asTest "should compute co2 score for tShirtCotonFrance"
+                , tShirtCotonEurope
+                    |> expectCo2 8.33
+                    |> asTest "should compute co2 score for tShirtCotonEurope"
+                , tShirtCotonAsie
+                    |> expectCo2 9.73
+                    |> asTest "should compute co2 score for tShirtCotonAsie"
+                , jupeCircuitAsie
+                    |> expectCo2 33.55
+                    |> asTest "should compute co2 score for jupeCircuitAsie"
+                , manteauCircuitEurope
+                    |> expectCo2 2071.72
+                    |> asTest "should compute co2 score for manteauCircuitEurope"
+                , pantalonCircuitEurope
+                    |> expectCo2 25.49
+                    |> asTest "should compute co2 score for pantalonCircuitEurope"
+                , robeCircuitBangladesh
+                    |> expectCo2 39.72
+                    |> asTest "should compute co2 score for robeCircuitBangladesh"
+                ]
+            , describe "custom dyeing weighting"
+                [ { tShirtCotonFrance | dyeingWeighting = Just 0.5 }
+                    |> expectCo2 5.72
+                    |> asTest "should compute co2 score for tShirtCotonFrance using custom dyeing weighting"
+                , { tShirtCotonEurope | dyeingWeighting = Just 0.5 }
+                    |> expectCo2 7.14
+                    |> asTest "should compute co2 score for tShirtCotonEurope using custom dyeing weighting"
+                , { tShirtCotonAsie | dyeingWeighting = Just 0.5 }
+                    |> expectCo2 8.33
+                    |> asTest "should compute co2 score for tShirtCotonAsie using custom dyeing weighting"
+                , { jupeCircuitAsie | dyeingWeighting = Just 0.5 }
+                    |> expectCo2 30.93
+                    |> asTest "should compute co2 score for jupeCircuitAsie using custom dyeing weighting"
+                , { manteauCircuitEurope | dyeingWeighting = Just 0.5 }
+                    |> expectCo2 2064.69
+                    |> asTest "should compute co2 score for manteauCircuitEurope using custom dyeing weighting"
+                , { pantalonCircuitEurope | dyeingWeighting = Just 0.5 }
+                    |> expectCo2 22.4
+                    |> asTest "should compute co2 score for pantalonCircuitEurope using custom dyeing weighting"
+                , { robeCircuitBangladesh | dyeingWeighting = Just 0.5 }
+                    |> expectCo2 42.32
+                    |> asTest "should compute co2 score for robeCircuitBangladesh using custom dyeing weighting"
+                ]
             ]
         ]
