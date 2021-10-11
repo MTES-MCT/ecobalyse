1a9ac3090087c403b40281ccabdebc01f7f02884
Nicolas Perriault <nicolas@perriault.net>
2021-10-11 17:03:41 +0200

    Migrate consumming gitbook from github directly.

--- src/Request/Gitbook.elm
+++ src/Request/Gitbook.elm
@@ -4,22 +4,53 @@ import Data.Gitbook as Gitbook
 import Data.Session exposing (Session)
 import Http
 import RemoteData exposing (WebData)
-import RemoteData.Http exposing (defaultConfig)
 
 
-config : RemoteData.Http.Config
-config =
-    { defaultConfig
-        | headers =
-            [ Http.header "Authorization"
-                "Bearer UTZvYmUzbXRLWVA1a3hGMFdwcXpJbW1iSWkwMjotTWxDSm9nelJQQTF6VkFFQTFVQi0tTWxDSm9oLTVkd09ocUM3bFNIRw"
-            ]
+toPage : String -> String -> Gitbook.Page
+toPage path markdown =
+    let
+        blocks =
+            markdown |> String.split "\n\n"
+
+        title =
+            blocks
+                |> List.filter (String.startsWith "# ")
+                |> List.head
+                |> Debug.log "title"
+
+        finalTitle =
+            title
+                |> Maybe.map (String.replace "# " "")
+                |> Maybe.withDefault "Sans titre"
+
+        description =
+            blocks
+                |> List.filter (\block -> String.startsWith "---" block && String.endsWith "---" block)
+                |> List.head
+                |> Maybe.map (String.replace "---" "" >> String.replace "\n" "" >> String.replace "description:" "" >> String.replace ">-" "")
+
+        final =
+            blocks
+                |> List.map String.trim
+                |> List.filter (\block -> not (String.startsWith "---\n" block && String.endsWith "---" block))
+                |> List.filter (\block -> title /= Just block)
+                |> String.join "\n\n"
+    in
+    { title = finalTitle
+    , description = description
+    , markdown = final
+    , path = path
     }
 
 
 getPage : Session -> String -> (WebData Gitbook.Page -> msg) -> Cmd msg
-getPage _ page event =
-    RemoteData.Http.getWithConfig config
-        ("https://api-beta.gitbook.com/v1/spaces/-MexpTrvmqKNzuVtxdad/content/v/master/url/" ++ page ++ "?format=markdown")
-        event
-        (Gitbook.decodePage page)
+getPage _ path event =
+    Http.get
+        { url = "https://raw.githubusercontent.com/MTES-MCT/wikicarbone/docs/" ++ path ++ ".md"
+        , expect =
+            Http.expectString
+                (RemoteData.fromResult
+                    >> RemoteData.map (toPage path)
+                    >> event
+                )
+        }
