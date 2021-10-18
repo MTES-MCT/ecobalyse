aa5f75be4e44e1830960143882faf848cd4d79c9
Nicolas Perriault <nicolas@perriault.net>
2021-10-18 14:37:02 +0200

    Reformat data structures.

--- src/Data/Product.elm
+++ src/Data/Product.elm
@@ -24,27 +24,130 @@ type Id
 
 choices : List Product
 choices =
-    -- Note: we could probably attach the making process directly
-    -- Process uuid:
-    -- Confection (jeans);1f428a50-73c0-4fc1-ab39-00fd312458ee
-    -- Confection (gilet, jupe, pantalon, pull);387059fc-72cb-4a92-b1e7-2ef9242f8380
-    -- Confection (débardeur, tee-shirt, combinaison);26e3ca02-9bc0-45b4-b8b4-73f4b3701ad5
-    -- Confection (chemisier, manteau, veste, cape, robe);7fe48d7c-a568-4bd5-a3ac-cfa88255b4fe
-    -- Confection (ceinture, châle, chapeau, sac, écharpe);0a260a3f-260e-4b43-a0df-0cf673fda960
-    [ { id = Id "1", name = "Cape", mass = Mass.kilograms 0.95, pcrWaste = 0.2, ppm = 1600, grammage = 140, knitted = False, makingProcessUuid = Process.Uuid "7fe48d7c-a568-4bd5-a3ac-cfa88255b4fe" }
-    , { id = Id "2", name = "Châle", mass = Mass.kilograms 0.11, pcrWaste = 0.1, ppm = 1600, grammage = 140, knitted = False, makingProcessUuid = Process.Uuid "0a260a3f-260e-4b43-a0df-0cf673fda960" }
-    , { id = Id "3", name = "Chemisier", mass = Mass.kilograms 0.25, pcrWaste = 0.2, ppm = 5000, grammage = 40, knitted = False, makingProcessUuid = Process.Uuid "7fe48d7c-a568-4bd5-a3ac-cfa88255b4fe" }
-    , { id = Id "4", name = "Débardeur", mass = Mass.kilograms 0.17, pcrWaste = 0.15, ppm = 0, grammage = 0, knitted = True, makingProcessUuid = Process.Uuid "26e3ca02-9bc0-45b4-b8b4-73f4b3701ad5" }
-    , { id = Id "5", name = "Echarpe", mass = Mass.kilograms 0.11, pcrWaste = 0.1, ppm = 1600, grammage = 140, knitted = False, makingProcessUuid = Process.Uuid "0a260a3f-260e-4b43-a0df-0cf673fda960" }
-    , { id = Id "6", name = "Gilet", mass = Mass.kilograms 0.5, pcrWaste = 0.2, ppm = 0, grammage = 0, knitted = True, makingProcessUuid = Process.Uuid "387059fc-72cb-4a92-b1e7-2ef9242f8380" }
-    , { id = Id "7", name = "Jean", mass = Mass.kilograms 0.45, pcrWaste = 0.22, ppm = 3000, grammage = 140, knitted = False, makingProcessUuid = Process.Uuid "1f428a50-73c0-4fc1-ab39-00fd312458ee" }
-    , { id = Id "8", name = "Jupe", mass = Mass.kilograms 0.3, pcrWaste = 0.2, ppm = 5000, grammage = 40, knitted = False, makingProcessUuid = Process.Uuid "387059fc-72cb-4a92-b1e7-2ef9242f8380" }
-    , { id = Id "9", name = "Manteau", mass = Mass.kilograms 0.95, pcrWaste = 0.2, ppm = 1600, grammage = 140, knitted = False, makingProcessUuid = Process.Uuid "7fe48d7c-a568-4bd5-a3ac-cfa88255b4fe" }
-    , { id = Id "10", name = "Pantalon", mass = Mass.kilograms 0.45, pcrWaste = 0.2, ppm = 3000, grammage = 140, knitted = False, makingProcessUuid = Process.Uuid "387059fc-72cb-4a92-b1e7-2ef9242f8380" }
-    , { id = Id "11", name = "Pull", mass = Mass.kilograms 0.5, pcrWaste = 0.2, ppm = 0, grammage = 0, knitted = True, makingProcessUuid = Process.Uuid "387059fc-72cb-4a92-b1e7-2ef9242f8380" }
-    , { id = Id "12", name = "Robe", mass = Mass.kilograms 0.3, pcrWaste = 0.2, ppm = 5000, grammage = 40, knitted = False, makingProcessUuid = Process.Uuid "7fe48d7c-a568-4bd5-a3ac-cfa88255b4fe" }
+    -- Making process uuid:
+    -- - Confection (jeans);1f428a50-73c0-4fc1-ab39-00fd312458ee
+    -- - Confection (gilet, jupe, pantalon, pull);387059fc-72cb-4a92-b1e7-2ef9242f8380
+    -- - Confection (débardeur, tee-shirt, combinaison);26e3ca02-9bc0-45b4-b8b4-73f4b3701ad5
+    -- - Confection (chemisier, manteau, veste, cape, robe);7fe48d7c-a568-4bd5-a3ac-cfa88255b4fe
+    -- - Confection (ceinture, châle, chapeau, sac, écharpe);0a260a3f-260e-4b43-a0df-0cf673fda960
+    [ { id = Id "1"
+      , name = "Cape"
+      , mass = Mass.kilograms 0.95
+      , pcrWaste = 0.2
+      , ppm = 1600
+      , grammage = 140
+      , knitted = False
+      , makingProcessUuid = Process.Uuid "7fe48d7c-a568-4bd5-a3ac-cfa88255b4fe"
+      }
+    , { id = Id "2"
+      , name = "Châle"
+      , mass = Mass.kilograms 0.11
+      , pcrWaste = 0.1
+      , ppm = 1600
+      , grammage = 140
+      , knitted = False
+      , makingProcessUuid = Process.Uuid "0a260a3f-260e-4b43-a0df-0cf673fda960"
+      }
+    , { id = Id "3"
+      , name = "Chemisier"
+      , mass = Mass.kilograms 0.25
+      , pcrWaste = 0.2
+      , ppm = 5000
+      , grammage = 40
+      , knitted = False
+      , makingProcessUuid = Process.Uuid "7fe48d7c-a568-4bd5-a3ac-cfa88255b4fe"
+      }
+    , { id = Id "4"
+      , name = "Débardeur"
+      , mass = Mass.kilograms 0.17
+      , pcrWaste = 0.15
+      , ppm = 0
+      , grammage = 0
+      , knitted = True
+      , makingProcessUuid = Process.Uuid "26e3ca02-9bc0-45b4-b8b4-73f4b3701ad5"
+      }
+    , { id = Id "5"
+      , name = "Echarpe"
+      , mass = Mass.kilograms 0.11
+      , pcrWaste = 0.1
+      , ppm = 1600
+      , grammage = 140
+      , knitted = False
+      , makingProcessUuid = Process.Uuid "0a260a3f-260e-4b43-a0df-0cf673fda960"
+      }
+    , { id = Id "6"
+      , name = "Gilet"
+      , mass = Mass.kilograms 0.5
+      , pcrWaste = 0.2
+      , ppm = 0
+      , grammage = 0
+      , knitted = True
+      , makingProcessUuid = Process.Uuid "387059fc-72cb-4a92-b1e7-2ef9242f8380"
+      }
+    , { id = Id "7"
+      , name = "Jean"
+      , mass = Mass.kilograms 0.45
+      , pcrWaste = 0.22
+      , ppm = 3000
+      , grammage = 140
+      , knitted = False
+      , makingProcessUuid = Process.Uuid "1f428a50-73c0-4fc1-ab39-00fd312458ee"
+      }
+    , { id = Id "8"
+      , name = "Jupe"
+      , mass = Mass.kilograms 0.3
+      , pcrWaste = 0.2
+      , ppm = 5000
+      , grammage = 40
+      , knitted = False
+      , makingProcessUuid = Process.Uuid "387059fc-72cb-4a92-b1e7-2ef9242f8380"
+      }
+    , { id = Id "9"
+      , name = "Manteau"
+      , mass = Mass.kilograms 0.95
+      , pcrWaste = 0.2
+      , ppm = 1600
+      , grammage = 140
+      , knitted = False
+      , makingProcessUuid = Process.Uuid "7fe48d7c-a568-4bd5-a3ac-cfa88255b4fe"
+      }
+    , { id = Id "10"
+      , name = "Pantalon"
+      , mass = Mass.kilograms 0.45
+      , pcrWaste = 0.2
+      , ppm = 3000
+      , grammage = 140
+      , knitted = False
+      , makingProcessUuid = Process.Uuid "387059fc-72cb-4a92-b1e7-2ef9242f8380"
+      }
+    , { id = Id "11"
+      , name = "Pull"
+      , mass = Mass.kilograms 0.5
+      , pcrWaste = 0.2
+      , ppm = 0
+      , grammage = 0
+      , knitted = True
+      , makingProcessUuid = Process.Uuid "387059fc-72cb-4a92-b1e7-2ef9242f8380"
+      }
+    , { id = Id "12"
+      , name = "Robe"
+      , mass = Mass.kilograms 0.3
+      , pcrWaste = 0.2
+      , ppm = 5000
+      , grammage = 40
+      , knitted = False
+      , makingProcessUuid = Process.Uuid "7fe48d7c-a568-4bd5-a3ac-cfa88255b4fe"
+      }
     , tShirt
-    , { id = Id "14", name = "Veste", mass = Mass.kilograms 0.95, pcrWaste = 0.2, ppm = 3000, grammage = 140, knitted = False, makingProcessUuid = Process.Uuid "7fe48d7c-a568-4bd5-a3ac-cfa88255b4fe" }
+    , { id = Id "14"
+      , name = "Veste"
+      , mass = Mass.kilograms 0.95
+      , pcrWaste = 0.2
+      , ppm = 3000
+      , grammage = 140
+      , knitted = False
+      , makingProcessUuid = Process.Uuid "7fe48d7c-a568-4bd5-a3ac-cfa88255b4fe"
+      }
     ]
 
 
