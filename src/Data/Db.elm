c6f00116f83f18905433da88ff9a1cf1766ef923
Nicolas Perriault <nicolas@perriault.net>
2021-10-18 22:17:26 +0200

    Cleanup obsolete comments.

--- src/Data/Db.elm
+++ src/Data/Db.elm
@@ -6,21 +6,6 @@ import Data.Process exposing (Process)
 import Data.Product exposing (Product)
 
 
-
--- TODO:
--- - refactor to have two types: Db and Data:
---   * Data has all its members Maybe(WebData x)
---   * Db has all its members set
---
--- Loading order:
--- 1. processes (so we get materials)
--- 2. country processes (so we get countries as well)
--- 3. products
---
--- Notes:
--- - investigate using RemoteData.fromList https://package.elm-lang.org/packages/krisajenkins/remotedata/latest/RemoteData#fromList
-
-
 type alias Db =
     { countries : List Country2
     , materials : List Material
