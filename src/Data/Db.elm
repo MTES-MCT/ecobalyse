1b33a06263c47a2c4495dd3aa1c31834a22b3184
Nicolas Perriault <nicolas@perriault.net>
2021-10-25 14:21:55 +0200

    Load materials from external dedicated datasource.

--- src/Data/Db.elm
+++ src/Data/Db.elm
@@ -6,7 +6,6 @@ import Data.Process as Process exposing (Process)
 import Data.Product as Product exposing (Product)
 import Data.Transport as Transport exposing (Distances)
 import Json.Decode as Decode exposing (Decoder)
-import RemoteData exposing (WebData)
 
 
 type alias Db =
@@ -18,55 +17,27 @@ type alias Db =
     }
 
 
-type alias BaseData =
-    { countries : List Country
-    , processes : List Process
-    , products : List Product
-    , transports : Distances
-    }
-
-
 empty : Db
 empty =
-    buildFromBaseData
-        { countries = []
-        , processes = []
-        , products = []
-        , transports = Transport.emptyDistances
-        }
-
-
-build : WebData (List Process) -> WebData (List Country) -> WebData (List Product) -> WebData Distances -> WebData Db
-build processesData countriesData productsData transportsData =
-    countriesData
-        |> RemoteData.map BaseData
-        |> RemoteData.andMap processesData
-        |> RemoteData.andMap productsData
-        |> RemoteData.andMap transportsData
-        |> RemoteData.map buildFromBaseData
-
-
-buildFromBaseData : BaseData -> Db
-buildFromBaseData { countries, processes, products, transports } =
-    { processes = processes
-    , countries = countries
-    , materials = Material.fromProcesses processes
-    , products = products
-    , transports = transports
+    { countries = []
+    , materials = []
+    , processes = []
+    , products = []
+    , transports = Transport.emptyDistances
     }
 
 
 buildFromJson : String -> Result String Db
 buildFromJson json =
-    Decode.decodeString decodeBaseData json
+    Decode.decodeString decode json
         |> Result.mapError Decode.errorToString
-        |> Result.map buildFromBaseData
 
 
-decodeBaseData : Decoder BaseData
-decodeBaseData =
-    Decode.map4 BaseData
+decode : Decoder Db
+decode =
+    Decode.map5 Db
         (Decode.field "countries" Country.decodeList)
+        (Decode.field "materials" Material.decodeList)
         (Decode.field "processes" Process.decodeList)
         (Decode.field "products" Product.decodeList)
         (Decode.field "transports" Transport.decodeDistances)
--- src/Data/Material.elm
+++ src/Data/Material.elm
@@ -9,18 +9,21 @@ import Json.Encode as Encode
 type alias Material =
     { uuid : Process.Uuid -- Note: we use the material + spinning process uuid here
     , name : String
+    , shortName : String
     , category : Category
     }
 
 
 fromProcesses : List Process -> List Material
 fromProcesses =
+    -- FIXME: obsololete once we import materials.json -> REMOVE
     Process.cat1 Process.Textile
         >> Process.cat2 Process.Material
         >> List.map
             (\{ uuid, name, cat3 } ->
                 { uuid = uuid
                 , name = name
+                , shortName = ""
                 , category =
                     case cat3 of
                         Process.SyntheticMaterials ->
@@ -49,23 +52,6 @@ firstFromCategory category =
         >> Result.fromMaybe ("Aucune matière dans la catégorie " ++ Category.toString category)
 
 
-decode : Decoder Material
-decode =
-    Decode.map3 Material
-        (Decode.field "uuid" (Decode.map Process.Uuid Decode.string))
-        (Decode.field "name" Decode.string)
-        (Decode.field "category" Category.decode)
-
-
-encode : Material -> Encode.Value
-encode v =
-    Encode.object
-        [ ( "uuid", Encode.string (Process.uuidToString v.uuid) )
-        , ( "name", Encode.string v.name )
-        , ( "category", Category.encode v.category )
-        ]
-
-
 shortName : Material -> String
 shortName =
     .name
@@ -89,3 +75,32 @@ ucFirst string =
 
         [] ->
             string
+
+
+decode : Decoder Material
+decode =
+    Decode.map4 Material
+        (Decode.field "uuid" (Decode.map Process.Uuid Decode.string))
+        (Decode.field "name" Decode.string)
+        (Decode.field "shortName" Decode.string)
+        (Decode.field "category" Category.decode)
+
+
+decodeList : Decoder (List Material)
+decodeList =
+    Decode.list decode
+
+
+encode : Material -> Encode.Value
+encode v =
+    Encode.object
+        [ ( "uuid", v.uuid |> Process.uuidToString |> Encode.string )
+        , ( "name", v.name |> Encode.string )
+        , ( "shortName", v |> shortName |> Encode.string )
+        , ( "category", v.category |> Category.toString |> Encode.string )
+        ]
+
+
+encodeAll : List Material -> String
+encodeAll =
+    Encode.list encode >> Encode.encode 0
