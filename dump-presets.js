e28563f193a7f7ae2fd89a1bdf83ab3814cf99b5
Nicolas Perriault <nicolas@perriault.net>
2021-11-22 11:55:42 +0100

    Fix build steps order.

--- dump-presets.js
+++ dump-presets.js
@@ -1,20 +1,15 @@
 /**
  * This script perform test simulations from presets and dumps the resulting
  * output in a static JSON file.
+ *
+ * This scripts:
+ * - Create a big JSON aggregate of all public JSON data files
+ * - Loads the DumpPresets Elm app and feeds it with this file as a flag
+ * - Subscribes and collect Elm app result JSON output
+ * - Write it to public/data/dump-presets.json
  */
 const fs = require("fs");
 const { Elm } = require("./dump-presets-app");
-
-/* TODO :
-- create an Elm main entrypoint :
-  * loading Db from static (see Db tests)
-  * batch running simulation for presets
-  * collect results and dump them as JSON
-- Load Elm app here (ensure it's compiled)
-- Init it with appropriate flags(?)
-- Subscribe to result output, collect result
-- Write resulting JSON to public/data/dump-presets.json with these results
-*/
 
 function getJson(path) {
   return JSON.parse(fs.readFileSync(path).toString());
@@ -31,8 +26,14 @@ const jsonDb = JSON.stringify({
 const elmApp = Elm.DumpPresets.init({ flags: { jsonDb } });
 
 elmApp.ports.output.subscribe((result) => {
+  // Exit with error if the process failed
+  if ("error" in result) {
+    throw new Error(`Error while dumping test preset results: ${jsonExport["error"]}`);
+  }
+
+  // Attempt creating a new static JSON dump
   try {
-    fs.writeFileSync("public/data/dump-presets.json", JSON.stringify(result));
+    fs.writeFileSync("public/data/dump-presets.json", JSON.stringify(result, null, 2));
     console.log("Successfully generated preset results at public/data/dump-presets.json");
   } catch (err) {
     throw new Error(`Unable to generate preset results: ${err}`);
