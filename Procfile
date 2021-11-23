d34b57a8b0aba88291644037beac7fc750293f5b
Nicolas Perriault <nicolas@perriault.net>
2021-11-23 16:43:01 +0100

    Use env var to set server PORT.

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
 web: npm run server:start
-
--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require("express");
 const { Elm } = require("./server-app");
 
 const app = express();
-const port = 3000;
+const host = "0.0.0.0";
+const port = process.env.PORT || 3000;
 const { buildJsonDb } = require("./lib");
 
 const elmApp = Elm.Server.init({
@@ -41,6 +42,6 @@ app.get("/", ({ query }, res) => {
   });
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+app.listen(port, host, () => {
+  console.log(`Example app listening at http://${host}:${port}`);
 });
