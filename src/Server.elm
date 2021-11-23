78e32effd5586a7dfdab327f9585f4ee6aec5d5f
Nicolas Perriault <nicolas@perriault.net>
2021-11-23 12:06:27 +0100

    Implement a minimalist HTTP API.

--- src/Server.elm
+++ src/Server.elm
@@ -1,7 +1,7 @@
 port module Server exposing (main)
 
 import Data.Co2 as Co2
-import Data.Db as Db
+import Data.Db as Db exposing (Db)
 import Data.Inputs as Inputs
 import Data.Simulator as Simulator
 import Json.Decode as Decode
@@ -9,48 +9,60 @@ import Json.Encode as Encode
 
 
 type alias Flags =
-    {}
+    { jsonDb : String }
 
 
 type alias Model =
-    {}
+    { db : Result String Db }
 
 
 type Msg
-    = Received String
+    = Received { inputs : Encode.Value, fn : Encode.Value }
 
 
 init : Flags -> ( Model, Cmd Msg )
-init _ =
+init { jsonDb } =
     -- TODO: pass json string as a flag, parse to init Db, store Db result in model
     --       alternative: generate static Db elm module as we do for tests -> mutualize
-    ( {}
+    ( { db = Db.buildFromJson jsonDb }
     , Cmd.none
     )
 
 
-toResponse : Result String Float -> Cmd Msg
-toResponse result =
+toResponse : Encode.Value -> Result String Float -> Cmd Msg
+toResponse fn result =
     case result of
         Ok score ->
-            Encode.object [ ( "score", Encode.float score ) ] |> output
+            Encode.object
+                [ ( "score", Encode.float score )
+                , ( "fn", fn )
+                ]
+                |> output
 
         Err error ->
-            Encode.object [ ( "error", Encode.string error ) ] |> output
+            Encode.object
+                [ ( "error", Encode.string error )
+                , ( "fn", fn )
+                ]
+                |> output
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    case msg of
-        Received json ->
+    case ( msg, model.db ) of
+        ( Received { inputs, fn }, Ok db ) ->
             ( model
-            , json
-                |> Decode.decodeString Inputs.decodeQuery
+            , inputs
+                |> Decode.decodeValue Inputs.decodeQuery
                 |> Result.mapError Decode.errorToString
-                -- FIXME: Db should be loaded at this point
-                |> Result.andThen (Simulator.compute Db.empty)
+                |> Result.andThen (Simulator.compute db)
                 |> Result.map (.co2 >> Co2.inKgCo2e)
-                |> toResponse
+                |> toResponse fn
+            )
+
+        ( _, Err error ) ->
+            ( model
+            , Cmd.none
             )
 
 
@@ -63,7 +75,7 @@ main =
         }
 
 
-port input : (String -> msg) -> Sub msg
+port input : ({ inputs : Encode.Value, fn : Encode.Value } -> msg) -> Sub msg
 
 
 port output : Encode.Value -> Cmd msg
