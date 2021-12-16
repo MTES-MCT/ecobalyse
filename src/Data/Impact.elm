5f8a1832622e75aa4e4772ef832eab8e7edeb5fd
Nicolas Perriault <nicolas@perriault.net>
2021-12-16 11:35:57 +0100

    Add Impact encoder.

--- src/Data/Impact.elm
+++ src/Data/Impact.elm
@@ -2,6 +2,7 @@ module Data.Impact exposing (..)
 
 import Dict
 import Json.Decode as Decode exposing (Decoder)
+import Json.Encode as Encode
 
 
 type Trigram
@@ -23,19 +24,24 @@ decodeList : Decoder (List Impact)
 decodeList =
     let
         decodeDictValue =
-            Decode.map2 (\a b -> { label = a, unit = b })
+            Decode.map2 (\label unit -> { label = label, unit = unit })
                 (Decode.field "label_fr" Decode.string)
                 (Decode.field "unit_fr" Decode.string)
+
+        toImpact ( key, { label, unit } ) =
+            Impact (trigramFromString key) label unit
     in
     Decode.dict decodeDictValue
-        |> Decode.andThen
-            (Dict.toList
-                >> List.map
-                    (\( trigram, { label, unit } ) ->
-                        Impact (Trigram trigram) label unit
-                    )
-                >> Decode.succeed
-            )
+        |> Decode.andThen (Dict.toList >> List.map toImpact >> Decode.succeed)
+
+
+encodeImpact : Impact -> Encode.Value
+encodeImpact v =
+    Encode.object
+        [ ( "trigram", v.trigram |> trigramToString |> Encode.string )
+        , ( "label", Encode.string v.label )
+        , ( "unit", Encode.string v.unit )
+        ]
 
 
 trigramToString : Trigram -> String
