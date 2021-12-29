42dc7ae00c578be998dec4cb3d07e00445a9e7f6
Nicolas Perriault <nicolas@perriault.net>
2021-12-29 15:30:28 +0100

    Add pure Elm query string parameters parser.

--- src/Server/Query.elm
+++ src/Server/Query.elm
@@ -9,7 +9,102 @@ import Json.Decode as Decode exposing (Decoder)
 import Json.Decode.Extra as DecodeExtra
 import Json.Decode.Pipeline as Pipe
 import Json.Encode as Encode
-import Mass
+import Mass exposing (Mass)
+import Url.Parser.Query as Query
+
+
+
+-- Query String
+
+
+fromQueryString : Query.Parser Inputs.Query
+fromQueryString =
+    Query.map Inputs.Query
+        (massParser "mass")
+        |> apply (materialParser "material")
+        |> apply (productParser "product")
+        |> apply (countriesParser "countries")
+        |> apply (ratioParser "dyeingWeighting")
+        |> apply (ratioParser "airTransportRatio")
+        |> apply (ratioParser "recycledRatio")
+        |> apply customCountryMixesParser
+
+
+apply : Query.Parser a -> Query.Parser (a -> b) -> Query.Parser b
+apply argParser funcParser =
+    -- See https://package.elm-lang.org/packages/elm/url/latest/Url-Parser-Query#map8
+    Query.map2 (<|) funcParser argParser
+
+
+floatParser : String -> Query.Parser (Maybe Float)
+floatParser key =
+    Query.custom key <|
+        \stringList ->
+            case stringList of
+                [ str ] ->
+                    String.toFloat str
+
+                _ ->
+                    Nothing
+
+
+massParser : String -> Query.Parser Mass
+massParser key =
+    floatParser key
+        |> Query.map
+            (Maybe.map Mass.kilograms
+                >> Maybe.withDefault (Mass.kilograms -1)
+            )
+
+
+productParser : String -> Query.Parser Product.Id
+productParser key =
+    Query.string key
+        |> Query.map
+            (Maybe.map Product.Id
+                >> Maybe.withDefault (Product.Id "")
+            )
+
+
+materialParser : String -> Query.Parser Process.Uuid
+materialParser key =
+    Query.string key
+        |> Query.map
+            (Maybe.map Process.Uuid
+                >> Maybe.withDefault (Process.Uuid "")
+            )
+
+
+countriesParser : String -> Query.Parser (List Country.Code)
+countriesParser key =
+    Query.custom key <|
+        \stringList ->
+            -- if List.length countries == 0 && not (String.contains "[]" key) then
+            List.map Country.Code stringList
+
+
+ratioParser : String -> Query.Parser (Maybe Unit.Ratio)
+ratioParser key =
+    floatParser key
+        |> Query.map (Maybe.map Unit.ratio)
+
+
+impactParser : String -> Query.Parser (Maybe Unit.Impact)
+impactParser key =
+    floatParser key
+        |> Query.map (Maybe.map Unit.impact)
+
+
+customCountryMixesParser : Query.Parser Inputs.CustomCountryMixes
+customCountryMixesParser =
+    Query.map3 Inputs.CustomCountryMixes
+        (impactParser "customCountryMix[fabric]")
+        (impactParser "customCountryMix[dyeing]")
+        (impactParser "customCountryMix[making]")
+
+
+
+-- Express JSON Query
 
 
 expressQueryDecoder : Decoder Inputs.Query
