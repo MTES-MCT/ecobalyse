5ad731164c141c594f1201bdafdc1ffd7dfe35b5
Nicolas Perriault <nicolas@perriault.net>
2022-01-04 12:28:43 +0100

    Add impact selector to Examples page.

--- src/Page/Examples.elm
+++ src/Page/Examples.elm
@@ -6,44 +6,47 @@ import Data.Session exposing (Session)
 import Data.Simulator as Simulator
 import Html exposing (..)
 import Html.Attributes exposing (..)
-import Route
 import Views.Container as Container
+import Views.Impact as ImpactView
 import Views.Summary as SummaryView
 
 
 type alias Model =
-    ()
+    { impact : Impact.Trigram }
 
 
 type Msg
-    = NoOp
+    = SwitchImpact Impact.Trigram
 
 
 init : Session -> ( Model, Session, Cmd Msg )
 init session =
-    ( (), session, Cmd.none )
+    ( { impact = Impact.trg "cch" }
+    , session
+    , Cmd.none
+    )
 
 
 update : Session -> Msg -> Model -> ( Model, Session, Cmd Msg )
 update session msg model =
     case msg of
-        NoOp ->
-            ( model, session, Cmd.none )
+        SwitchImpact impact ->
+            ( { model | impact = impact }, session, Cmd.none )
 
 
-viewExamples : Session -> Html Msg
-viewExamples session =
+viewExamples : Session -> Model -> Html Msg
+viewExamples session { impact } =
     div []
         [ div [ class "row mb-3" ]
             [ div [ class "col-md-7 col-lg-8 col-xl-9" ]
                 [ h1 [ class "mb-3" ] [ text "Exemples de simulation" ]
                 ]
             , div [ class "col-md-5 col-lg-4 col-xl-3 text-center text-md-end" ]
-                [ a
-                    [ Route.href (Route.Simulator Impact.defaultTrigram Nothing)
-                    , class "btn btn-primary w-100"
-                    ]
-                    [ text "Faire une simulation" ]
+                [ ImpactView.selector
+                    { impacts = session.db.impacts
+                    , selected = impact
+                    , switch = SwitchImpact
+                    }
                 ]
             ]
         , Inputs.presets
@@ -51,7 +54,9 @@ viewExamples session =
                 (Simulator.compute session.db
                     >> SummaryView.view
                         { session = session
-                        , impact = Impact.default
+                        , impact =
+                            Impact.getDefinition impact session.db.impacts
+                                |> Result.withDefault Impact.default
                         , reusable = True
                         }
                     >> (\v -> div [ class "col" ] [ v ])
@@ -61,10 +66,10 @@ viewExamples session =
 
 
 view : Session -> Model -> ( String, List (Html Msg) )
-view session _ =
+view session model =
     ( "Exemples"
     , [ Container.centered [ class "pb-5" ]
-            [ viewExamples session
+            [ viewExamples session model
             ]
       ]
     )
