2a5c607133aa590406c58e35027bd21c8f173f04
Mathieu Agopian <mathieu@agopian.info>
2022-01-04 14:52:30 +0100

    Add a test to ensure the first country is the material's default country

--- tests/Data/InputsTest.elm
+++ tests/Data/InputsTest.elm
@@ -1,7 +1,9 @@
 module Data.InputsTest exposing (..)
 
+import Data.Country as Country
 import Data.Inputs as Inputs
 import Expect exposing (Expectation)
+import List.Extra as LE
 import Test exposing (..)
 import TestDb exposing (testDb)
 
@@ -34,6 +36,22 @@ suite =
                         |> Inputs.b64decode
                         |> Expect.equal (Ok sampleQuery)
                         |> asTest "should base64 encode and decode a query"
+                    ]
+                , describe "A list of countries with a first country different than the material's default country"
+                    [ Inputs.tShirtCotonAsie
+                        |> (\query ->
+                                let
+                                    badCountries =
+                                        query.countries
+                                            |> LE.setAt 0 (Country.codeFromString "FR")
+                                in
+                                { query | countries = badCountries }
+                           )
+                        |> Inputs.fromQuery db
+                        |> Result.map .countries
+                        |> Result.andThen (List.head >> Result.fromMaybe "Couldn't get the first country from the list")
+                        |> Expect.equal (Country.findByCode (Country.codeFromString "CN") db.countries)
+                        |> asTest "should replace the first country with the material's default country"
                     ]
                 ]
 
