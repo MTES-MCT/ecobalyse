0e6699e718f6b557d390d2b7b5ec708a527b8b17
Nicolas Perriault <nicolas@perriault.net>
2022-02-04 12:22:11 +0100

    Introduce generic Explorer table data structure.

--- src/Page/Explore/Products.elm
+++ src/Page/Explore/Products.elm
@@ -10,109 +10,107 @@ import Views.Icon as Icon
 import Views.Table as Table
 
 
-details : Db -> Product -> Html msg
-details _ product =
-    Table.responsiveDefault [ class "view-details" ]
-        [ tbody []
-            [ tr []
-                [ th [] [ text "Identifiant" ]
-                , td [] [ code [] [ text (Product.idToString product.id) ] ]
-                ]
-            , tr []
-                [ th [] [ text "Nom" ]
-                , td [] [ text product.name ]
-                ]
-            , tr []
-                [ th [] [ text "Masse par défaut" ]
-                , td [] [ Format.kg product.mass ]
-                ]
-            , tr []
-                [ th [] [ text "Taux de perte (PCR)" ]
-                , td [] [ Format.ratio product.pcrWaste ]
-                ]
-            , tr []
-                [ th [] [ text "Type de procédé" ]
-                , td []
+table : { detailed : Bool } -> List { label : String, toCell : Product -> Html msg }
+table { detailed } =
+    [ { label = "Identifiant"
+      , toCell =
+            \product ->
+                td []
+                    [ if detailed then
+                        code [] [ text (Product.idToString product.id) ]
+
+                      else
+                        a [ Route.href (Route.Explore (Db.Products (Just product.id))) ]
+                            [ code [] [ text (Product.idToString product.id) ] ]
+                    ]
+      }
+    , { label = "Nom"
+      , toCell = \product -> td [] [ text product.name ]
+      }
+    , { label = "Masse par défaut"
+      , toCell = \product -> td [] [ Format.kg product.mass ]
+      }
+    , { label = "Taux de perte (PCR)"
+      , toCell = \product -> td [] [ Format.ratio product.pcrWaste ]
+      }
+    , { label = "Type de procédé"
+      , toCell =
+            \product ->
+                td []
                     [ if product.knitted then
                         text "Tricotage"
 
                       else
                         text "Tissage"
                     ]
-                ]
-            , tr []
-                [ th [] [ text "Pick-per-meter" ]
-                , td []
+      }
+    , { label = "Pick-per-meter"
+      , toCell =
+            \product ->
+                td []
                     [ if product.knitted then
                         text "N/A"
 
                       else
-                        text <| Format.formatInt "picks-per-meter" product.ppm
+                        text <| Format.formatInt "picks/m" product.ppm
                     ]
-                ]
-            , tr []
-                [ th [] [ text "Grammage" ]
-                , td []
+      }
+    , { label = "Grammage"
+      , toCell =
+            \product ->
+                td []
                     [ if product.knitted then
                         text "N/A"
 
                       else
-                        text <| Format.formatInt "gr. per kg" product.grammage
+                        text <| Format.formatInt "gr/kg" product.grammage
                     ]
-                ]
-            , tr []
-                [ th [] [ text "Type de procédé" ]
-                , td [] [ text product.fabricProcess.name ]
-                ]
-            , tr []
-                [ th [] [ text "Confection" ]
-                , td [] [ text product.makingProcess.name ]
-                ]
-            , tr []
-                [ th [] [ text "Procédé de repassage" ]
-                , td [] [ text product.useIroningProcess.name ]
-                ]
-            , tr []
-                [ th [] [ text "Procédé composite d'utilisation hors-repassage" ]
-                , td [] [ text product.useNonIroningProcess.name ]
-                ]
-            , tr []
-                [ th [] [ text "Cycles d'entretien (par défaut)" ]
-                , td [] [ text (String.fromInt product.useDefaultNbCycles) ]
-                ]
-            , tr []
-                [ th [] [ text "Utilisations avant lavage" ]
-                , td [] [ product.wearsPerCycle |> String.fromInt |> text ]
-                ]
-            , tr []
-                [ th [] [ text "Nombre de jours porté" ]
-                , td []
-                    [ Format.days product.daysOfWear
-                    , forInfoNotice
-                    ]
-                ]
-            , tr []
-                [ th [] [ text "Ratio de séchage électrique" ]
-                , td []
-                    [ div [] [ Format.ratio product.useRatioDryer ]
-                    , forInfoNotice
-                    ]
-                ]
-            , tr []
-                [ th [] [ text "Ratio de repassage" ]
-                , td []
-                    [ div [] [ Format.ratio product.useRatioIroning ]
-                    , forInfoNotice
-                    ]
-                ]
-            , tr []
-                [ th [] [ text "Temps de repassage" ]
-                , td []
-                    [ div [] [ Format.hours product.useTimeIroning ]
-                    , forInfoNotice
-                    ]
-                ]
-            ]
+      }
+    , { label = "Procédé"
+      , toCell = \product -> td [] [ text product.fabricProcess.name ]
+      }
+    , { label = "Confection"
+      , toCell = \product -> td [] [ text product.makingProcess.name ]
+      }
+    , { label = "Nombre de jours porté"
+      , toCell = \product -> td [] [ Format.days product.daysOfWear ]
+      }
+    , { label = "Cycles d'entretien (par défaut)"
+      , toCell = \product -> td [] [ product.wearsPerCycle |> String.fromInt |> text ]
+      }
+    , { label = "Utilisations avant lavage"
+      , toCell = \product -> td [] [ text (String.fromInt product.useDefaultNbCycles) ]
+      }
+    , { label = "Procédé de repassage"
+      , toCell = \product -> td [] [ text product.useIroningProcess.name ]
+      }
+    , { label = "Procédé d'utilisation hors-repassage"
+      , toCell = \product -> td [] [ text product.useNonIroningProcess.name ]
+      }
+    , { label = "Séchage électrique"
+      , toCell = \product -> td [] [ Format.ratio product.useRatioDryer ]
+      }
+    , { label = "Repassage (part)"
+      , toCell = \product -> td [] [ Format.ratio product.useRatioIroning ]
+      }
+    , { label = "Repassage (temps)"
+      , toCell = \product -> td [] [ Format.hours product.useTimeIroning ]
+      }
+    ]
+
+
+details : Db -> Product -> Html msg
+details _ product =
+    Table.responsiveDefault [ class "view-details" ]
+        [ table { detailed = True }
+            |> List.map
+                (\{ label, toCell } ->
+                    tr []
+                        [ th [] [ text label ]
+                        , toCell product
+                        ]
+                )
+            |> tbody []
         ]
 
 
@@ -120,79 +118,16 @@ view : List Product -> Html msg
 view products =
     Table.responsiveDefault [ class "view-list" ]
         [ thead []
-            [ tr []
-                [ th [] [ text "Identifiant" ]
-                , th [] [ text "Nom" ]
-                , th [] [ text "Masse par défaut" ]
-                , th [] [ text "Taux de perte (PCR)" ]
-                , th [] [ text "Type de procédé" ]
-                , th [] [ text "Pick-per-meter" ]
-                , th [] [ text "Grammage" ]
-                , th [] [ text "Procédé" ]
-                , th [] [ text "Confection" ]
-                , th [] [ text "Nombre de jours porté" ]
-                , th [] [ text "Cycles d'entretien (par défaut)" ]
-                , th [] [ text "Utilisations avant lavage" ]
-                , th [] [ text "Repassage" ]
-                , th [] [ text "Hors-repassage" ]
-                , th [] [ text "Séchage électrique" ]
-                , th [] [ text "Repassage (part)" ]
-                , th [] [ text "Repassage (temps)" ]
-                ]
+            [ table { detailed = False }
+                |> List.map (\{ label } -> th [] [ text label ])
+                |> tr []
             ]
         , products
-            |> List.map row
+            |> List.map
+                (\product ->
+                    table { detailed = False }
+                        |> List.map (\{ toCell } -> toCell product)
+                        |> tr []
+                )
             |> tbody []
-        ]
-
-
-row : Product -> Html msg
-row product =
-    tr []
-        [ td []
-            [ a [ Route.href (Route.Explore (Db.Products (Just product.id))) ]
-                [ code [] [ text (Product.idToString product.id) ] ]
-            ]
-        , td [] [ text product.name ]
-        , td [] [ Format.kg product.mass ]
-        , td [] [ Format.ratio product.pcrWaste ]
-        , td []
-            [ if product.knitted then
-                text "Tricotage"
-
-              else
-                text "Tissage"
-            ]
-        , td []
-            [ if product.knitted then
-                text "N/A"
-
-              else
-                text <| Format.formatInt "picks-per-meter" product.ppm
-            ]
-        , td []
-            [ if product.knitted then
-                text "N/A"
-
-              else
-                text <| Format.formatInt "gr. per kg" product.grammage
-            ]
-        , td [] [ text product.fabricProcess.name ]
-        , td [] [ text product.makingProcess.name ]
-        , td [ class "text-end" ] [ Format.days product.daysOfWear ]
-        , td [ class "text-end" ] [ product.wearsPerCycle |> String.fromInt |> text ]
-        , td [ class "text-end" ] [ text (String.fromInt product.useDefaultNbCycles) ]
-        , td [] [ text product.useIroningProcess.name ]
-        , td [] [ text product.useNonIroningProcess.name ]
-        , td [ class "text-end" ] [ Format.ratio product.useRatioDryer ]
-        , td [ class "text-end" ] [ Format.ratio product.useRatioIroning ]
-        , td [ class "text-end" ] [ Format.hours product.useTimeIroning ]
-        ]
-
-
-forInfoNotice : Html msg
-forInfoNotice =
-    div [ class "text-muted fs-7" ]
-        [ span [ class "me-1" ] [ Icon.info ]
-        , text "Affichage pour information, valeur intégrée dans les précalculs de procédé"
         ]
