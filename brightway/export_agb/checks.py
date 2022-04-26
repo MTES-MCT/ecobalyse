cfb7cbc3db9bd737c48f1e4bfc2052c8f3fc7a56
Mathieu Agopian <mathieu@agopian.info>
2022-04-26 18:10:07 +0200

    Improve checks: compare to Agribalyse_Synthese.csv impacts

--- brightway/export_agb/checks.py
+++ brightway/export_agb/checks.py
@@ -4,11 +4,15 @@
 """Vérification des exports de la base Agribalyse."""
 
 import copy
+import csv
 import json
-from impacts import impacts
+import re
+from impacts import impacts_to_synthese
+
+THRESHOLD = 4  # en pourcentage : 10 -> 10%
 
 
-def checks(products, processes):
+def check_missing_steps(products):
     # Nombre de produits qui n'ont pas 5 étapes
     count = 0
     for key, product in products.items():
@@ -16,14 +20,56 @@ def checks(products, processes):
             if step == {}:
                 count += 1
                 print(f"{key} has no {step_key}")
+    return count
 
-    print(f"{count} missing steps")
 
+def check_ciqual_impacts(processes, synthese_filename):
+    ciqual_code_regex = re.compile(r"\[Ciqual code: (\d+)\]")
+
+    processes_to_ciqual = {}
+    for process in processes:
+        match = re.search(ciqual_code_regex, process)
+        if match:
+            ciqual_code = match[1]
+            processes_to_ciqual[ciqual_code] = processes[process]
+
+    print(
+        "Liste des différences d'impact entre la synthèse agribalyse et les exports json:"
+    )
+    with open(synthese_filename) as csvfile:
+        reader = csv.DictReader(csvfile)
+        count = 0
+        for row in reader:
+            ciqual_code = row["Code CIQUAL"]
+            for (impact, (trigram, multiplier)) in impacts_to_synthese.items():
+                impact_a = float(row[impact]) * multiplier
+                impact_b = processes_to_ciqual[ciqual_code][trigram]
+                diff = get_diff(impact_a, impact_b)
+                if diff:
+                    # import ipdb
+
+                    # ipdb.set_trace()
+                    count += 1
+                    print(
+                        f"{ciqual_code} (impact {trigram}): {diff} ({round(diff * 100 / abs((max(impact_a, impact_b))))}%)"
+                    )
+        return count
+
+
+def get_diff(impact_a, impact_b):
+    max_impact = max(impact_a, impact_b)
+    min_impact = min(impact_a, impact_b)
+    diff = max_impact - min_impact
+    threshold = abs(max_impact) * THRESHOLD / 100
+    if diff > threshold:
+        return diff
+
+
+def check_impact_diff(products, processes):
     # Différence entre les impacts globaux et la somme des sous-impacts à l'étape consommation
     diff_impact = copy.deepcopy(processes)
 
     count = 0
-    threshold = 4  # en pourcentage : 10 -> 10%
 
     for key, product in products.items():
         process = diff_impact[key]
@@ -34,11 +80,10 @@ def checks(products, processes):
                 process[impact] -= diff_impact[ingredient][impact] * amount
 
         for impact in process.keys():
-            if abs(process[impact]) > abs(processes[key][impact]) * threshold / 100:
+            if abs(process[impact]) > abs(processes[key][impact]) * THRESHOLD / 100:
                 count += 1
                 print(f"{key} (impact {impact}): {process[impact]}")
-
-    print(f"Total de {count} impacts qui ont une différence supérieure à {threshold}%")
+    return count
 
 
 def read_json(filename):
@@ -50,4 +95,20 @@ if __name__ == "__main__":
     products = read_json("products.json")
     processes = read_json("processes.json")
 
-    checks(products, processes)
+    print(">>> Check missing steps")
+    count = check_missing_steps(products)
+    print(f"{count} missing steps")
+
+    print()
+
+    print(">>> Check impact differences between export and Agribalyse_Synthese")
+    count = check_ciqual_impacts(processes, "../Agribalyse_Synthese.csv")
+    print(f"Total de {count} impacts qui ont une différence supérieure à {THRESHOLD}%")
+
+    print()
+
+    print(
+        ">>> Check impact differences at consumer between global impact and sum of impacts at supermarket"
+    )
+    count = check_impact_diff(products, processes)
+    print(f"Total de {count} impacts qui ont une différence supérieure à {THRESHOLD}%")
--- brightway/export_agb/impacts.py
+++ brightway/export_agb/impacts.py
@@ -63,3 +63,37 @@ impacts = {
         "comparative toxic unit for human (CTUh) ",
     ),
 }
+
+"""Correspondance between the impact name we chose in our export (the trigrams) and the name in the Agribalyse_synthese.
+
+The key is the Agribalyse_synthese name, the value is a tuple (trigram, multiplier),
+with `multiplier` the unit (eg E-06) in the Agribalyse_synthese name.
+
+"""
+impacts_to_synthese = {
+    # TODO: find/add the equivalent in `impacts`
+    # "Score unique EF (mPt/kg de produit)": "",
+    "Changement climatique (kg CO2 eq/kg de produit)": ("cch", 1),
+    "Appauvrissement de la couche d'ozone (E-06 kg CVC11 eq/kg de produit)": (
+        "ozd",
+        0.000001,
+    ),
+    "Rayonnements ionisants (kBq U-235 eq/kg de produit)": ("ior", 1),
+    "Formation photochimique d'ozone (E-03 kg NMVOC eq/kg de produit)": ("pco", 0.001),
+    "Particules (E-06 disease inc./kg de produit)": ("pma", 0.000001),
+    "Acidification terrestre et eaux douces (mol H+ eq/kg de produit)": ("acd", 1),
+    "Eutrophisation terreste (mol N eq/kg de produit)": ("tre", 1),
+    "Eutrophisation eaux douces (E-03 kg P eq/kg de produit)": ("fwe", 0.001),
+    "Eutrophisation marine (E-03 kg N eq/kg de produit)": ("swe", 0.001),
+    "Utilisation du sol (Pt/kg de produit)": ("ldu", 1),
+    "Écotoxicité pour écosystèmes aquatiques d'eau douce (CTUe/kg de produit)": (
+        "etf",
+        1,
+    ),
+    "Épuisement des ressources eau (m3 depriv./kg de produit)": ("wtu", 1),
+    "Épuisement des ressources énergétiques (MJ/kg de produit)": ("fru", 1),
+    "Épuisement des ressources minéraux (E-06 kg Sb eq/kg de produit)": (
+        "mru",
+        0.000001,
+    ),
+}
