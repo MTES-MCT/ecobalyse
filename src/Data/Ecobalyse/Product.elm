d9a1181cb772bc77586d11835ad855f3d076b3df
Mathieu Agopian <mathieu@agopian.info>
2022-05-20 16:42:48 +0200

    Refactor

--- src/Data/Ecobalyse/Product.elm
+++ src/Data/Ecobalyse/Product.elm
@@ -1,14 +1,10 @@
 module Data.Ecobalyse.Product exposing
     ( Product
-    , ProductDefinition
     , ProductName
     , Products
-    , Step
-    , decodeProductDefinition
     , decodeProducts
     , empty
     , findByName
-    , productFromDefinition
     , updateAmount
     )
 
@@ -66,13 +62,13 @@ type alias ProductDefinition =
     }
 
 
-insertIngredient : ProcessName -> Amount -> Impacts -> Step -> Step
-insertIngredient processName amount impacts step =
+insertProcess : ProcessName -> Amount -> Impacts -> Step -> Step
+insertProcess processName amount impacts step =
     Dict.insert processName (Process amount impacts) step
 
 
-stepFromProcesses : List Ingredient -> Processes -> Result String Step
-stepFromProcesses ingredients processes =
+stepFromIngredients : List Ingredient -> Processes -> Result String Step
+stepFromIngredients ingredients processes =
     ingredients
         |> List.foldl
             (\( processName, amount ) stepResult ->
@@ -81,7 +77,7 @@ stepFromProcesses ingredients processes =
                     impactsResult =
                         Process.findByName processName processes
                 in
-                Result.map2 (insertIngredient processName amount) impactsResult stepResult
+                Result.map2 (insertProcess processName amount) impactsResult stepResult
             )
             (Ok Dict.empty)
 
@@ -89,11 +85,11 @@ stepFromProcesses ingredients processes =
 productFromDefinition : Processes -> ProductDefinition -> Result String Product
 productFromDefinition processes { consumer, supermarket, distribution, packaging, plant } =
     Ok Product
-        |> RE.andMap (stepFromProcesses consumer processes)
-        |> RE.andMap (stepFromProcesses supermarket processes)
-        |> RE.andMap (stepFromProcesses distribution processes)
-        |> RE.andMap (stepFromProcesses packaging processes)
-        |> RE.andMap (stepFromProcesses plant processes)
+        |> RE.andMap (stepFromIngredients consumer processes)
+        |> RE.andMap (stepFromIngredients supermarket processes)
+        |> RE.andMap (stepFromIngredients distribution processes)
+        |> RE.andMap (stepFromIngredients packaging processes)
+        |> RE.andMap (stepFromIngredients plant processes)
 
 
 updateAmount : ProcessName -> Amount -> Step -> Step
@@ -140,47 +136,34 @@ insertProduct productName product products =
     Dict.insert productName product products
 
 
+productsFromDefinitions : Processes -> Dict ProductName ProductDefinition -> Result String Products
+productsFromDefinitions processes definitions =
+    definitions
+        |> Dict.foldl
+            (\productName productDefinition productsResult ->
+                let
+                    productResult : Result String Product
+                    productResult =
+                        productFromDefinition processes productDefinition
+                in
+                Result.map2 (insertProduct productName) productResult productsResult
+            )
+            (Ok Dict.empty)
+
+
 decodeProducts : Processes -> Decoder Products
 decodeProducts processes =
-    loggingDecoder
-        (Decode.dict decodeProductDefinition
-            |> Decode.andThen
-                (\dict ->
-                    dict
-                        |> Dict.foldl
-                            (\productName productDefinition productsResult ->
-                                let
-                                    productResult : Result String Product
-                                    productResult =
-                                        productFromDefinition processes productDefinition
-                                in
-                                Result.map2 (insertProduct productName) productResult productsResult
-                            )
-                            (Ok Dict.empty)
-                        |> (\result ->
-                                case result of
-                                    Ok products ->
-                                        Decode.succeed products
-
-                                    Err error ->
-                                        Decode.fail error
-                           )
-                )
-        )
-
-
-loggingDecoder : Decoder a -> Decoder a
-loggingDecoder realDecoder =
-    Decode.value
+    Decode.dict decodeProductDefinition
         |> Decode.andThen
-            (\value ->
-                case Decode.decodeValue realDecoder value of
-                    Ok decoded ->
-                        Decode.succeed decoded
+            (\definitions ->
+                definitions
+                    |> productsFromDefinitions processes
+                    |> (\result ->
+                            case result of
+                                Ok products ->
+                                    Decode.succeed products
 
-                    Err error ->
-                        error
-                            |> Decode.errorToString
-                            |> Debug.log "decode error"
-                            |> Decode.fail
+                                Err error ->
+                                    Decode.fail error
+                       )
             )
