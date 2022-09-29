4116824c7064732e722367403667fe7df0250169
Nicolas Perriault <nicolas@perriault.net>
2022-09-29 17:08:30 +0200

    Introduces new Recipe data module.

---
+++ tests/Data/Food/RecipeTest.elm
@@ -0,0 +1,15 @@
+module Data.Food.RecipeTest exposing (..)
+
+import Data.Food.Recipe as Recipe
+import Expect
+import Test exposing (..)
+import TestUtils exposing (asTest)
+
+
+suite : Test
+suite =
+    describe "Food.Recipe"
+        [ describe "compute" [ Test.todo "compute" ]
+        , describe "fromQuery" [ Test.todo "fromQuery" ]
+        , describe "toQuery" [ Test.todo "toQuery" ]
+        ]
