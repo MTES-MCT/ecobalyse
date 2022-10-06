684c0331b568b6412273c61f9daaba5758e5f711
Nicolas Perriault <nicolas@perriault.net>
2022-10-06 15:14:36 +0200

    Prepare server test suite to accept food API tests.

--- tests/server.spec.js
+++ tests/server.spec.js
@@ -8,6 +8,13 @@ async function makeRequest(path, query = []) {
   return await request(app).get(path).query(query.join("&"));
 }
 
+function expectFieldErrorMessage(response, field, message) {
+  expectStatus(response, 400);
+  expect("errors" in response.body).toEqual(true);
+  expect(field in response.body.errors).toEqual(true);
+  expect(response.body.errors[field]).toMatch(message);
+}
+
 describe("Web", () => {
   it("should render the homepage", async () => {
     const response = await request(app).get("/");
@@ -18,27 +25,6 @@ describe("Web", () => {
 });
 
 describe("API", () => {
-  const successQuery =
-    // Successful query params.
-    // Note: it's important to pass query string parameters as actual strings here,
-    // so we can test for actual qs parsing from the server.
-    [
-      "mass=0.17",
-      "product=tshirt",
-      "materials[]=coton;0.5",
-      "materials[]=acrylique;0.5",
-      "countryFabric=CN",
-      "countryDyeing=CN",
-      "countryMaking=CN",
-    ];
-
-  function expectFieldErrorMessage(response, field, message) {
-    expectStatus(response, 400);
-    expect("errors" in response.body).toEqual(true);
-    expect(field in response.body.errors).toEqual(true);
-    expect(response.body.errors[field]).toMatch(message);
-  }
-
   describe("Not found", () => {
     it("should render a 404 response", async () => {
       const response = await request(app).get("/xxx");
@@ -47,208 +33,225 @@ describe("API", () => {
     });
   });
 
-  describe("/api", () => {
-    it("should render the OpenAPI documentation", async () => {
-      const response = await request(app).get("/api");
+  describe("Common", () => {
+    describe("/api", () => {
+      it("should render the OpenAPI documentation", async () => {
+        const response = await request(app).get("/api");
 
-      expectStatus(response, 200);
-      expect(response.body.openapi).toEqual("3.0.1");
-      expect(response.body.info.title).toEqual("API Ecobalyse");
+        expectStatus(response, 200);
+        expect(response.body.openapi).toEqual("3.0.1");
+        expect(response.body.info.title).toEqual("API Ecobalyse");
+      });
     });
-  });
 
-  describe("/countries", () => {
-    it("should render with countries list", async () => {
-      const response = await request(app).get("/api/countries");
+    describe("/countries", () => {
+      it("should render with countries list", async () => {
+        const response = await request(app).get("/api/countries");
 
-      expectStatus(response, 200);
-      expect(response.body).toContainObject({ code: "FR", name: "France" });
-    });
-  });
-
-  describe("/materials", () => {
-    it("should render with materials list", async () => {
-      const response = await request(app).get("/api/materials");
-
-      expectStatus(response, 200);
-      expect(response.body).toContainObject({
-        id: "coton",
-        name: "Fil de coton conventionnel, inventaire partiellement agrégé",
+        expectStatus(response, 200);
+        expect(response.body).toContainObject({ code: "FR", name: "France" });
       });
     });
   });
 
-  describe("/products", () => {
-    it("should render with products list", async () => {
-      const response = await request(app).get("/api/products");
+  describe("Textile", () => {
+    const successQuery =
+      // Successful query params.
+      // Note: it's important to pass query string parameters as actual strings here,
+      // so we can test for actual qs parsing from the server.
+      [
+        "mass=0.17",
+        "product=tshirt",
+        "materials[]=coton;0.5",
+        "materials[]=acrylique;0.5",
+        "countryFabric=CN",
+        "countryDyeing=CN",
+        "countryMaking=CN",
+      ];
+    describe("/materials", () => {
+      it("should render with materials list", async () => {
+        const response = await request(app).get("/api/materials");
 
-      expectStatus(response, 200);
-      expect(response.body).toContainObject({ id: "tshirt", name: "T-shirt" });
-    });
-  });
-
-  describe("/api/simulator", () => {
-    it("should accept a valid query", async () => {
-      const response = await makeRequest("/api/simulator", successQuery);
-
-      expectStatus(response, 200);
-      expect(response.body.impacts.cch).toBeGreaterThan(0);
-    });
-
-    it("should validate the mass param", async () => {
-      expectFieldErrorMessage(
-        await makeRequest("/api/simulator", ["mass=-1"]),
-        "mass",
-        /supérieure ou égale à zéro/,
-      );
-    });
-
-    it("should validate the materials param", async () => {
-      expectFieldErrorMessage(
-        await makeRequest("/api/simulator", ["materials[]=xxx;1"]),
-        "materials",
-        /Matière non trouvée id=xxx/,
-      );
-    });
-
-    it("should validate the product param", async () => {
-      expectFieldErrorMessage(
-        await makeRequest("/api/simulator", ["product=xxx"]),
-        "product",
-        /Produit non trouvé id=xxx/,
-      );
-    });
-
-    it("should validate the country params are present", async () => {
-      expectFieldErrorMessage(
-        await makeRequest("/api/simulator", ["countryFabric=FR,countryDyeing=FR"]),
-        "countryMaking",
-        /Code pays manquant/,
-      );
-    });
-
-    it("should validate the countryFabric param (invalid code)", async () => {
-      expectFieldErrorMessage(
-        await makeRequest("/api/simulator", ["countryFabric=XX"]),
-        "countryFabric",
-        /Code pays invalide: XX/,
-      );
-    });
-
-    it("should validate the countryDyeing param (invalid code)", async () => {
-      expectFieldErrorMessage(
-        await makeRequest("/api/simulator", ["countryDyeing=XX"]),
-        "countryDyeing",
-        /Code pays invalide: XX/,
-      );
-    });
-
-    it("should validate the countryMaking param (invalid code)", async () => {
-      expectFieldErrorMessage(
-        await makeRequest("/api/simulator", ["countryMaking=XX"]),
-        "countryMaking",
-        /Code pays invalide: XX/,
-      );
-    });
-
-    it("should validate the disabledSteps param", async () => {
-      expectFieldErrorMessage(
-        await makeRequest("/api/simulator", ["disabledSteps=xxx"]),
-        "disabledSteps",
-        /Code étape inconnu: xxx/i,
-      );
-    });
-
-    it("should perform a simulation featuring 20 impacts", async () => {
-      const response = await makeRequest("/api/simulator/", successQuery);
-
-      expectStatus(response, 200);
-      expect(Object.keys(response.body.impacts)).toHaveLength(20);
-    });
-
-    it("should validate the airTransportRatio param", async () => {
-      expectFieldErrorMessage(
-        await makeRequest("/api/simulator", ["airTransportRatio=2"]),
-        "airTransportRatio",
-        /doit être compris entre/,
-      );
-    });
-
-    it("should validate the makingWaste param", async () => {
-      expectFieldErrorMessage(
-        await makeRequest("/api/simulator", ["makingWaste=0.9"]),
-        "makingWaste",
-        /doit être compris entre/,
-      );
-    });
-
-    it("should validate the picking param", async () => {
-      expectFieldErrorMessage(
-        await makeRequest("/api/simulator", ["picking=10"]),
-        "picking",
-        /doit être compris entre/,
-      );
-    });
-
-    it("should validate the surfaceMass param", async () => {
-      expectFieldErrorMessage(
-        await makeRequest("/api/simulator", ["surfaceMass=10"]),
-        "surfaceMass",
-        /doit être compris entre/,
-      );
-    });
-
-    it("should validate the disabledFading param", async () => {
-      expectFieldErrorMessage(
-        await makeRequest("/api/simulator", ["disabledFading=untrue"]),
-        "disabledFading",
-        /ne peut être que true ou false/,
-      );
-    });
-  });
-
-  describe("/api/simulator/fwe", () => {
-    it("should accept a valid query", async () => {
-      const response = await makeRequest("/api/simulator/fwe", successQuery);
-
-      expectStatus(response, 200);
-      expect(response.body.impacts.fwe).toBeGreaterThan(0);
-    });
-  });
-
-  describe("/api/simulator/detailed", () => {
-    it("should accept a valid query", async () => {
-      const response = await makeRequest("/api/simulator/detailed", successQuery);
-
-      expectStatus(response, 200);
-      expect(response.body.lifeCycle).toHaveLength(8);
-    });
-  });
-});
-
-describe("End to end simulations", () => {
-  const e2e = JSON.parse(fs.readFileSync(`${__dirname}/e2e.json`).toString());
-
-  function toComparable(impacts) {
-    return Object.keys(impacts)
-      .sort()
-      .map((trigram) => {
-        return { [trigram]: impacts[trigram] };
+        expectStatus(response, 200);
+        expect(response.body).toContainObject({
+          id: "coton",
+          name: "Fil de coton conventionnel, inventaire partiellement agrégé",
+        });
       });
-  }
-
-  for (const { name, query, impacts } of e2e) {
-    it(name, async () => {
-      const response = await makeRequest("/api/simulator", query);
-      expectStatus(response, 200);
-      e2eOutput.push({
-        name,
-        query,
-        impacts: response.body.impacts,
-      });
-      expect(toComparable(response.body.impacts)).toEqual(toComparable(impacts));
     });
-  }
+
+    describe("/products", () => {
+      it("should render with products list", async () => {
+        const response = await request(app).get("/api/products");
+
+        expectStatus(response, 200);
+        expect(response.body).toContainObject({ id: "tshirt", name: "T-shirt" });
+      });
+    });
+
+    describe("/simulator", () => {
+      it("should accept a valid query", async () => {
+        const response = await makeRequest("/api/simulator", successQuery);
+
+        expectStatus(response, 200);
+        expect(response.body.impacts.cch).toBeGreaterThan(0);
+      });
+
+      it("should validate the mass param", async () => {
+        expectFieldErrorMessage(
+          await makeRequest("/api/simulator", ["mass=-1"]),
+          "mass",
+          /supérieure ou égale à zéro/,
+        );
+      });
+
+      it("should validate the materials param", async () => {
+        expectFieldErrorMessage(
+          await makeRequest("/api/simulator", ["materials[]=xxx;1"]),
+          "materials",
+          /Matière non trouvée id=xxx/,
+        );
+      });
+
+      it("should validate the product param", async () => {
+        expectFieldErrorMessage(
+          await makeRequest("/api/simulator", ["product=xxx"]),
+          "product",
+          /Produit non trouvé id=xxx/,
+        );
+      });
+
+      it("should validate the country params are present", async () => {
+        expectFieldErrorMessage(
+          await makeRequest("/api/simulator", ["countryFabric=FR,countryDyeing=FR"]),
+          "countryMaking",
+          /Code pays manquant/,
+        );
+      });
+
+      it("should validate the countryFabric param (invalid code)", async () => {
+        expectFieldErrorMessage(
+          await makeRequest("/api/simulator", ["countryFabric=XX"]),
+          "countryFabric",
+          /Code pays invalide: XX/,
+        );
+      });
+
+      it("should validate the countryDyeing param (invalid code)", async () => {
+        expectFieldErrorMessage(
+          await makeRequest("/api/simulator", ["countryDyeing=XX"]),
+          "countryDyeing",
+          /Code pays invalide: XX/,
+        );
+      });
+
+      it("should validate the countryMaking param (invalid code)", async () => {
+        expectFieldErrorMessage(
+          await makeRequest("/api/simulator", ["countryMaking=XX"]),
+          "countryMaking",
+          /Code pays invalide: XX/,
+        );
+      });
+
+      it("should validate the disabledSteps param", async () => {
+        expectFieldErrorMessage(
+          await makeRequest("/api/simulator", ["disabledSteps=xxx"]),
+          "disabledSteps",
+          /Code étape inconnu: xxx/i,
+        );
+      });
+
+      it("should perform a simulation featuring 20 impacts", async () => {
+        const response = await makeRequest("/api/simulator/", successQuery);
+
+        expectStatus(response, 200);
+        expect(Object.keys(response.body.impacts)).toHaveLength(20);
+      });
+
+      it("should validate the airTransportRatio param", async () => {
+        expectFieldErrorMessage(
+          await makeRequest("/api/simulator", ["airTransportRatio=2"]),
+          "airTransportRatio",
+          /doit être compris entre/,
+        );
+      });
+
+      it("should validate the makingWaste param", async () => {
+        expectFieldErrorMessage(
+          await makeRequest("/api/simulator", ["makingWaste=0.9"]),
+          "makingWaste",
+          /doit être compris entre/,
+        );
+      });
+
+      it("should validate the picking param", async () => {
+        expectFieldErrorMessage(
+          await makeRequest("/api/simulator", ["picking=10"]),
+          "picking",
+          /doit être compris entre/,
+        );
+      });
+
+      it("should validate the surfaceMass param", async () => {
+        expectFieldErrorMessage(
+          await makeRequest("/api/simulator", ["surfaceMass=10"]),
+          "surfaceMass",
+          /doit être compris entre/,
+        );
+      });
+
+      it("should validate the disabledFading param", async () => {
+        expectFieldErrorMessage(
+          await makeRequest("/api/simulator", ["disabledFading=untrue"]),
+          "disabledFading",
+          /ne peut être que true ou false/,
+        );
+      });
+    });
+
+    describe("/simulator/fwe", () => {
+      it("should accept a valid query", async () => {
+        const response = await makeRequest("/api/simulator/fwe", successQuery);
+
+        expectStatus(response, 200);
+        expect(response.body.impacts.fwe).toBeGreaterThan(0);
+      });
+    });
+
+    describe("/simulator/detailed", () => {
+      it("should accept a valid query", async () => {
+        const response = await makeRequest("/api/simulator/detailed", successQuery);
+
+        expectStatus(response, 200);
+        expect(response.body.lifeCycle).toHaveLength(8);
+      });
+    });
+
+    describe("End to end textile simulations", () => {
+      const e2e = JSON.parse(fs.readFileSync(`${__dirname}/e2e.json`).toString());
+
+      function toComparable(impacts) {
+        return Object.keys(impacts)
+          .sort()
+          .map((trigram) => {
+            return { [trigram]: impacts[trigram] };
+          });
+      }
+
+      for (const { name, query, impacts } of e2e) {
+        it(name, async () => {
+          const response = await makeRequest("/api/simulator", query);
+          expectStatus(response, 200);
+          e2eOutput.push({
+            name,
+            query,
+            impacts: response.body.impacts,
+          });
+          expect(toComparable(response.body.impacts)).toEqual(toComparable(impacts));
+        });
+      }
+    });
+  });
 });
 
 afterAll(() => {
