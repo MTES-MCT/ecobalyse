bcb65e3be58d2d6557a1a4950d7dd7473f4f49e0
Mathieu Agopian <mathieu@agopian.info>
2022-10-14 11:48:55 +0200

    Remove all references and code related to the long unused impacts ccb, ccf and ccl

--- tests/server.spec.js
+++ tests/server.spec.js
@@ -169,11 +169,11 @@ describe("API", () => {
         );
       });
 
-      it("should perform a simulation featuring 20 impacts", async () => {
+      it("should perform a simulation featuring 17 impacts", async () => {
         const response = await makeRequest("/api/simulator/", successQuery);
 
         expectStatus(response, 200);
-        expect(Object.keys(response.body.impacts)).toHaveLength(20);
+        expect(Object.keys(response.body.impacts)).toHaveLength(17);
       });
 
       it("should validate the airTransportRatio param", async () => {
