f5b40d706467ced600554cb72dfde605b254d8a7
Nicolas Perriault <nicolas@perriault.net>
2022-10-20 17:20:39 +0200

    Add summary sidebar, revamp layout.

--- src/Page/Food/Builder.elm
+++ src/Page/Food/Builder.elm
@@ -8,8 +8,9 @@ module Page.Food.Builder exposing
 
 import Data.Food.Db as FoodDb
 import Data.Food.Recipe as Recipe
-import Data.Impact as Impact
+import Data.Impact as Impact exposing (Impacts)
 import Data.Session exposing (Session)
+import Data.Unit as Unit
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
@@ -19,19 +20,26 @@ import RemoteData exposing (WebData)
 import Request.Common
 import Request.Food.Db as RequestDb
 import Route
+import Views.Alert as Alert
+import Views.Component.Summary as SummaryComp
 import Views.Container as Container
+import Views.Format as Format
+import Views.Impact as ImpactView
 import Views.Spinner as Spinner
 
 
 type alias Model =
     { dbState : WebData FoodDb.Db
     , query : Recipe.Query
+    , impact : Impact.Trigram
     }
 
 
 type Msg
     = DbLoaded (WebData FoodDb.Db)
     | LoadQuery Recipe.Query
+    | NoOp
+    | SwitchImpact Impact.Trigram
 
 
 init : Session -> ( Model, Session, Cmd Msg )
@@ -40,6 +48,7 @@ init session =
         model =
             { dbState = RemoteData.Loading
             , query = Recipe.empty
+            , impact = Impact.defaultTrigram
             }
     in
     if FoodDb.isEmpty session.foodDb then
@@ -62,24 +71,22 @@ update : Session -> Msg -> Model -> ( Model, Session, Cmd Msg )
 update session msg model =
     case msg of
         DbLoaded dbState ->
-            ( { model | dbState = dbState }
-            , session
-            , Cmd.none
-            )
+            ( { model | dbState = dbState }, session, Cmd.none )
+
+        NoOp ->
+            ( model, session, Cmd.none )
 
         LoadQuery query ->
-            ( { model | query = query }
-            , session
-            , Cmd.none
-            )
+            ( { model | query = query }, session, Cmd.none )
+
+        SwitchImpact impact ->
+            ( { model | impact = impact }, session, Cmd.none )
 
 
 menuView : Html Msg
 menuView =
     div [ class "d-flex gap-2" ]
-        [ a [ class "btn btn-primary", Route.href Route.FoodExplore ]
-            [ text "«\u{00A0}Explorateur de recettes" ]
-        , button [ class "btn btn-outline-primary", onClick (LoadQuery Recipe.empty) ]
+        [ button [ class "btn btn-outline-primary", onClick (LoadQuery Recipe.empty) ]
             [ text "Empty recipe" ]
         , button [ class "btn btn-outline-primary", onClick (LoadQuery Recipe.tunaPizza) ]
             [ text "Tuna Pizza" ]
@@ -112,10 +119,69 @@ debugQueryView foodDb query =
         ]
 
 
+viewSidebar : FoodDb.Db -> Model -> Impacts -> Html Msg
+viewSidebar foodDb model impacts =
+    let
+        definition =
+            foodDb.impacts
+                |> Impact.getDefinition model.impact
+                |> Result.withDefault Impact.invalid
+    in
+    div
+        [ class "d-flex flex-column gap-3 mb-3 sticky-md-top"
+        , style "top" "7px"
+        ]
+        [ ImpactView.impactSelector
+            { impacts = foodDb.impacts
+            , selectedImpact = model.impact
+            , switchImpact = SwitchImpact
+
+            -- We don't use the following two configs
+            , selectedFunctionalUnit = Unit.PerItem
+            , switchFunctionalUnit = always NoOp
+            , scope = Impact.Food
+            }
+        , SummaryComp.view
+            { header = []
+            , body =
+                [ div [ class "d-flex flex-column m-auto gap-1 px-2" ]
+                    [ div [ class "display-4 lh-1 text-center text-nowrap" ]
+                        [ impacts
+                            |> Impact.getImpact model.impact
+                            |> Unit.impactToFloat
+                            |> Format.formatImpactFloat definition 2
+                        ]
+                    ]
+                ]
+            , footer = []
+            }
+        , a [ class "btn btn-primary", Route.href Route.FoodExplore ]
+            [ text "Explorateur de recettes" ]
+        ]
+
+
 mainView : FoodDb.Db -> Model -> Html Msg
 mainView foodDb model =
     div []
-        [ debugQueryView foodDb model.query
+        [ div [ class "row gap-3 gap-lg-0" ]
+            [ div [ class "col-lg-4 order-lg-2 d-flex flex-column gap-3" ]
+                [ case Recipe.compute foodDb model.query of
+                    Ok impacts ->
+                        viewSidebar foodDb model impacts
+
+                    Err error ->
+                        Alert.simple
+                            { level = Alert.Danger
+                            , content = [ text error ]
+                            , title = Nothing
+                            , close = Nothing
+                            }
+                ]
+            , div [ class "col-lg-8 order-lg-1 d-flex flex-column" ]
+                [ menuView
+                , debugQueryView foodDb model.query
+                ]
+            ]
         ]
 
 
@@ -123,9 +189,7 @@ view : Session -> Model -> ( String, List (Html Msg) )
 view _ model =
     ( "Constructeur de recette"
     , [ Container.centered [ class "pb-3" ]
-            [ h1 [ class "h2" ] [ text "TODO" ]
-            , menuView
-            , case model.dbState of
+            [ case model.dbState of
                 RemoteData.Success foodDb ->
                     mainView foodDb model
 
