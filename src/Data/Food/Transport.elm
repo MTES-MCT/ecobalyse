f602bd331d6fd006934c38fb6e9822b6be57f11a
Mathieu Agopian <mathieu@agopian.info>
2022-10-20 16:02:31 +0200

    Order alphabetically

--- src/Data/Food/Transport.elm
+++ src/Data/Food/Transport.elm
@@ -9,10 +9,17 @@ type alias Transport =
     Quantity.Quantity Float (Quantity.Product Mass.Kilograms Length.Meters)
 
 
-tonKilometers : Float -> Transport
-tonKilometers amount =
-    -- Could equally be written `Quantity.product  (Mass.metricTons 1) (Length.kilometers amount)
-    Quantity.product (Mass.metricTons amount) (Length.kilometers 1)
+getLength : Mass -> Transport -> Length
+getLength mass transport =
+    Quantity.over mass transport
+
+
+inKgKms : Transport -> Float
+inKgKms transport =
+    -- Transport is stored in kg.m, we want it in kg.km
+    inTonKilometers transport
+        -- 1 km == 1000m
+        * 1000
 
 
 inTonKilometers : Transport -> Float
@@ -25,14 +32,7 @@ inTonKilometers (Quantity.Quantity transport) =
         / 1000
 
 
-inKgKms : Transport -> Float
-inKgKms transport =
-    -- Transport is stored in kg.m, we want it in kg.km
-    inTonKilometers transport
-        -- 1 km == 1000m
-        * 1000
-
-
-getLength : Mass -> Transport -> Length
-getLength mass transport =
-    Quantity.over mass transport
+tonKilometers : Float -> Transport
+tonKilometers amount =
+    -- Could equally be written `Quantity.product  (Mass.metricTons 1) (Length.kilometers amount)
+    Quantity.product (Mass.metricTons amount) (Length.kilometers 1)
