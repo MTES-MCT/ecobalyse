5bdf82f0faefff6e7fc2dc430ce9e35733a6033f
Nicolas Perriault <nicolas@perriault.net>
2022-10-24 16:59:43 +0200

    Improve ingredient list layout.

---
+++ src/Views/Component/MassInput.elm
@@ -0,0 +1,45 @@
+module Views.Component.MassInput exposing (view)
+
+import Html exposing (..)
+import Html.Attributes as Attr exposing (..)
+import Html.Events exposing (..)
+import Mass exposing (Mass)
+
+
+type alias Config msg =
+    { mass : Mass
+    , onChange : Maybe Mass -> msg
+    }
+
+
+view : Config msg -> Html msg
+view { mass, onChange } =
+    div [ class "input-group input-group" ]
+        [ input
+            [ class "form-control text-end incdec-arrows-left"
+            , type_ "number"
+            , step "1"
+            , mass
+                |> Mass.inGrams
+                |> round
+                |> String.fromInt
+                |> value
+            , title "Quantité en grammes"
+            , onInput <|
+                \str ->
+                    onChange
+                        (if str == "" then
+                            Nothing
+
+                         else
+                            str
+                                |> String.toFloat
+                                |> Maybe.map Mass.grams
+                        )
+            , Attr.min "0"
+            ]
+            []
+        , span [ class "input-group-text", title "grammes" ]
+            [ text "g"
+            ]
+        ]
