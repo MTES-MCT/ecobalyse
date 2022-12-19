1886b4b60e06933df04e9143f0e5981e0dd6d593
Nicolas Perriault <nicolas@perriault.net>
2022-12-19 11:20:38 +0100

    Load common db properties to builder and explorer dbs.

--- src/Data/Food/Builder/Db.elm
+++ src/Data/Food/Builder/Db.elm
@@ -5,26 +5,35 @@ module Data.Food.Builder.Db exposing
     , isEmpty
     )
 
+import Data.Country exposing (Country)
 import Data.Food.Ingredient as Ingredient exposing (Ingredient)
 import Data.Food.Process as Process exposing (Process)
 import Data.Impact as Impact
+import Data.Textile.Db as TextileDb
+import Data.Transport as Transport
 import Json.Decode as Decode
 
 
 type alias Db =
-    { impacts : List Impact.Definition
+    { -- Common datasources
+      countries : List Country
+    , impacts : List Impact.Definition
+    , transports : Transport.Distances
 
-    ---- builder Processes are straightforward imports of public/data/food/processes/builder.json
+    -- Builder specific datasources
+    -- Builder Processes are straightforward imports of public/data/food/processes/builder.json
     , processes : List Process
 
-    ---- Ingredients are imported from public/data/food/ingredients.json
+    -- Ingredients are imported from public/data/food/ingredients.json
     , ingredients : List Ingredient
     }
 
 
 empty : Db
 empty =
-    { impacts = []
+    { countries = []
+    , impacts = []
+    , transports = Transport.emptyDistances
     , processes = []
     , ingredients = []
     }
@@ -35,14 +44,14 @@ isEmpty db =
     db == empty
 
 
-buildFromJson : List Impact.Definition -> String -> String -> Result String Db
-buildFromJson impacts builderProcessesJson ingredientsJson =
+buildFromJson : TextileDb.Db -> String -> String -> Result String Db
+buildFromJson { countries, transports, impacts } builderProcessesJson ingredientsJson =
     builderProcessesJson
         |> Decode.decodeString (Process.decodeList impacts)
         |> Result.andThen
             (\processes ->
                 ingredientsJson
                     |> Decode.decodeString (Ingredient.decodeIngredients processes)
-                    |> Result.map (Db impacts processes)
+                    |> Result.map (Db countries impacts transports processes)
             )
         |> Result.mapError Decode.errorToString
