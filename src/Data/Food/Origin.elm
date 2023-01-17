fc1a8340941eca715c54b6836d939ed4614cbd73
Nicolas Perriault <nicolas@perriault.net>
2023-01-17 16:55:45 +0100

    Add a new Data.Food.Origin type, data and utils.

---
+++ src/Data/Food/Origin.elm
@@ -0,0 +1,64 @@
+module Data.Food.Origin exposing
+    ( Origin(..)
+    , decode
+    , fromString
+    , toLabel
+    , toString
+    )
+
+import Json.Decode as Decode exposing (Decoder)
+import Json.Decode.Extra as DE
+
+
+type Origin
+    = France
+    | EuropeAndMaghreb
+    | OutOfEuropeAndMaghreb
+
+
+decode : Decoder Origin
+decode =
+    Decode.string
+        |> Decode.andThen (fromString >> DE.fromResult)
+
+
+fromString : String -> Result String Origin
+fromString string =
+    case string of
+        "France" ->
+            Ok France
+
+        "EuropeAndMaghreb" ->
+            Ok EuropeAndMaghreb
+
+        "OutOfEuropeAndMaghreb" ->
+            Ok OutOfEuropeAndMaghreb
+
+        _ ->
+            Err <| "Origine géographique inconnue : " ++ string
+
+
+toLabel : Origin -> String
+toLabel origin =
+    case origin of
+        France ->
+            "France"
+
+        EuropeAndMaghreb ->
+            "Europe et Maghreb"
+
+        OutOfEuropeAndMaghreb ->
+            "Hors Europe et Maghreb"
+
+
+toString : Origin -> String
+toString origin =
+    case origin of
+        France ->
+            "France"
+
+        EuropeAndMaghreb ->
+            "EuropeAndMaghreb"
+
+        OutOfEuropeAndMaghreb ->
+            "OutOfEuropeAndMaghreb"
