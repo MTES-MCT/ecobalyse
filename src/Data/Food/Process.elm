abb4fb172aeab6373c8ea6a5611f49eadc0c6351
Mathieu Agopian <mathieu@agopian.info>
2023-03-09 11:52:59 +0100

    Add cooling transport processes

--- src/Data/Food/Process.elm
+++ src/Data/Food/Process.elm
@@ -67,6 +67,8 @@ type alias WellKnown =
     { lorryTransport : Process
     , boatTransport : Process
     , planeTransport : Process
+    , lorryCoolingTransport : Process
+    , boatCoolingTransport : Process
     , water : Process
     , lowVoltageElectricity : Process
     , domesticGasHeat : Process
@@ -243,6 +245,8 @@ loadWellKnown processes =
         |> resolve "lorry"
         |> resolve "boat"
         |> resolve "plane"
+        |> resolve "lorry-cooling"
+        |> resolve "boat-cooling"
         |> resolve "tapwater"
         |> resolve "low-voltage-electricity"
         |> resolve "domestic-gas-heat"
