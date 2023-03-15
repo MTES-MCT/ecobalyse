909b89473fe2d5befb58826d1e211e7d1b13f1c6
Mathieu Agopian <mathieu@agopian.info>
2023-03-15 19:53:23 +0100

    Don't rename the processes when updating their impacts with agb3.1.1

--- data/food/manual_update_agb3.1.1/update_impacts_311.py
+++ data/food/manual_update_agb3.1.1/update_impacts_311.py
@@ -1,11 +1,10 @@
-"""Update builder.json impacts with impacts from agb3.1.1.csv (manually exported from simapro)
-example : python update_impacts_3.1.1.py"""
+"""Update builder.json impacts with impacts from agb3.1.1.csv
+(manually exported from simapro) example : python update_impacts_3.1.1.py
 
-import copy
+"""
+
 import pandas as pd
 import json
-import hashlib
-import uuid
 from food.export_agb import export_builder
 
 # INPUT
@@ -14,7 +13,6 @@ INGREDIENTS_BASE = "../export_agb/ingredients_base.json"
 IMPACTS = "../../../public/data/impacts.json"
 # OUTPUT
 PROCESSES = "../../../public/data/food/processes/builder.json"
-
 
 
 if __name__ == "__main__":
@@ -55,53 +53,57 @@ if __name__ == "__main__":
     }
 
     df.rename(columns=rename_columns, inplace=True)
-    df.drop(columns = [''], inplace=True)
-    df.set_index("name", inplace = True, verify_integrity=True)
+    df.drop(columns=[''], inplace=True)
+    df.set_index("name", inplace=True, verify_integrity=True)
     agb311_dict = df.to_dict('index')
 
-    # processes name changed between agb3.1 and agb3.11 
+    # processes name changed between agb3.1 and agb3.1.1
     # so we have to update the names
 
-    name_change_df =pd.read_csv("name_change.csv")
-    name_change_df.set_index("name3.1", inplace = True, verify_integrity=True)
+    name_change_df = pd.read_csv("name_change.csv")
+    name_change_df.set_index("name3.1", inplace=True, verify_integrity=True)
     name_change_dict = name_change_df.to_dict('index')
-    name_change_dict = {k:v["name3.1.1"] for (k,v) in name_change_dict.items()}
+    name_change_dict = {k: v["name3.1.1"]
+                        for (k, v) in name_change_dict.items()}
 
     with open(PROCESSES) as json_file:
         processes_list = json.load(json_file)
 
     with open(IMPACTS, "r") as f:
         impacts_ecobalyse = json.load(f)
-        
-    for proc in processes_list:
-        # replace old name with new names is there is one    
-        if proc["name"] in name_change_dict.keys():
-            print(f"Process replacement : AGB3.1 {proc['name']} replaced by AGB3.1.1 {name_change_dict[proc['name']]}")
-            proc["name"] = name_change_dict[proc["name"]]
 
-        # update builder.json with agb3.1.1 
+    for proc in processes_list:
+        # replace old name with new names if there is one
+        process_name = proc["name"]
+        if proc["name"] in name_change_dict.keys():
+            process_name = name_change_dict[proc["name"]]
+            print(f"Name change: \"{proc['name']}\" -> \"{process_name}\"")
+
+        # update builder.json with agb3.1.1
         try:
-            for impact,value in agb311_dict[proc["name"]].items():
+            for impact, value in agb311_dict[process_name].items():
                 proc["impacts"][impact] = value
 
-            proc["impacts"]["pef"] = export_builder.compute_pef(impacts_ecobalyse, proc["impacts"])
+            proc["impacts"]["pef"] = export_builder.compute_pef(
+                impacts_ecobalyse, proc["impacts"])
         except KeyError as e:
-            print(f"KeyError, normal for complex ingredients 'constructed by ecobalyse', which are computed in the next step : {e}")
+            print(f"KeyError, normal for complex ingredients 'constructed by "
+                  f"ecobalyse', which are computed in the next step : {e}")
 
-    # Compute impacts for complex ingredients from other ingredients impacts 
+    processes_dic = {p["name"]: p for p in processes_list}
 
-    # Parse the ingredients_base.json, which contains complex ingredients to add/compute
+    # Compute impacts for complex ingredients from other ingredients impacts
+    # Parse the ingredients_base.json, which contains complex ingredients to
+    # add/compute.
     with open(INGREDIENTS_BASE, "r") as f:
         ingredients_base = json.load(f)
 
-    processes_dic = {p["name"]:p for p in processes_list}
+    (_, complex_processes) = export_builder.compute_ingredient_list(
+        processes_dic, ingredients_base)
 
-    (ingredient_list, complex_processes) = export_builder.compute_ingredient_list(
-        processes_dic, ingredients_base
-    )
     # update processes_dic with complex impacts
     for complex_p in complex_processes:
-        processes_dic[complex_p["name"]] = complex_p 
-    
+        processes_dic[complex_p["name"]] = complex_p
+
     processes_list = list(processes_dic.values())
     export_builder.export_json(processes_list, PROCESSES)
