36e99f45b0b739115e51df186ab6358b70e495d0
Christophe Combelles <ccomb@free.fr>
2023-04-18 19:35:17 +0200

    Refactored the explore notebook

--- data/notebooks/explore.py
+++ data/notebooks/explore.py
@@ -3,12 +3,12 @@ This file is used in the `explore` Jupyter Notebook
 """
 from IPython.core.display import display, Markdown
 from bw2data.utils import get_activity
-from ipywidgets import interact
 import bw2calc
 import bw2data
 import bw2io
 import ipywidgets
 import os
+import pandas
 
 bw2data.projects.set_current("Ecobalyse")
 bw2io.bw2setup()
@@ -22,48 +22,60 @@ else:
 
 databases = list(bw2data.databases.keys())
 # widgets
-database = ipywidgets.Dropdown(value=databases[0], options=databases)
-search = ipywidgets.Text(value="", placeholder="Search string", description="SEARCH")
-methods = sorted({method[0] for method in bw2data.methods})
-method = ipywidgets.Dropdown(value=methods[11], options=methods)
-limit = ipywidgets.BoundedIntText(value=10, min=0, step=1, description="RESULTS")
-activity = ipywidgets.Dropdown(
+f_database = ipywidgets.Dropdown(
+    value=databases[0], options=databases, description="DATABASE"
+)
+f_search = ipywidgets.Text(value="", placeholder="Search string", description="SEARCH")
+METHODS = sorted({m[0] for m in bw2data.methods})
+f_method = ipywidgets.Dropdown(value=METHODS[11], options=METHODS, description="METHOD")
+f_limit = ipywidgets.BoundedIntText(value=10, min=0, step=1, description="LIMIT")
+f_activity = ipywidgets.Dropdown(
     options=[""]
-    + list(bw2data.Database(database.value).search(search.value, limit=limit.value)),
+    + list(
+        bw2data.Database(f_database.value).search(f_search.value, limit=f_limit.value)
+    ),
     description="ACTIVITY",
 )
+f_results = ipywidgets.Output(value="Résultat")
+f_details = ipywidgets.Output(value="Détails")
 
 display(Markdown("# Search in the database :"))
 
 
-def search_activity(DATABASE=database, SEARCH="", LIMIT=limit):
-    database = bw2data.Database(DATABASE)
-    activities = list(database.search(SEARCH, limit=LIMIT))
-    if len(activities) == 0:
-        display(Markdown(("No result")))
+@f_results.capture()
+def search_activity(change):
+    f_details.clear_output()
+    database = change.new if change.owner is f_database else f_database.value
+    search = change.new if change.owner is f_search else f_search.value
+    limit = change.new if change.owner is f_limit else f_limit.value
+    f_activity.value = None
+    db = bw2data.Database(database)
+    results = list(db.search(search, limit=limit))
+    if len(results) == 0:
+        f_results.clear_output()
+        display(Markdown("(No results)"))
         return
-    results = [""] + list(bw2data.Database(DATABASE).search(SEARCH, limit=LIMIT))
-    act = activity
-    act.options = results
-    display(Markdown("# Results"))
-    for act in results:
-        print(f"{act}")
+    f_activity.options = [""] + results
+    f_results.clear_output()
+    display(Markdown("## Results"))
+    with pandas.option_context("display.max_colwidth", None):
+        display(pandas.DataFrame(results, columns=["name", "code", "location"]))
     display(Markdown("---"))
 
 
-interact(search_activity, DATABASE=database, SEARCH="", LIMIT=limit)
+@f_details.capture()
+def show_activity(change):
+    activity = change.new if change.owner is f_activity else f_activity.value
+    method = change.new if change.owner is f_method else f_method.value
+    f_details.clear_output()
 
-activity.observe(search_activity, "value")
-
-
-def show_activity(METHOD=method, ACTIVITY=activity):
     # IMPACTS
-    if not ACTIVITY:
+    if not activity or not method:
         return
-    lca = bw2calc.LCA({ACTIVITY: 1})
+    lca = bw2calc.LCA({activity: 1})
     lca.lci()
-    display(Markdown(f"# IMPACTS for {ACTIVITY}"))
-    for method in [method for method in bw2data.methods if method[0] == METHOD]:
+    display(Markdown(f"# IMPACTS for {activity}"))
+    for method in [m for m in bw2data.methods if m[0] == method]:
         lca.switch_method(method)
         lca.lcia()
         print(
@@ -72,8 +84,8 @@ def show_activity(METHOD=method, ACTIVITY=activity):
     display(Markdown("---"))
 
     # ACTIVITY DATA
-    display(Markdown(f"# {ACTIVITY}"))
-    for title, content in ACTIVITY.items():
+    display(Markdown(f"# {activity}"))
+    for title, content in activity.items():
         display(Markdown(f"## {title}"))
         if type(content) is dict:
             for subtitle, subcontent in content.items():
@@ -89,7 +101,7 @@ def show_activity(METHOD=method, ACTIVITY=activity):
     display(Markdown("---"))
 
     # PRODUCTION
-    exchanges = ACTIVITY.production()
+    exchanges = activity.production()
     display(Markdown(f"# There are {len(exchanges)} production exchanges:"))
     for exchange in exchanges:
         amount = exchange.get("amount", "N/A")
@@ -103,7 +115,7 @@ def show_activity(METHOD=method, ACTIVITY=activity):
     display(Markdown("---"))
 
     # BIOSPHERE
-    exchanges = ACTIVITY.biosphere()
+    exchanges = activity.biosphere()
     display(Markdown(f"# There are {len(exchanges)} exchanges with the biosphere:"))
     for exchange in exchanges:
         amount = exchange.get("amount", "N/A")
@@ -113,7 +125,7 @@ def show_activity(METHOD=method, ACTIVITY=activity):
     display(Markdown("---"))
 
     # TECHNOSPHERE
-    exchanges = ACTIVITY.technosphere()
+    exchanges = activity.technosphere()
     display(Markdown(f"# There are {len(exchanges)} exchanges with the technosphere:"))
     for exchange in exchanges:
         amount = exchange.get("amount", "N/A")
@@ -127,7 +139,7 @@ def show_activity(METHOD=method, ACTIVITY=activity):
     display(Markdown("---"))
 
     # SUBSTITUTIONS
-    exchanges = ACTIVITY.substitution()
+    exchanges = activity.substitution()
     display(Markdown(f"# There are {len(exchanges)} substitution exchanges:"))
     for exchange in exchanges:
         amount = exchange.get("amount", "N/A")
@@ -141,4 +153,15 @@ def show_activity(METHOD=method, ACTIVITY=activity):
     display(Markdown("---"))
 
 
-_ = interact(show_activity, METHOD=method, ACTIVITY=activity)
+f_database.observe(search_activity, names="value")
+f_search.observe(search_activity, names="value")
+f_limit.observe(search_activity, names="value")
+f_activity.observe(show_activity, names="value")
+f_method.observe(show_activity, names="value")
+display(f_database, f_search, f_limit)
+display(f_results)
+display(f_activity)
+display(f_method)
+display(f_details)
+
+# _ = interact(show_activity, method=f_method, activity=f_activity)
