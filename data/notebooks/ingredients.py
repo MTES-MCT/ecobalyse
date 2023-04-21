89dd02f558c6f11b8ccfbbe0c211cc597a307c9f
Christophe Combelles <ccomb@free.fr>
2023-04-21 17:41:30 +0200

    display json, not dict

--- data/notebooks/ingredients.py
+++ data/notebooks/ingredients.py
@@ -300,7 +300,7 @@ def list_ingredients():
             Markdown(f"# List of {len(ingredients)} ingredients:"),
             pandas.DataFrame(ingredients.values(), columns=list(FIELDS.values())),
             Markdown(f"# Resulting JSON file:"),
-            json.load(fp),
+            json.dumps(json.load(fp), indent=2),
         )
 
 
