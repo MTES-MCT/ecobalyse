f789266fe544b5a7a379be9ebf4c39d410c02b88
Christophe Combelles <ccomb@free.fr>
2023-04-24 18:01:51 +0200

    Single csv file with code, kind, alias, displayName and name

--- data/food/export_agb/export_builder.py
+++ data/food/export_agb/export_builder.py
@@ -9,141 +9,22 @@ import csv
 import json
 import argparse
 import brightway2 as bw
-from collections import defaultdict
 from food.impacts import impacts
 import uuid
 import hashlib
 
-processes_kind = {
-    # transformation
-    "Cooking, industrial, 1kg of cooked product/ FR U": "transformation",
-    'Mixing, processing, at plant "dummy process"': "transformation",
-    "Canning fruits or vegetables, industrial, 1kg of canned product/ FR U": "transformation",
-    # packaging
-    "Corrugated board box {RER}| production | Cut-off, S - Copied from Ecoinvent": "packaging",
-    "Kraft paper, unbleached {RER}| production | Cut-off, S - Copied from Ecoinvent": "packaging",
-    "Polystyrene, expandable {RER}| production | Cut-off, S - Copied from Ecoinvent": "packaging",
-    "Packaging glass, white {RER w/o CH+DE}| production | Cut-off, S - Copied from Ecoinvent": "packaging",
-    "Polypropylene, granulate {RER}| production | Cut-off, S - Copied from Ecoinvent": "packaging",
-    "Polyethylene terephthalate, granulate, bottle grade {RER}| production | Cut-off, S - Copied from Ecoinvent": "packaging",
-    "Packaging film, low density polyethylene {RER}| production | Cut-off, S - Copied from Ecoinvent": "packaging",
-    "Polyethylene, high density, granulate {RER}| production | Cut-off, S - Copied from Ecoinvent": "packaging",
-    "Steel, unalloyed {RER}| steel production, converter, unalloyed | Cut-off, S - Copied from Ecoinvent": "packaging",
-    "Polyvinylchloride, suspension polymerised {RER}| polyvinylchloride production, suspension polymerisation | Cut-off, S - Copied from Ecoinvent": "packaging",
-    "Aluminium, primary, ingot {RoW}| production | Cut-off, S - Copied from Ecoinvent": "packaging",
-}
 
-
-processes_alias = {
-    # transformation
-    "Cooking, industrial, 1kg of cooked product/ FR U": "cooking",
-    'Mixing, processing, at plant "dummy process"': "mixing",
-    "Canning fruits or vegetables, industrial, 1kg of canned product/ FR U": "canning",
-    # packaging
-    "Corrugated board box {RER}| production | Cut-off, S - Copied from Ecoinvent": "cardboard",
-    "Kraft paper, unbleached {RER}| production | Cut-off, S - Copied from Ecoinvent": "paper",
-    "Polystyrene, expandable {RER}| production | Cut-off, S - Copied from Ecoinvent": "ps",
-    "Packaging glass, white {RER w/o CH+DE}| production | Cut-off, S - Copied from Ecoinvent": "glass",
-    "Polypropylene, granulate {RER}| production | Cut-off, S - Copied from Ecoinvent": "pp",
-    "Polyethylene terephthalate, granulate, bottle grade {RER}| production | Cut-off, S - Copied from Ecoinvent": "pet",
-    "Packaging film, low density polyethylene {RER}| production | Cut-off, S - Copied from Ecoinvent": "ldpe",
-    "Polyethylene, high density, granulate {RER}| production | Cut-off, S - Copied from Ecoinvent": "hdpe",
-    "Steel, unalloyed {RER}| steel production, converter, unalloyed | Cut-off, S - Copied from Ecoinvent": "steel",
-    "Polyvinylchloride, suspension polymerised {RER}| polyvinylchloride production, suspension polymerisation | Cut-off, S - Copied from Ecoinvent": "pvc",
-    "Aluminium, primary, ingot {RoW}| production | Cut-off, S - Copied from Ecoinvent": "aluminium",
-    "Electricity, low voltage {FR}| market for | Cut-off, S - Copied from Ecoinvent": "low-voltage-electricity",
-    "Tap water {Europe without Switzerland}| market for | Cut-off, S - Copied from Ecoinvent": "tapwater",
-    "Transport, freight, aircraft {RER}| intercontinental | Cut-off, S - Copied from Ecoinvent": "plane",
-    "Transport, freight, sea, transoceanic ship {GLO}| processing | Cut-off, S - Copied from Ecoinvent": "boat",
-    "Transport, freight, lorry 16-32 metric ton, EURO5 {RER}| transport, freight, lorry 16-32 metric ton, EURO5 | Cut-off, S - Copied from Ecoinvent": "lorry",
-    "Transport, freight, sea, transoceanic ship with reefer, cooling {GLO}| processing | Cut-off, S - Copied from Ecoinvent": "boat-cooling",
-    "Transport, freight, lorry with refrigeration machine, 7.5-16 ton, EURO5, R134a refrigerant, cooling {GLO}| transport, freight, lorry with refrigeration machine, 7.5-16 ton, EURO5, R134a refrigerant, cooling | Cut-off, S - Copied from Ecoinvent": "lorry-cooling",
-    # distribution
-    "Heat, central or small-scale, natural gas {Europe without Switzerland}| market for heat, central or small-scale, natural gas | Cut-off, S - Copied from Ecoinvent": "domestic-gas-heat",
-}
-
-
-processes_display_name = {
-    # transformation
-    "Cooking, industrial, 1kg of cooked product/ FR U": "Cuisson",
-    'Mixing, processing, at plant "dummy process"': "Mélange",
-    "Canning fruits or vegetables, industrial, 1kg of canned product/ FR U": "Mise en conserve",
-    # packaging
-    "Corrugated board box {RER}| production | Cut-off, S - Copied from Ecoinvent": "Carton",
-    "Kraft paper, unbleached {RER}| production | Cut-off, S - Copied from Ecoinvent": "Papier",
-    "Polystyrene, expandable {RER}| production | Cut-off, S - Copied from Ecoinvent": "Polystyrène",
-    "Packaging glass, white {RER w/o CH+DE}| production | Cut-off, S - Copied from Ecoinvent": "Verre",
-    "Polypropylene, granulate {RER}| production | Cut-off, S - Copied from Ecoinvent": "Polypropylène",
-    "Polyethylene terephthalate, granulate, bottle grade {RER}| production | Cut-off, S - Copied from Ecoinvent": "PET",
-    "Packaging film, low density polyethylene {RER}| production | Cut-off, S - Copied from Ecoinvent": "Polyéthylène basse densité",
-    "Polyethylene, high density, granulate {RER}| production | Cut-off, S - Copied from Ecoinvent": "Polyéthylène haute densité",
-    "Steel, unalloyed {RER}| steel production, converter, unalloyed | Cut-off, S - Copied from Ecoinvent": "Acier",
-    "Polyvinylchloride, suspension polymerised {RER}| polyvinylchloride production, suspension polymerisation | Cut-off, S - Copied from Ecoinvent": "PVC",
-    "Aluminium, primary, ingot {RoW}| production | Cut-off, S - Copied from Ecoinvent": "Aluminium",
-}
-
-
-def get_activities(agribalyse_db, processes_name):
+def get_activities(agribalyse_db, processes_code):
     activities = []
 
-    for index, process_name in enumerate(processes_name):
-        stripped_name = (
-            process_name.strip()
-        )  # Remove extraneous newline at the end of the line.
-        results = agribalyse_db.search(stripped_name)
-        for result in results:
-            if result["name"] == stripped_name:
-                # We found an exact match, use this instead of the first result
-                # which may not be the most relevant
-                activity = result
-                break
-        else:
-            # We didn't find an exact match
-            activity = results[0]
-        activities.append(activity)
+    for index, code in enumerate(processes_code):
+        activities.append(agribalyse_db.get(code))
         if index % 100 == 0 and index:
             print(f"Chargement de {index} activités", end="\r")
 
     print(f"Chargement de {len(activities)} activités terminé")
 
     return activities
-
-
-def add_process(processes, activity):
-    activity_name = activity["name"]
-    processes[activity]["name"] = activity_name
-
-    if activity_name in processes_alias:
-        processes[activity]["alias"] = processes_alias[activity_name]
-    if activity_name in processes_display_name:
-        processes[activity]["displayName"] = processes_display_name[activity_name]
-
-    processes[activity]["unit"] = activity._data["unit"]
-    processes[activity]["simapro_id"] = activity._data["code"]
-
-    processes[activity]["system_description"] = activity._data["simapro metadata"][
-        "System description"
-    ]
-
-    # Useful info like the category_tags and comment are in the production exchange
-    prod_exchange = list(activity.production())[0]
-    processes[activity]["category_tags"] = prod_exchange._data["categories"]
-    if prod_exchange._data["comment"]:
-        processes[activity]["comment"] = prod_exchange._data["comment"]
-    category = activity._data["simapro metadata"]["Category type"]
-
-    # We have our own classification/categorization.
-    if (
-        activity._data["simapro metadata"]["Category type"] == "material"
-        and "Food" in processes[activity]["category_tags"]
-        and processes[activity]["unit"] == "kilogram"
-    ):
-        category = "ingredient"
-    elif activity_name in processes_kind:
-        category = processes_kind[activity_name]
-
-    processes[activity]["category"] = category
-    processes[activity]["impacts"] = {}
 
 
 def open_db(dbname):
@@ -164,14 +45,12 @@ def init_lcas(demand):
     return lcas
 
 
-def impacts_for_activity(activity, lcas, impacts_ecobalyse, bvi_data):
+def impacts_for_activity(activity, lcas, bvi_data):
     activity_impacts = {}
     # Compute every impact but the PEF (computed later) and BVI (imported from bvi_data)
     for impact in impacts.keys():
         lca = lcas[impact]
-
-        demand = {activity: 1}
-        lca.redo_lcia(demand)
+        lca.redo_lcia({activity: 1})
         activity_impacts[impact] = lca.score
 
     # Add the bvi impact that's been imported from another source (not coming from agribalyse,
@@ -195,14 +74,14 @@ def impacts_for_activity(activity, lcas, impacts_ecobalyse, bvi_data):
     return activity_impacts
 
 
-def compute_lca(processes, lcas, impacts_ecobalyse, bvi_data):
-    num_processes = len(processes)
-    print(f"Calcul de l'impact pour {num_processes} procédés")
-    for index, activity in enumerate(processes):
-        impacts = impacts_for_activity(activity, lcas, impacts_ecobalyse, bvi_data)
-        processes[activity]["impacts"] = impacts
+def compute_lca(activities, lcas, bvi_data):
+    num_activities = len(activities)
+    print(f"Calcul de l'impact pour {num_activities} procédés")
+    for index, activity in enumerate([v["activity"] for v in activities.values()]):
+        impacts = impacts_for_activity(activity, lcas, bvi_data)
+        activities[activity["code"]]["export_data"]["impacts"] = impacts
         if index % 10 == 0:
-            print(f"{round(index * 100 / num_processes)}%", end="\r")
+            print(f"{round(index * 100 / num_activities)}%", end="\r")
     print("100%")
 
 
@@ -232,31 +111,31 @@ def parse_ingredient_list(ingredients_base):
         for variant in ingredient["variants"].values():
             if is_complex_ingredient(variant):
                 # This is a complex ingredient, we need to create a new process from the elements we have.
-                processes_to_add.append(variant["simple_ingredient_default"])
-                processes_to_add.append(variant["simple_ingredient_variant"])
+                processes_to_add.append({"code": variant["simple_ingredient_default"]})
+                processes_to_add.append({"code": variant["simple_ingredient_variant"]})
     return processes_to_add
 
 
-def compute_ingredient_list(processes, ingredients_base):
+def compute_ingredient_list(activities, ingredients_base):
     new_processes = []
 
     for ingredient in ingredients_base:
         for variant_name, variant in ingredient["variants"].items():
             if is_complex_ingredient(variant):
                 # This is a complex ingredient, we need to create a new process from the elements we have.
-                complex_ingredient_default = get_process_by_id(
-                    processes, ingredient["default"]
-                )
+                complex_ingredient_default = activities[ingredient["default"]][
+                    "export_data"
+                ]
                 # The ratio is the quantity of simple ingredient necessary to produce 1 unit of complex ingredient
                 # For example, you need 1.16 kg of wheat (simple) to produce 1 kg of flour (complex) -> ratio = 1.16
                 ratio = variant["ratio"]
 
-                simple_ingredient_default = get_process_by_id(
-                    processes, variant["simple_ingredient_default"]
-                )
-                simple_ingredient_variant = get_process_by_id(
-                    processes, variant["simple_ingredient_variant"]
-                )
+                simple_ingredient_default = activities[
+                    variant["simple_ingredient_default"]
+                ]["export_data"]
+                simple_ingredient_variant = activities[
+                    variant["simple_ingredient_variant"]
+                ]["export_data"]
 
                 new_process = copy.deepcopy(complex_ingredient_default)
                 new_process[
@@ -301,9 +180,11 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    processes_to_export_file = "builder_processes_to_export.txt"
-    with open(processes_to_export_file, "r") as f:
-        processes_to_export = [line for line in f.readlines() if line.strip()]
+    with open("builder_processes_to_export.csv") as f:
+        processes_to_export = [
+            dict([(k, v) for k, v in d.items() if v])
+            for d in list(csv.DictReader(f, dialect="unix"))
+        ]
 
     # Parse the ingredients_base.json, which may contain complex ingredients to add/compute
     with open("ingredients_base.json", "r") as f:
@@ -318,20 +199,48 @@ if __name__ == "__main__":
 
     agb = open_db("Agribalyse 3.0")
 
-    activities = get_activities(agb, processes_to_export)
+    activities = {
+        p["code"]: {
+            "activity": agb.get(p["code"]),
+            "export_data": p,  # agb.get(p["code"]).as_dict(),
+        }
+        for p in processes_to_export
+    }
     print(f"Total de {len(activities)} activités trouvées dans agribalyse")
 
-    processes = defaultdict(dict)
-    for activity in activities:
-        add_process(processes, activity)
+    for v in list(activities.values()):
+        # move data
+        activity, export_data = v["activity"], v["export_data"]
+        export_data["simapro_id"] = export_data["code"]
+        del export_data["code"]
+        export_data["name"] = activity.get("simapro name", activity["name"])
+        export_data["unit"] = activity["unit"]
+        export_data["system_description"] = activity["simapro metadata"][
+            "System description"
+        ]
+
+        # Useful info like the category_tags and comment are in the production exchange
+        prod_exchange = list(activity.production())[0]
+        export_data["category_tags"] = prod_exchange["categories"]
+        if prod_exchange["comment"]:
+            export_data["comment"] = prod_exchange["comment"]
+
+        category = activity["simapro metadata"]["Category type"]
+        # We have our own classification/categorization.
+        if (
+            category == "material"
+            and "Food" in export_data["category_tags"]
+            and activity["unit"] == "kilogram"
+        ):
+            category = "ingredient"
+        elif "kind" in export_data:
+            category = export_data["kind"]
+
+        export_data["category"] = category
+        export_data["impacts"] = {}
 
     # Just get a random process, for example the very first one
-    random_process = next(iter(processes))
-    lcas = init_lcas({random_process: 1})
-
-    impacts_file = "../../../public/data/impacts.json"
-    with open(impacts_file, "r") as f:
-        impacts_ecobalyse = json.load(f)
+    lcas = init_lcas({next(iter(activities.values()))["activity"]: 1})
 
     bvi_data_file = "bvi.csv"
     bvi_data = {}
@@ -340,11 +249,11 @@ if __name__ == "__main__":
         for row in reader:
             bvi_data[row["process_name"]] = row["bvi"]
 
-    compute_lca(processes, lcas, impacts_ecobalyse, bvi_data)
+    compute_lca(activities, lcas, bvi_data)
 
     # Extract simple and complex ingredients. Complex ingredients are need a new process to be added.
     (ingredient_list, new_processes) = compute_ingredient_list(
-        processes, ingredients_base
+        activities, ingredients_base
     )
 
     # Export the ingredients.json file
@@ -354,13 +263,10 @@ if __name__ == "__main__":
     )
     export_json(ingredient_list, ingredients_export_file)
 
-    # reformat processes in a list of dictionaries
-    processes_list = list(processes.values())
-
     # Add the new processes we computed for the complex ingredients
-    processes_list += new_processes
+    export = [v["export_data"] for v in activities.values()] + new_processes
 
     processes_export_file = "../../../public/data/food/processes/builder.json"
-    print(f"Export de {len(processes_list)} procédés vers {processes_export_file}")
-    export_json(processes_list, processes_export_file)
+    print(f"Export de {len(export)} procédés vers {processes_export_file}")
+    export_json(export, processes_export_file)
     print("Terminé.")
