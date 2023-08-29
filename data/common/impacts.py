df0ae13dbac3a866986420025fb4368791ad9ae4
Christophe Combelles <ccomb@free.fr>
2023-08-29 18:28:09 +0200

    missing impacts

---
+++ data/common/impacts.py
@@ -0,0 +1,86 @@
+impacts = {
+    "acd": ("Environmental Footprint 3.1 (adapted) patch wtu", "Acidification"),
+    "ozd": ("Environmental Footprint 3.1 (adapted) patch wtu", "Ozone depletion"),
+    "cch": ("Environmental Footprint 3.1 (adapted) patch wtu", "Climate change"),
+    "fwe": (
+        "Environmental Footprint 3.1 (adapted) patch wtu",
+        "Eutrophication, freshwater",
+    ),
+    "swe": (
+        "Environmental Footprint 3.1 (adapted) patch wtu",
+        "Eutrophication, marine",
+    ),
+    "tre": (
+        "Environmental Footprint 3.1 (adapted) patch wtu",
+        "Eutrophication, terrestrial",
+    ),
+    "pco": (
+        "Environmental Footprint 3.1 (adapted) patch wtu",
+        "Photochemical ozone formation",
+    ),
+    "pma": ("Environmental Footprint 3.1 (adapted) patch wtu", "Particulate matter"),
+    "ior": (
+        "Environmental Footprint 3.1 (adapted) patch wtu",
+        "Ionising radiation",
+    ),
+    "fru": (
+        "Environmental Footprint 3.1 (adapted) patch wtu",
+        "Resource use, fossils",
+    ),
+    "mru": (
+        "Environmental Footprint 3.1 (adapted) patch wtu",
+        "Resource use, minerals and metals",
+    ),
+    "ldu": (
+        "Environmental Footprint 3.1 (adapted) patch wtu",
+        "Land use",
+    ),
+    "wtu": (
+        "Environmental Footprint 3.1 (adapted) patch wtu",
+        "Water use",
+    ),
+    "etf-o1": (
+        "Environmental Footprint 3.1 (adapted) patch wtu",
+        "Ecotoxicity, freshwater - organics - p.1",
+    ),
+    "etf-o2": (
+        "Environmental Footprint 3.1 (adapted) patch wtu",
+        "Ecotoxicity, freshwater - organics - p.2",
+    ),
+    "etf-i": (
+        "Environmental Footprint 3.1 (adapted) patch wtu",
+        "Ecotoxicity, freshwater - inorganics",
+    ),
+    "etf1": (
+        "Environmental Footprint 3.1 (adapted) patch wtu",
+        "Ecotoxicity, freshwater - part 1",
+    ),
+    "etf2": (
+        "Environmental Footprint 3.1 (adapted) patch wtu",
+        "Ecotoxicity, freshwater - part 2",
+    ),
+    "htc": (
+        "Environmental Footprint 3.1 (adapted) patch wtu",
+        "Human toxicity, cancer",
+    ),
+    "htc-o": (
+        "Environmental Footprint 3.1 (adapted) patch wtu",
+        "Human toxicity, cancer - organics",
+    ),
+    "htc-i": (
+        "Environmental Footprint 3.1 (adapted) patch wtu",
+        "Human toxicity, cancer - inorganics",
+    ),
+    "htn": (
+        "Environmental Footprint 3.1 (adapted) patch wtu",
+        "Human toxicity, non-cancer",
+    ),
+    "htn-o": (
+        "Environmental Footprint 3.1 (adapted) patch wtu",
+        "Human toxicity, non-cancer - organics",
+    ),
+    "htn-i": (
+        "Environmental Footprint 3.1 (adapted) patch wtu",
+        "Human toxicity, non-cancer - inorganics",
+    ),
+}
