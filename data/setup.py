3ff4df9e3728398f8bf51f3b4de1ac46a42f344e
Christophe Combelles <ccomb@free.fr>
2023-09-09 10:25:03 +0200

    bw_projects in install_requires

--- data/setup.py
+++ data/setup.py
@@ -1,3 +1,8 @@
 from setuptools import setup
 
-setup(name="ecobalyse_data", version="1.0", packages=["food", "common", "textile"])
+setup(
+    name="ecobalyse_data",
+    version="1.0",
+    packages=["food", "common", "textile"],
+    install_requires=["bw_projects"],
+)
