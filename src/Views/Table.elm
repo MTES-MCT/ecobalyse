2e7b0da08a9128a5247ed80f693de4bbf4f25b3f
Nicolas Perriault <nicolas@perriault.net>
2023-09-21 23:04:19 +0200

    Limit detailed impacts table visible height in the sidebar.

--- src/Views/Table.elm
+++ src/Views/Table.elm
@@ -35,42 +35,44 @@ percentageTable data =
         text ""
 
     else
-        table [ class "table table-hover w-100 m-0" ]
-            [ data
-                |> List.map
-                    (\( name, value ) ->
-                        { name = name
-                        , impact = value
-                        , percent = value / total * 100
-                        , width =
-                            if value < 0 then
-                                abs (value / minimum * 100)
+        div [ class "table-responsive", style "max-height" "400px" ]
+            [ table [ class "table table-hover w-100 m-0" ]
+                [ data
+                    |> List.map
+                        (\( name, value ) ->
+                            { name = name
+                            , impact = value
+                            , percent = value / total * 100
+                            , width =
+                                if value < 0 then
+                                    abs (value / minimum * 100)
 
-                            else
-                                value / maximum * 100
-                        }
-                    )
-                |> List.map
-                    (\{ name, impact, percent, width } ->
-                        tr [ title <| name ++ ": " ++ Format.formatFloat 2 percent ++ "\u{202F}% (" ++ Format.formatFloat 2 impact ++ "\u{202F}µPts)" ]
-                            [ th [ class "text-truncate fw-normal fs-8", style "max-width" "200px", title name ] [ text name ]
-                            , td [ class "HorizontalBarChart", style "width" "200px", style "vertical-align" "middle" ]
-                                [ div
-                                    [ class "ext"
-                                    , classList [ ( "pos", percent >= 0 ), ( "neg", percent < 0 ) ]
-                                    ]
+                                else
+                                    value / maximum * 100
+                            }
+                        )
+                    |> List.map
+                        (\{ name, impact, percent, width } ->
+                            tr [ title <| name ++ ": " ++ Format.formatFloat 2 percent ++ "\u{202F}% (" ++ Format.formatFloat 2 impact ++ "\u{202F}µPts)" ]
+                                [ th [ class "text-truncate fw-normal fs-8", style "max-width" "200px", title name ] [ text name ]
+                                , td [ class "HorizontalBarChart", style "width" "200px", style "vertical-align" "middle" ]
                                     [ div
-                                        [ class "bar"
-                                        , classList [ ( "bg-secondary", percent >= 0 ), ( "bg-success", percent < 0 ) ]
-                                        , style "width" (String.fromFloat width ++ "%")
+                                        [ class "ext"
+                                        , classList [ ( "pos", percent >= 0 ), ( "neg", percent < 0 ) ]
                                         ]
-                                        []
+                                        [ div
+                                            [ class "bar"
+                                            , classList [ ( "bg-secondary", percent >= 0 ), ( "bg-success", percent < 0 ) ]
+                                            , style "width" (String.fromFloat width ++ "%")
+                                            ]
+                                            []
+                                        ]
+                                    ]
+                                , td [ class "text-end text-nowrap fs-8" ]
+                                    [ Format.percent percent
                                     ]
                                 ]
-                            , td [ class "text-end text-nowrap fs-8" ]
-                                [ Format.percent percent
-                                ]
-                            ]
-                    )
-                |> tbody []
+                        )
+                    |> tbody []
+                ]
             ]
