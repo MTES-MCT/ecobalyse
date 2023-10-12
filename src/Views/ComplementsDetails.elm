b10436dccd6eb1fb3529206111973881e72df582
Nicolas Perriault <nicolas@perriault.net>
2023-10-12 08:45:46 +0200

    Cleanup and reorganize stylesheet.

--- src/Views/ComplementsDetails.elm
+++ src/Views/ComplementsDetails.elm
@@ -13,7 +13,6 @@ type alias Config =
 
 view : Config -> List (Html msg) -> Html msg
 view { complementsImpacts } detailedImpacts =
-    -- TODO: Move this to Views.BaseElement as a generic element complements view
     details [ class "ComplementsDetails fs-7" ]
         (summary []
             [ div [ class "ComplementsTable d-flex justify-content-between w-100" ]
