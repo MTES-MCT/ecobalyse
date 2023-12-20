c627be9c037fc2af2e2bb160b2be947a4dfd7e6b
Nicolas Perriault <nicolas@perriault.net>
2023-12-20 09:52:25 +0100

    Fix error with non-existent "polyester" material id.

--- src/Data/Textile/Inputs.elm
+++ src/Data/Textile/Inputs.elm
@@ -1059,7 +1059,7 @@ pullLaineAsie =
 jupePolyesterAsie : Query
 jupePolyesterAsie =
     { jupeCotonAsie
-        | materials = [ { id = Material.Id "polyester", share = Split.full, spinning = Nothing, country = Nothing } ]
+        | materials = [ { id = Material.Id "pet", share = Split.full, spinning = Nothing, country = Nothing } ]
     }
 
 
@@ -1076,5 +1076,5 @@ manteauMixAsie =
 tShirtPolyesterAsie : Query
 tShirtPolyesterAsie =
     { tShirtCotonAsie
-        | materials = [ { id = Material.Id "polyester", share = Split.full, spinning = Nothing, country = Nothing } ]
+        | materials = [ { id = Material.Id "pet", share = Split.full, spinning = Nothing, country = Nothing } ]
     }
