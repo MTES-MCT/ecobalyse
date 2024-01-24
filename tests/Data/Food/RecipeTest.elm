f0aac16678d20786185b40a9b8834e585fa8bb88
paulboosz <paulboosz@gmail.com>
2024-01-24 17:11:41 +0100

    Fix organic lamb (#457)
    Co-authored-by: Ingredient editor <ecobalyse@beta.gouv.fr>

--- tests/Data/Food/RecipeTest.elm
+++ tests/Data/Food/RecipeTest.elm
@@ -149,7 +149,7 @@ suite =
                                 Expect.fail err
 
                             Ok result ->
-                                expectImpactEqual (Unit.impact 109.94732826911402) result
+                                expectImpactEqual (Unit.impact 109.70732826911402) result
                         )
                      , asTest "should have the ingredients' total ecs impact with the complement taken into account"
                         (case carrotCakeResults |> Result.map (Tuple.second >> .recipe >> .ingredientsTotal >> Impact.getImpact Definition.Ecs) of
@@ -157,7 +157,7 @@ suite =
                                 Expect.fail err
 
                             Ok result ->
-                                expectImpactEqual (Unit.impact 71.74315461752435) result
+                                expectImpactEqual (Unit.impact 71.50315461752434) result
                         )
                      , describe "Scoring"
                         (case carrotCakeResults |> Result.map (Tuple.second >> .scoring) of
@@ -168,13 +168,13 @@ suite =
 
                             Ok scoring ->
                                 [ Unit.impactToFloat scoring.all
-                                    |> Expect.within (Expect.Absolute 0.01) 206.8017910137816
+                                    |> Expect.within (Expect.Absolute 0.01) 206.42860098541917
                                     |> asTest "should properly score total impact"
                                 , Unit.impactToFloat scoring.allWithoutComplements
                                     |> Expect.within (Expect.Absolute 0.01) 207.03596775657905
                                     |> asTest "should properly score total impact without complements"
                                 , Unit.impactToFloat scoring.complements
-                                    |> Expect.within (Expect.Absolute 0.01) 0.2341767427974326
+                                    |> Expect.within (Expect.Absolute 0.01) 0.6073667711598745
                                     |> asTest "should properly score complement impact"
                                 , (Unit.impactToFloat scoring.allWithoutComplements - Unit.impactToFloat scoring.complements)
                                     |> Expect.within (Expect.Absolute 0.0001) (Unit.impactToFloat scoring.all)
