f226474ca6210cd9ab32091122bc7c08abb72809
Christophe Combelles <ccomb@free.fr>
2024-01-26 14:52:51 +0100

    put the surfaces in a json cache file

--- data/spapi/simapro.py
+++ data/spapi/simapro.py
@@ -1,6 +1,9 @@
-import win32com.client
+# encoding: utf-8
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
+import json
+import os.path
+import win32com.client
 
 server = win32com.client.Dispatch("SimaPro.SimaProServer")
 server.Server = "local server"
@@ -9,15 +12,27 @@ server.Database = "Professional"
 server.OpenDatabase()
 server.OpenProject("AGB3.1.1 2023-03-06", "")
 
+surfaces = {}
+if os.path.exists("surfaces.json"):
+    surfaces = json.load(open("surfaces.json"))
+
 api = FastAPI()
 
+
 @api.get("/surface", response_class=JSONResponse)
-async def surface(request: Request, process: str):
+async def surface(_: Request, process: str):
     """exemple:
     project: process: "Soft wheat grain, organic, 15% moisture, Central Region, at feed plant {FR} U"
     """
-    server.Analyse("AGB3.1.1 2023-03-06", 0, process, "Methods", "Selected LCI results", "")
-    return {
-         "surface": server.AnalyseResult(0, 5).Amount,
-         "unit": server.AnalyseResult(0, 5).UnitName
-    }
+    global surfaces  # 🤮
+    if process not in surfaces:
+        server.Analyse(
+            "AGB3.1.1 2023-03-06", 0, process, "Methods", "Selected LCI results", ""
+        )
+        amount = server.AnalyseResult(0, 5).Amount
+        surfaces[process] = amount
+        with open("surfaces.json", "w") as fp:
+            json.dump(surfaces, fp, ensure_ascii=False)
+    else:
+        amount = surfaces[process]
+    return {"surface": amount, "unit": server.AnalyseResult(0, 5).UnitName}
