d6d7a3d134d6ff1f69f65df58c0c11493ad85bce
Mathieu Agopian <mathieu@agopian.info>
2024-02-02 15:55:23 +0100

    Formatting

--- compute-aggregated.js
+++ compute-aggregated.js
@@ -2,27 +2,37 @@ require("dotenv").config();
 const fs = require("fs");
 const { Elm } = require("./compute-aggregated-app");
 
-
 const elmApp = Elm.ComputeAggregated.init({
-	"flags": {
-		"definitionsString": fs.readFileSync("public/data/impacts.json", "utf-8"),
-		"textileProcessesString": fs.readFileSync("public/data/textile/processes_impacts.json", "utf-8"),
-		"foodProcessesString": fs.readFileSync("public/data/food/processes_impacts.json", "utf-8")
-	}
+  flags: {
+    definitionsString: fs.readFileSync("public/data/impacts.json", "utf-8"),
+    textileProcessesString: fs.readFileSync("public/data/textile/processes_impacts.json", "utf-8"),
+    foodProcessesString: fs.readFileSync("public/data/food/processes_impacts.json", "utf-8"),
+  },
 });
 
-elmApp.ports.export.subscribe(({ textileProcesses, foodProcesses, textileProcessesOnlyAggregated, foodProcessesOnlyAggregated }) => {
-	try {
-	  fs.writeFileSync('public/data/textile/processes_impacts.json', JSON.stringify(textileProcesses, null, 2));
-	  fs.writeFileSync('public/data/food/processes_impacts.json', JSON.stringify(foodProcesses, null, 2));
-	  fs.writeFileSync('public/data/textile/processes.json', JSON.stringify(textileProcessesOnlyAggregated, null, 2));
-	  fs.writeFileSync('public/data/food/processes.json', JSON.stringify(foodProcessesOnlyAggregated, null, 2));
-		console.log("EXPORTED!");
-	} catch (err) {
-	  console.error(err);
-	}
-});
+const exportJson = (filePath, json) => {
+  fs.writeFileSync(filePath, JSON.stringify(json, null, 2) + "\n");
+};
 
-elmApp.ports.logError.subscribe(errorMessage => {
-	console.error("Error:", errorMessage);
+elmApp.ports.export.subscribe(
+  ({
+    textileProcesses,
+    foodProcesses,
+    textileProcessesOnlyAggregated,
+    foodProcessesOnlyAggregated,
+  }) => {
+    try {
+      exportJson("public/data/textile/processes_impacts.json", textileProcesses);
+      exportJson("public/data/food/processes_impacts.json", foodProcesses);
+      exportJson("public/data/textile/processes.json", textileProcessesOnlyAggregated);
+      exportJson("public/data/food/processes.json", foodProcessesOnlyAggregated);
+      console.log("EXPORTED!");
+    } catch (err) {
+      console.error(err);
+    }
+  },
+);
+
+elmApp.ports.logError.subscribe((errorMessage) => {
+  console.error("Error:", errorMessage);
 });
