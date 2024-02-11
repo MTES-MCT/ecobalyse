35d13f7d6ed9976c93febbf49a298113a81de70e
Christophe Combelles <ccomb@free.fr>
2024-02-11 09:46:40 +0100

    Fixed textile db generation

--- src/Data/Textile/Db.elm
+++ src/Data/Textile/Db.elm
@@ -3,7 +3,7 @@ module Data.Textile.Db exposing
     , buildFromJson
     )
 
-import Data.Impact.Definition as Definition
+import Data.Impact.Definition exposing (Definitions)
 import Data.Textile.Material as Material exposing (Material)
 import Data.Textile.Process as TextileProcess
 import Data.Textile.Product as Product exposing (Product)
@@ -19,28 +19,24 @@ type alias Db =
     }
 
 
-buildFromJson : String -> Result String Db
-buildFromJson json =
-    Decode.decodeString decode json
+buildFromJson : Definitions -> String -> Result String Db
+buildFromJson definitions json =
+    Decode.decodeString (decode definitions) json
         |> Result.mapError Decode.errorToString
 
 
-decode : Decoder Db
-decode =
-    Decode.field "impacts" Definition.decode
+decode : Definitions -> Decoder Db
+decode definitions =
+    Decode.field "processes" (TextileProcess.decodeList definitions)
         |> Decode.andThen
-            (\definitions ->
-                Decode.field "processes" (TextileProcess.decodeList definitions)
+            (\processes ->
+                Decode.map2 (Db processes)
+                    (Decode.field "materials" (Material.decodeList processes))
+                    (Decode.field "products" (Product.decodeList processes))
                     |> Decode.andThen
-                        (\processes ->
-                            Decode.map2 (Db processes)
-                                (Decode.field "materials" (Material.decodeList processes))
-                                (Decode.field "products" (Product.decodeList processes))
-                                |> Decode.andThen
-                                    (\partiallyLoaded ->
-                                        TextileProcess.loadWellKnown processes
-                                            |> Result.map partiallyLoaded
-                                            |> DE.fromResult
-                                    )
+                        (\partiallyLoaded ->
+                            TextileProcess.loadWellKnown processes
+                                |> Result.map partiallyLoaded
+                                |> DE.fromResult
                         )
             )
