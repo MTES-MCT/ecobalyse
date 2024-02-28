aa79d212b2e35c5b6e375d45006701f8bd837b7b
Christophe Combelles <ccomb@free.fr>
2024-02-28 19:07:41 +0100

    fixed region adding

--- data/common/distances/add_regions.py
+++ data/common/distances/add_regions.py
@@ -19,14 +19,17 @@ old = dict()
 with open("distances.json") as f:
     old = json.load(f)
 
-new = copy.deepcopy(old)
+tmp = copy.deepcopy(old)
 for origin, destinations in old.items():
-    if origin in regions.values():
-        for r in [r for r, c in regions.items() if c == origin]:
-            new[r] = destinations
     for destination, distances in destinations.items():
-        if destination in regions:
-            new[origin][regions[destination]] = distances
+        for r in [r for r, c in regions.items() if c == destination]:
+            tmp[origin][r] = distances
+new = copy.deepcopy(tmp)
+with open("distances_with_regions.tmp.json", "w") as f:
+    json.dump(tmp, f, ensure_ascii=False)
+for origin, destinations in tmp.items():
+    for r in [r for r, c in regions.items() if c == origin]:
+        new[r] = destinations
 
 with open("distances_with_regions.json", "w") as f:
     json.dump(new, f, ensure_ascii=False)
