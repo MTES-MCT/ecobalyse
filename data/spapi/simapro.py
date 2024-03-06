27248fe378d5a255c4343498dc506ec8cfd458df
Christophe Combelles <ccomb@free.fr>
2024-03-06 06:02:10 +0300

    Get impacts from simapro API by default, otherwise brightway for constructed ingredients (#516)
    - Exposed a complete API to retrieve impacts from simapro with the com
    interface
    - Adapted the export and ingredient editor to this API
    - Fallback on brightway for constructed ingredients or if simapro fails
    to provide impacts

--- data/spapi/simapro.py
+++ data/spapi/simapro.py
@@ -3,36 +3,64 @@ from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
 import json
 import os.path
+from time import sleep
 import win32com.client
 
 server = win32com.client.Dispatch("SimaPro.SimaProServer")
 server.Server = "local server"
 server.alias = r"C:\Users\Public\Documents\SimaPro\Database"
 server.Database = "Professional"
+print("Opening database...")
 server.OpenDatabase()
-server.OpenProject("AGB3.1.1 2023-03-06", "")
-
-surfaces = {}
-if os.path.exists("surfaces.json"):
-    surfaces = json.load(open("surfaces.json"))
 
 api = FastAPI()
+lock: bool = False
+current_project = None
 
 
-@api.get("/surface", response_class=JSONResponse)
-async def surface(_: Request, process: str):
+@api.get("/impact", response_class=JSONResponse)
+async def impact(_: Request, project: str, process: str, method: str):
     """exemple:
-    project: process: "Soft wheat grain, organic, 15% moisture, Central Region, at feed plant {FR} U"
+    project: "AGB3.1.1 2023-03-06"
+    method: "Environmental Footprint 3.1 (adapted) patch wtu"
+    process: "Soft wheat grain, organic, 15% moisture, Central Region, at feed plant {FR} U"
     """
-    global surfaces  # 🤮
-    if process not in surfaces:
-        server.Analyse(
-            "AGB3.1.1 2023-03-06", 0, process, "Methods", "Selected LCI results", ""
-        )
-        amount = server.AnalyseResult(0, 5).Amount
-        surfaces[process] = amount
-        with open("surfaces.json", "w") as fp:
-            json.dump(surfaces, fp, ensure_ascii=False)
-    else:
-        amount = surfaces[process]
-    return {"surface": amount}
+    print(f"{project}/{process}/{method}")
+    global lock
+    global server
+    global current_project
+    while lock:
+        print("waiting for lock release...")
+        sleep(1)
+
+    lock = True
+
+    impacts = {}
+    if os.path.exists("impacts.json"):
+        impacts = json.load(open("impacts.json"))
+    try:
+        if not impacts.get(f"{project}/{process}", {}).get(method, {}):
+            if not server.ProjectOpen or project != current_project:
+                print("Opening project...")
+                server.OpenProject(project, "")
+                current_project = project
+
+            print("Computing results...")
+            server.Analyse(project, 0, process, "Methods", method, "")
+            results, i = {}, 0
+            while (r := server.AnalyseResult(0, i)).IndicatorName:
+                results[r.IndicatorName] = {"amount": r.Amount, "unit": r.UnitName}
+                i += 1
+            impacts.setdefault(f"{project}/{process}", {})
+            impacts[f"{project}/{process}"][method] = results
+            with open("impacts.json", "w") as fp:
+                json.dump(impacts, fp, ensure_ascii=False)
+        else:
+            results = impacts.get(f"{project}/{process}", {}).get(method, {})
+    except Exception as e:
+        results = repr(e)
+        current_project = None
+
+    lock = False
+    print(results)
+    return results
