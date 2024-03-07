9d1efdd32232d5b2b105671aa479106df8a12f41
Christophe Combelles <ccomb@free.fr>
2024-03-07 23:26:29 +0300

    Improve export (#522)
    - fixed unit conversion from simapro
    - faster export with better COM handling
    - get more from simapro by handling errors

--- data/spapi/simapro.py
+++ data/spapi/simapro.py
@@ -3,7 +3,6 @@ from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
 import json
 import os.path
-from time import sleep
 import win32com.client
 
 server = win32com.client.Dispatch("SimaPro.SimaProServer")
@@ -12,10 +11,10 @@ server.alias = r"C:\Users\Public\Documents\SimaPro\Database"
 server.Database = "Professional"
 print("Opening database...")
 server.OpenDatabase()
+projects = [server.Projects(i) for i in range(server.Projects.Count())]
+print(f"Existing projects: {', '.join(projects)}")
 
 api = FastAPI()
-lock: bool = False
-current_project = None
 
 
 @api.get("/impact", response_class=JSONResponse)
@@ -26,28 +25,42 @@ async def impact(_: Request, project: str, process: str, method: str):
     process: "Soft wheat grain, organic, 15% moisture, Central Region, at feed plant {FR} U"
     """
     print(f"{project}/{process}/{method}")
-    global lock
-    global server
-    global current_project
-    while lock:
-        print("waiting for lock release...")
-        sleep(1)
-
-    lock = True
 
     impacts = {}
     if os.path.exists("impacts.json"):
         impacts = json.load(open("impacts.json"))
-    try:
-        if not impacts.get(f"{project}/{process}", {}).get(method, {}):
-            if not server.ProjectOpen or project != current_project:
-                print("Opening project...")
-                server.OpenProject(project, "")
-                current_project = project
 
-            print("Computing results...")
-            server.Analyse(project, 0, process, "Methods", method, "")
+    if impacts.get(f"{project}/{process}", {}).get(method, {}):
+        return impacts.get(f"{project}/{process}", {}).get(method, {})
+    else:
+        if project not in projects:
+            return f"project {project} does not exist in SimaPro"
+        if not server.ProjectOpen or project != server.CurrentProject:
+            print(f"Opening project {project}...")
+            server.OpenProject(project, "")
+
+        print("Computing results...")
+        existing = [
+            e
+            for e in [
+                ((i, server.FindProcess(project, i, process)[0])) for i in range(12)
+            ]
+            if e[1]
+        ]
+        found = existing[0] if len(existing) else None
+        if found:
+            server.Analyse(project, found[0], process, "Methods", method, "")
             results, i = {}, 0
+            try:
+                # try the first and stop if it raises (typically on a Dummy process.
+                # Seems a bug in the COM intf)
+                server.AnalyseResult(0, 0)
+            except:
+                impacts.setdefault(f"{project}/{process}", {})
+                impacts[f"{project}/{process}"][method] = results
+                with open("impacts.json", "w") as fp:
+                    json.dump(impacts, fp, ensure_ascii=False)
+                return {}
             while (r := server.AnalyseResult(0, i)).IndicatorName:
                 results[r.IndicatorName] = {"amount": r.Amount, "unit": r.UnitName}
                 i += 1
@@ -55,12 +68,6 @@ async def impact(_: Request, project: str, process: str, method: str):
             impacts[f"{project}/{process}"][method] = results
             with open("impacts.json", "w") as fp:
                 json.dump(impacts, fp, ensure_ascii=False)
+            return results
         else:
-            results = impacts.get(f"{project}/{process}", {}).get(method, {})
-    except Exception as e:
-        results = repr(e)
-        current_project = None
-
-    lock = False
-    print(results)
-    return results
+            return {}
