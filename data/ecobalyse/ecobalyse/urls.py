26e702623d57899c6ebc1b92ca84074654018297
Christophe Combelles <ccomb@free.fr>
2024-03-11 13:26:04 +0100

    renamed backend project to ecobalyse

--- data/ecobalyse/ecobalyse/urls.py
+++ data/ecobalyse/ecobalyse/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for backend project.
+URL configuration for ecobalyse project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
