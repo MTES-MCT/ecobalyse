0b79a8826cc5b6bf793e93692ec94fb888d6e169
Christophe Combelles <ccomb@free.fr>
2024-03-12 13:39:22 +0100

    basic models for textile data

--- data/ecobalyse/backend/admin.py
+++ data/ecobalyse/backend/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Process, Material, Product, Exemple
 
-# Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    pass
+
+
+class MaterialAdmin(admin.ModelAdmin):
+    pass
+
+
+class ProcessAdmin(admin.ModelAdmin):
+    pass
+
+
+class ExempleAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Material, MaterialAdmin)
+admin.site.register(Process, ProcessAdmin)
+admin.site.register(Exemple, ExempleAdmin)
