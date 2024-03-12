0efb68e1f42fd9881b88d8a46521232020733c06
Christophe Combelles <ccomb@free.fr>
2024-03-12 17:16:26 +0100

    Added processes and materials

---
+++ data/ecobalyse/reinit.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+rm db.sqlite3
+rm backend/migrations/*
+python manage.py makemigrations backend
+python manage.py migrate
+echo "Creating initial data"
+python manage.py shell -c "from backend.init import init; init()"
+python manage.py createsuperuser --username admin --email ""
