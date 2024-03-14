d28a6823e4ea33d2d728b1813493402ad8c7e63f
Christophe Combelles <ccomb@free.fr>
2024-03-14 15:31:33 +0100

    same with STATIC_ROOT

--- data/ecobalyse/ecobalyse/settings.py
+++ data/ecobalyse/ecobalyse/settings.py
@@ -117,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = "/app/dist"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
