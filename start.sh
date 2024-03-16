583fbe68dc65c25393760bafd8e15c1d4e8fdccc
Christophe Combelles <ccomb@free.fr>
2024-03-16 10:33:08 +0100

    add comments and remote duplicate traps

--- start.sh
+++ start.sh
@@ -3,13 +3,9 @@
 bin/run &
 pushd data/ecobalyse
 gunicorn -b 127.0.0.1:8002 ecobalyse.wsgi &
-sleep 0.5
-trap "pkill SIGTERM -P $$" SIGTERM
 popd
-
 npm run server:start &
+
 trap "pkill SIGTERM -P $$" SIGTERM
-
 wait -n
-
 pkill -P $$
