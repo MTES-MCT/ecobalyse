6fcebef00aa57c786a49fc2194807e5875d8b7a6
Christophe Combelles <ccomb@free.fr>
2024-03-16 12:36:45 +0100

    allowed hosts

--- data/ecobalyse/ecobalyse/settings.py
+++ data/ecobalyse/ecobalyse/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-6v9tk##n9+42jz-0uk694d&=ra5k^ch^=$z_2hov@+1p4hwyi$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["django"]
 
 
 # Application definition
