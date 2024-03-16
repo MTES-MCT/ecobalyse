7e8c4acb51cc982db84f374e1c565433c57e5c35
Christophe Combelles <ccomb@free.fr>
2024-03-16 17:32:54 +0100

    don't force script name

--- data/ecobalyse/ecobalyse/settings.py
+++ data/ecobalyse/ecobalyse/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "admin/static/"
+STATIC_URL = "static/"
 STATIC_ROOT = "/app/dist"
 
 # Default primary key field type
