3f2c725eb334e90f5c84c718527c3338f8f22430
Christophe Combelles <ccomb@free.fr>
2024-03-18 13:48:32 +0100

    squash experiments

--- start.sh
+++ start.sh
@@ -1,19 +1,22 @@
 #/bin/bash
 
 # run all three tasks in the background
-bin/run &
-pushd data/ecobalyse
 
-# warning: temporary reinit of the application with a local sqlite
-data/ecobalyse/reinit.sh
-#
-
-gunicorn -b 127.0.0.1:8002 ecobalyse.wsgi &
-popd
+# express
 npm run server:start &
 
+# django
+pushd data/ecobalyse
+./reinit.sh # warning: temporary reinit of the application with a local sqlite
+python manage.py collectstatic --noinput
+gunicorn -b 127.0.0.1:8002 ecobalyse.wsgi &
+popd
+
+# nginx
+bin/run &
+
 # if the current shell is killed, also terminate all its children
-trap "echo pkill SIGTERM -P $$" SIGTERM
+trap "pkill SIGTERM -P $$" SIGTERM
 
 # wait for a single child to finish,
 wait -n
