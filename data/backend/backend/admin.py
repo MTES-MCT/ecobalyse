9fd0302f7379c6d8bbfe32b9123747e673b6a8c0
Christophe Combelles <ccomb@free.fr>
2024-03-19 22:54:59 +0100

    use mailgun instead of scaleway for email. Enable the mailauth user model

--- data/backend/backend/admin.py
+++ data/backend/backend/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin, GroupAdmin
-from django.contrib.auth.models import Group, User
+from django.contrib.auth.admin import GroupAdmin
+from django.contrib.auth.models import Group
+from django.contrib.auth import get_user_model
 
 
 class MyAdminSite(admin.AdminSite):
@@ -11,4 +12,6 @@ class MyAdminSite(admin.AdminSite):
 
 admin_site = MyAdminSite(name="myadmin")
 admin_site.register(Group, GroupAdmin)
-admin_site.register(User, UserAdmin)
+user_model = get_user_model()
+useradmin_model = admin.site._registry[user_model].__class__
+admin_site.register(user_model, useradmin_model)
