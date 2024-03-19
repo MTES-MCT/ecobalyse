bc51f5a0704579c9166d77c4296fd05badae4131
Christophe Combelles <ccomb@free.fr>
2024-03-19 21:10:33 +0100

    adding a (disabled) custom AdminSite

--- data/backend/backend/urls.py
+++ data/backend/backend/urls.py
@@ -14,10 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path, include
+from backend.admin import admin_site
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("admin/", admin_site.urls),
     path("accounts/", include("mailauth.urls")),
 ]
