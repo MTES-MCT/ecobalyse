80a3629ca7ebd711f62a71306dc240f4bf018992
Christophe Combelles <ccomb@free.fr>
2024-03-19 18:10:38 +0100

    enable de mailauth without user model

--- data/backend/reinit.sh
+++ data/backend/reinit.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 rm -f db.sqlite3
 rm -f backend/migrations/*
-python manage.py makemigrations textile
+python manage.py makemigrations mailauth textile
+###python manage.py makemigrations mailauth mailauth_user textile
 python manage.py migrate
 echo "Creating initial data"
 python manage.py shell -c "from textile.init import init; init()"
