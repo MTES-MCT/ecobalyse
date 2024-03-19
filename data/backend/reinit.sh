b2736903e13a07f5c5929215657b4096a6b7d43e
Christophe Combelles <ccomb@free.fr>
2024-03-19 12:54:04 +0100

    missing renaming

--- data/backend/reinit.sh
+++ data/backend/reinit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 rm -f db.sqlite3
 rm -f backend/migrations/*
-python manage.py makemigrations backend
+python manage.py makemigrations textile
 python manage.py migrate
 echo "Creating initial data"
 python manage.py shell -c "from textile.init import init; init()"
