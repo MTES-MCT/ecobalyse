0176d5ba2c4c840e38468327da7b8588339e9dc4
Christophe Combelles <ccomb@free.fr>
2024-03-19 21:24:06 +0100

    Use a custom AdminSite and register lost registrations of Group and User

--- data/backend/backend/admin.py
+++ data/backend/backend/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin, GroupAdmin
+from django.contrib.auth.models import Group, User
 
 
 class MyAdminSite(admin.AdminSite):
@@ -7,4 +9,6 @@ class MyAdminSite(admin.AdminSite):
     index_title = "Ecobalyse administration"
 
 
-admin_site = admin.site  # MyAdminSite(name="myadmin")
+admin_site = MyAdminSite(name="myadmin")
+admin_site.register(Group, GroupAdmin)
+admin_site.register(User, UserAdmin)
