7fa21d23d32d515775aefcb1aa3bbb6700bfff5c
Christophe Combelles <ccomb@free.fr>
2024-03-20 10:09:14 +0100

    enable mailauth.contrib.admin

--- data/backend/backend/admin.py
+++ data/backend/backend/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import GroupAdmin
 from django.contrib.auth.models import Group
 from django.contrib.auth import get_user_model
+from mailauth.contrib.admin.views import AdminLoginView
 
 
 class MyAdminSite(admin.AdminSite):
@@ -11,7 +12,9 @@ class MyAdminSite(admin.AdminSite):
 
 
 admin_site = MyAdminSite(name="myadmin")
+User = get_user_model()
+UserAdmin = admin.site._registry[User].__class__
+admin_site.login = AdminLoginView.as_view(site=admin_site)
+
+admin_site.register(User, UserAdmin)
 admin_site.register(Group, GroupAdmin)
-user_model = get_user_model()
-useradmin_model = admin.site._registry[user_model].__class__
-admin_site.register(user_model, useradmin_model)
