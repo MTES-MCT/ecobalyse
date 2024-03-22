bbc4bcb57606de84404750457b3e732c69b55ca2
Christophe Combelles <ccomb@free.fr>
2024-03-22 17:32:05 +0100

    json login and json register

--- data/backend/authentication/urls.py
+++ data/backend/authentication/urls.py
@@ -5,6 +5,7 @@ from django.urls import include, path, re_path
 
 urlpatterns = [
     path("login/", views.LoginView.as_view(), name="login"),
+    path("login.json/", views.LoginView.as_view(), name="login_json"),
     path("", include("mailauth.urls")),
     path(
         "register/success/",
@@ -12,6 +13,7 @@ urlpatterns = [
         name="registration-requested",
     ),
     path("register/", register, name="register"),
+    path("register.json/", register, name="register_json"),
     re_path(
         "register/(?P<token>.*)$",
         views.Activate.as_view(),
