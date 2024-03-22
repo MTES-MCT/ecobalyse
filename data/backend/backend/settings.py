0e560185a6bd7b1e3d3e0a3509a53769098d49a2
Christophe Combelles <ccomb@free.fr>
2024-03-22 09:27:20 +0100

    set site, page and header titles

--- data/backend/backend/settings.py
+++ data/backend/backend/settings.py
@@ -17,6 +17,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+SITE_NAME = "Ecobalyse"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
