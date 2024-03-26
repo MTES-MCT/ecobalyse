c1c12bf4752a196aad22f9b77075588ea15a6d71
Nicolas Perriault <nicolas@perriault.net>
2024-03-26 18:18:24 +0100

    Update texts and contents.

--- src/Page/Home.elm
+++ src/Page/Home.elm
@@ -148,25 +148,31 @@ calculatorPickerModalContent =
 
 viewInfo : Html Msg
 viewInfo =
-    Container.centered [ id "decouvrir-ecobalyse", class "overlappedImage" ]
-        [ img
-            [ src "img/illustration-score.png"
-            , alt "Une étiquette présentant différents scores d'impact environnemental"
-            ]
-            []
-        , div [ class "d-flex flex-column gap-2" ]
-            [ h2 [] [ text "Un eco-score pour informer les consommateurs" ]
-            , blockquote [ class "d-inline-block fw-bold mx-5 mt-3 mb-2" ]
-                [ p [ class "mb-0" ]
-                    [ text "«\u{00A0}74%\u{00A0}des Français aimeraient avoir plus d’informations sur l’impact environnemental et sociétal des produits qu’ils achètent.\u{00A0}»" ]
-                , Html.cite [ class "fw-normal fs-7 text-muted mb-5" ]
-                    [ text "14ème baromètre de la consommation responsable Greenflex et ADEME - 2021" ]
+    Container.centered [ id "decouvrir-ecobalyse", class "my-5" ]
+        [ div [ class "row" ]
+            [ div [ class "col-md-8 offset-md-2 d-flex flex-column gap-2" ]
+                [ h3 [ class "mb-1" ] [ text "Un coût environnemental pour informer les consommateurs" ]
+                , blockquote [ class "d-inline-block fw-bold fs-5 mx-5 mt-3 mb-2" ]
+                    [ p [ class "mb-0" ]
+                        [ text "«\u{00A0}74%\u{00A0}des Français aimeraient avoir plus d’informations sur l’impact environnemental et sociétal des produits qu’ils achètent.\u{00A0}»" ]
+                    , Html.cite [ class "fw-normal fs-7 text-muted mb-5" ]
+                        [ text "14ème baromètre de la consommation responsable Greenflex et ADEME - 2021" ]
+                    ]
+                , h3 [ class "my-3" ] [ text "Inscrit dans la loi Climat et Résilience de 2021" ]
+                , p [] [ text "“Un affichage destiné à apporter au consommateur une information relative aux impacts environnementaux (...) d'un bien, d'un service ou d'une catégorie de biens ou de services mis sur le marché national est rendu obligatoire” — Article L.541-9-11 du code de l’environnement" ]
+                , h3 [ class "my-3" ] [ text "Les secteurs Textile et Alimentaire, premiers concernés" ]
+                , """Un projet de méthodologie est élaboré à partir de **29 expérimentations** qui se sont tenues
+                     en 2021 et 2022, des travaux d’un **conseil scientifique alimentaire** et d’un **comité d’experts
+                     textile**, de nombreux échanges avec les parties prenantes depuis la mise en ligne d’Ecobalyse…"""
+                    |> Markdown.simple []
+                , """Pour le secteur textile, la méthodologie aujourd’hui présentée est un **premier projet de
+                     référentiel technique**. Elle est soumise à concertation et **toute contribution est [la bienvenue]({url}).**"""
+                    |> String.replace "{url}" Env.communityUrl
+                    |> Markdown.simple []
+                , """Le projet de calculateur pour le produits alimentaires a été temporairement retiré, dans
+                     l’attente de la présentation d’une première proposition de méthode complète."""
+                    |> Markdown.simple []
                 ]
-            , h3 [ class "my-3" ] [ text "Inscrit dans la loi Climat et Résilience de 2021" ]
-            , p [] [ text "“Un affichage destiné à apporter au consommateur une information relative aux impacts environnementaux (...) d'un bien, d'un service ou d'une catégorie de biens ou de services mis sur le marché national est rendu obligatoire” — Article L.541-9-11 du code de l’environnement" ]
-            , h3 [ class "my-3" ] [ text "Les secteurs Textile et Alimentaire, premiers concernés" ]
-            , p [] [ text "Les méthodologies de calcul doivent être définies d’ici fin 2023 pour les produits alimentaires et textiles. Les travaux pour aider à définir une méthodologie de calcul réglementaire sont en cours." ]
-            , p [] [ text "Nous publions les mises à jour et le calendrier pour les secteurs Textile et Alimentaire. D’autres secteurs suivront dans les années à venir." ]
             ]
         ]
 
@@ -196,8 +202,10 @@ viewTools =
                         ]
                         []
                     , div [ class "card-body p-4 pb-0 fs-7" ]
-                        [ h3 [ class "h5 fw-bold" ] [ text "Calculateur d’impacts écologiques" ]
-                        , text "Un calculateur gratuit qui permet d’obtenir les impacts d’un produit sur la base de critères simples et accessibles aux marques."
+                        [ h3 [ class "h5 fw-bold" ] [ text "Calculateur de coût environnemental" ]
+                        , """Un calculateur gratuit qui permet de modéliser le coût environnemental d’un produit, et
+                             donc ses différents impacts, sur la base de critères simples et accessibles aux marques."""
+                            |> Markdown.simple []
                         ]
                     , div [ class "card-footer bg-white border-top-0 text-end fw-bold fs-5 px-4" ] [ text "→" ]
                     ]
@@ -265,24 +273,35 @@ viewContribution =
                     , alt "Picto d'une bulle de conversation"
                     ]
                     []
-                , p [] [ q [] [ text "Tiens, ce chiffre me parait étonnant…" ] ]
-                , p [] [ q [] [ text "Et si on utilisait la surface du tissu plutôt que la masse du vêtement\u{00A0}?" ] ]
-                , p [] [ q [] [ text "Pourquoi l’impact diminue lorsque la production se fait au Myanmar\u{00A0}?" ] ]
+                , p [] [ q [] [ text "Lorsque j’applique la méthode sur mes produits, je suis surpris du résultat\u{00A0}!" ] ]
+                , p [] [ q [] [ text "Comment pourrait-on prendre en compte les labels dont je bénéficie\u{00A0}?" ] ]
+                , p [] [ q [] [ text "Comment dois-je calculer ma largeur de gamme si je suis distribué sur une plateforme\u{00A0}?" ] ]
                 ]
-            , div [ class "col-sm-8 bg-light mt-5 mb-5 p-5" ]
-                [ h3 [ class "h5 fw-bold" ] [ text "Contribuez à améliorer le calcul d’impacts écologiques" ]
-                , p [] [ text "La définition de la méthode de calcul et la mise en œuvre de l’éco-score nécessitent un travail collectif au long cours en relation avec les acteurs de chaque filière." ]
-                , p [] [ text "Vous êtes une marque, un producteur, un bureau d’étude ou un distributeur\u{00A0}:" ]
-                , ul [ class "mb-5" ]
-                    [ li [] [ text "Partagez les données d’impact de votre production," ]
-                    , li [] [ text "Suggérez une amélioration de la méthodologie ou du calculateur," ]
-                    , li [] [ text "Proposez votre participation aux travaux collectifs. " ]
+            , div [ class "col-sm-8 flex-columns gap-5 bg-light mt-5 mb-5 p-5" ]
+                [ h3 [ class "h5 fw-bold mb-3" ] [ text "Contribuez à la concertation en cours sur la méthode de calcul du coût environnemental" ]
+                , """La méthode présentée aujourd’hui dans Ecobalyse est [soumise à concertation]({url}).
+                     Les retours de chacun, positifs ou critiques, seront utiles\u{00A0}!
+
+                     Vous êtes une **marque**, un **producteur**, un **bureau d’étude** ou un **distributeur**\u{00A0}:
+
+- **Faites préciser** les aspects qui devraient l’être
+- **Partagez** les résultats de calcul appliqués à vos produits
+- **Suggérez** une amélioration de la méthodologie ou du calculateur
+                 """
+                    |> String.replace "{url}" Env.communityUrl
+                    |> Markdown.simple []
+                , div [ class "d-flex justify-content-center mt-4 gap-3" ]
+                    [ Link.external
+                        [ class "btn btn-primary"
+                        , href <| Env.communityUrl
+                        ]
+                        [ text "Rejoignez la communauté" ]
+                    , Link.external
+                        [ class "btn btn-outline-primary"
+                        , href "https://fabrique-numerique.gitbook.io/ecobalyse/textile/nous-contacter"
+                        ]
+                        [ text "Contactez l’équipe Écobalyse" ]
                     ]
-                , Link.external
-                    [ class "btn btn-primary"
-                    , href "https://fabrique-numerique.gitbook.io/ecobalyse/textile/nous-contacter"
-                    ]
-                    [ text "Contactez l’équipe Écobalyse" ]
                 ]
             ]
         ]
@@ -294,8 +313,7 @@ view _ { modal } =
     , [ div [ class "d-flex flex-column" ]
             [ div [ class "bg-light pt-5" ]
                 [ viewHero modal ]
-            , div [ class "pt-5" ]
-                [ viewInfo ]
+            , viewInfo
             , div [ class "bg-light pt-5" ]
                 [ viewTools ]
             , div [ class "pt-5" ]
