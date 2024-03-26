b02f279cbd26c7e9f6ba0c461f80f6df5815f792
Christophe Combelles <ccomb@free.fr>
2024-03-26 22:42:55 +0300

    update matieres, countries (#533)
    [story](https://www.notion.so/Int-gration-Ecoinvent-Pays-Transport-Mix-Elec-f4efb95e20264fbbb9254af6db4d8584)
    - updated custom and EI materials
    - updated transports
    - updated countries

--- tests/Data/Textile/SimulatorTest.elm
+++ tests/Data/Textile/SimulatorTest.elm
@@ -47,7 +47,7 @@ suite =
                 [ { tShirtCotonFrance
                     | countrySpinning = Nothing
                   }
-                    |> expectImpact db cch 4.950778937100406
+                    |> expectImpact db cch 4.582286466348852
                     |> asTest "should compute a simulation cch impact"
                 , describe "disabled steps"
                     [ { tShirtCotonFrance | disabledSteps = [ Label.Ennobling ] }
