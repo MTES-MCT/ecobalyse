cef59aa76d62583dc235f50a70534b63730e4704
Christophe Combelles <ccomb@free.fr>
2024-04-02 17:17:13 +0200

    first unit tests for django-only registration

--- data/backend/authentication/tests.py
+++ data/backend/authentication/tests.py
@@ -1,3 +1,34 @@
 from django.test import TestCase
+from django.urls import reverse
+from authentication.views import register
+from django.test import Client
 
-# Create your tests here.
+
+class DjangoAuthenticationTests(TestCase):
+    def test_display_register_form(self):
+        response = self.client.get(reverse("register"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Veuillez vous inscrire")
+
+    def test_register_post(self):
+        response = self.client.post(
+            reverse("register"),
+            {
+                "email": "test@example.com",
+                "first_name": "John",
+                "last_name": "doe",
+                "company": "ACME",
+                "terms_of_use": True,
+                "next": "/",
+            },
+        )
+        # check we redirect to the confirmation page
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(
+            response.headers["Location"], reverse("registration-requested")
+        )
+
+    def test_registration_confirmation(self):
+        response = self.client.get(reverse("registration-requested"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Vérifiez votre boîte e-mail")
