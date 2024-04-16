d7cc4f1ca43d1dcfd632e099c34793d26d32b72f
paulboosz <paulboosz@gmail.com>
2024-04-16 19:11:02 +0200

    visualize file

---
+++ data/common/score_history/visualize.py
@@ -0,0 +1,28 @@
+import pygwalker as pyg
+import pandas as pd
+import streamlit.components.v1 as components
+import streamlit as st
+
+# Adjust the width of the Streamlit page
+st.set_page_config(page_title="Ecobalyse - Suivi des scores", layout="wide")
+
+# Add Title
+st.title("Ecobalyse - Suivi des scores")
+
+# Import your data
+df = pd.read_csv("score_history.csv")
+
+
+def load_config(file_path):
+    with open(file_path, "r") as config_file:
+        config_str = config_file.read()
+    return config_str
+
+
+config = load_config("config.json")
+
+# Generate the HTML using Pygwalker
+pyg_html = pyg.to_html(df, spec=config, dark="light")
+
+# Embed the HTML into the Streamlit app
+components.html(pyg_html, height=1000, scrolling=True)
