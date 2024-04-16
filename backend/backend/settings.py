0a02b13a639a568dc3f341b99bd7e268fd20b2af
Christophe Combelles <ccomb@free.fr>
2024-04-16 12:07:48 +0200

    use join

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -120,7 +120,7 @@ else:
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
+            "NAME": join(BASE_DIR, "db.sqlite3"),
         }
     }
 
