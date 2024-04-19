66a1e882d83835222b2db8e3b8f5c0ed696c582b
Christophe Combelles <ccomb@free.fr>
2024-04-19 12:54:19 +0200

    Show an error if the json (even valid) does not allow to create an example

--- backend/textile/admin.py
+++ backend/textile/admin.py
@@ -1,7 +1,7 @@
 import json
 
 from django import forms
-from django.contrib import admin
+from django.contrib import admin, messages
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import path
@@ -46,16 +46,23 @@ class ExampleAdmin(admin.ModelAdmin):
         ] + super().get_urls()
 
     def from_json(self, request):
+        """/admin/textile/example/from-json/ form"""
         if request.method == "POST":
             form = ExampleJSONForm(request.POST)
             if form.is_valid():
                 json_example = json.loads(request.POST["example"])
-                example = Example._fromJSON(json_example)
-                example.save()
-                for share in json_example["query"]["materials"]:
-                    example.add_material(share)
-                self.message_user(request, "Your Example has been recorded")
-                return HttpResponseRedirect("..")
+                try:
+                    example = Example._fromJSON(json_example)
+                    example.save()
+
+                    for share in json_example["query"]["materials"]:
+                        example.add_material(share)
+                    self.message_user(request, "Your Example has been recorded")
+                    return HttpResponseRedirect("..")
+                except TypeError:
+                    self.message_user(
+                        request, "Your JSON doesn't seem valid", level=messages.ERROR
+                    )
         else:
             form = ExampleJSONForm()
         context = dict(self.admin_site.each_context(request), form=form)
