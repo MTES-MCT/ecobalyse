86ab6b0a71380c62bff35456709ed85f07cdde13
Vincent Jousse <vincent@jousse.org>
2024-06-19 09:23:50 +0200

    fix: remove fake details (#622)

--- backend/processes/views.py
+++ backend/processes/views.py
@@ -3,11 +3,7 @@ from os.path import join
 from authentication.views import is_token_valid
 from django.conf import settings
 from django.http import JsonResponse
-
-# from django.shortcuts import resolve_url
 from django.utils.translation import gettext_lazy as _
-
-# logger = logging.getLogger(__name__)
 
 PUBLIC_FOLDER = join(settings.GITROOT, "public", "data")
 
@@ -25,11 +21,6 @@ with open(join(PUBLIC_FOLDER, "food", "processes_impacts.json"), "r") as f:
 with open(join(PUBLIC_FOLDER, "textile", "processes_impacts.json"), "r") as f:
     textile_processes_detailed = f.read()
 
-with open(join(PUBLIC_FOLDER, "food", "processes_impacts_fake.json"), "r") as f:
-    food_processes_detailed_fake = f.read()
-
-with open(join(PUBLIC_FOLDER, "textile", "processes_impacts_fake.json"), "r") as f:
-    textile_processes_detailed_fake = f.read()
 
 processes_not_detailed = {
     "foodProcesses": food_processes,
@@ -41,31 +32,27 @@ processes_detailed = {
     "textileProcesses": textile_processes_detailed,
 }
 
-processes_detailed_fake = {
-    "foodProcesses": food_processes_detailed_fake,
-    "textileProcesses": textile_processes_detailed_fake,
-}
-
 
 def processes(request):
     token = request.headers.get("token")
-    fakeDetails = request.headers.get("fakeDetails")
-    if token:
-        # Token auth
-        if is_token_valid(token):
-            return JsonResponse(processes_detailed)
-        else:
-            return JsonResponse(
-                {"error": _("This token isn't valid")},
-                status=401,
-            )
+
+    if settings.BYPASS_AUTH:
+        return JsonResponse(processes_detailed)
     else:
-        u = request.user
-        if u.is_authenticated:
-            # Cookie auth
-            return JsonResponse(processes_detailed)
-        elif fakeDetails:
-            return JsonResponse(processes_detailed_fake)
+        if token:
+            # Token auth
+            if is_token_valid(token):
+                return JsonResponse(processes_detailed)
+            else:
+                return JsonResponse(
+                    {"error": _("This token isn't valid")},
+                    status=401,
+                )
         else:
-            # No auth
-            return JsonResponse(processes_not_detailed)
+            u = request.user
+            if u.is_authenticated:
+                # Cookie auth
+                return JsonResponse(processes_detailed)
+            else:
+                # No auth
+                return JsonResponse(processes_not_detailed)
