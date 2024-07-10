0714e206fc626829ccaae45c2d9114428ac8b6a6
Nicolas Perriault <nicolas@perriault.net>
2024-07-10 16:17:30 +0200

    feat(food,ui): render agribalyse process name in ingredient selector.

--- src/Page/Food.elm
+++ src/Page/Food.elm
@@ -705,6 +705,7 @@ createElementSelectorConfig db ingredientQuery { excluded, recipeIngredient, imp
             SetModal (AddIngredientModal (Just recipeIngredient) autocompleteState)
     , toId = .id >> Ingredient.idToString
     , toString = .name
+    , toTooltip = .default >> .name >> Process.nameToString
     , update =
         \_ newElement ->
             UpdateIngredient
