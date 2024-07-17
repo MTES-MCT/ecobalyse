04b5868c5433697dccc2d355df2e909cbfc71c9a
Vincent Jousse <vincent@jousse.org>
2024-07-17 16:41:30 +0200

    feat: checkout ecobalyse-private branch

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -1,1 +1,13 @@
+#!/usr/bin/env bash
+
 git clone git@github.com:MTES-MCT/ecobalyse-private.git
+
+# Scalingo should set the $SOURCE_VERSION variable
+DATA_BRANCH_NAME=$(./bin/extract-data-branch-from-pr.sh $SOURCE_VERSION)
+
+if [ $? -eq 0 ]; then
+  cd ecobalyse-private
+  echo "-> Checkout branch $DATA_BRANCH_NAME of ecobalyse-private"
+  git checkout $DATA_BRANCH_NAME && git pull
+  cd ..
+fi
