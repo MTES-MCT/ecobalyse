b810f6ad8d9eb82e63d451672055c052d0eb0ad7
Vincent Jousse <vincent@jousse.org>
2024-07-18 11:33:14 +0200

    fix: missing import

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const fs = require("fs");
+const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
