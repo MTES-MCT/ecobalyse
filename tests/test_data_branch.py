2f70fde07dc25d9704c298cd8a43170f72518f6c
Vincent Jousse <vincent@jousse.org>
2024-07-23 15:59:37 +0200

    refactor: test bad branch name

--- tests/test_data_branch.py
+++ tests/test_data_branch.py
@@ -1,4 +1,4 @@
-from bin.extract_data_branch_from_pr import extract_branch_name
+from lib.ecobalyse_github import extract_branch_name
 
 
 def test_extract_data_branch_name(client):
@@ -13,3 +13,14 @@ def test_extract_data_branch_name(client):
 
     eaiuiuea""")
     assert branch_name == "test_branch_name"
+
+    branch_name = extract_branch_name(
+        "nstnsnecobalyse_data: test_branch_name/9./_- nstnstn"
+    )
+    assert branch_name == "test_branch_name/9./_-"
+
+    # We should not include invalid characters in the match
+    branch_name = extract_branch_name(
+        "nstnsnecobalyse_data: test_bran<ch_name/9./_- nstnstn"
+    )
+    assert branch_name == "test_bran"
