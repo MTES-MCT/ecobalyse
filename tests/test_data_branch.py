12d217d58f9e3b112e91dbffa18fd5a14daeef62
Vincent Jousse <vincent@jousse.org>
2024-08-13 16:42:45 +0200

    refactor: use python instead of bash to get data branch (#685)

---
+++ tests/test_data_branch.py
@@ -0,0 +1,26 @@
+from ecobalyse.github import extract_branch_name
+
+
+def test_extract_data_branch_name(client):
+    branch_name = extract_branch_name("ecobalyse_data: test_branch_name")
+    assert branch_name == "test_branch_name"
+
+    branch_name = extract_branch_name("nstnsnecobalyse_data: test_branch_name nstnstn")
+    assert branch_name == "test_branch_name"
+
+    branch_name = extract_branch_name("""tnsrtntntsr
+    nstnsnecobalyse_data: test_branch_name nstnstn
+
+    eaiuiuea""")
+    assert branch_name == "test_branch_name"
+
+    branch_name = extract_branch_name(
+        "nstnsnecobalyse_data: test_branch_name/9./_- nstnstn"
+    )
+    assert branch_name == "test_branch_name/9./_-"
+
+    # We should not include invalid characters in the match
+    branch_name = extract_branch_name(
+        "nstnsnecobalyse_data: test_bran<ch_name/9./_- nstnstn"
+    )
+    assert branch_name == "test_bran"
