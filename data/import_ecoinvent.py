d480cc776ab3ab37c2c02dd1298547179c6722bb
Christophe Combelles <ccomb@free.fr>
2024-09-05 18:17:25 +0200

    fixex path

--- data/import_ecoinvent.py
+++ data/import_ecoinvent.py
@@ -1,16 +1,14 @@
 #!/usr/bin/env python3
 
 
-from os.path import join
-
 import bw2data
 import bw2io
 from bw2data.project import projects
 from common.import_ import add_missing_substances, import_simapro_csv
 
 # Ecoinvent
-EI391 = "Ecoinvent3.9.1.CSV.zip"
-EI310 = "Ecoinvent3.10.CSV.zip"
+EI391 = "./Ecoinvent3.9.1.CSV.zip"
+EI310 = "./Ecoinvent3.10.CSV.zip"
 BIOSPHERE = "biosphere3"
 PROJECT = "default"
 
@@ -23,12 +21,12 @@ def main():
     add_missing_substances(PROJECT, BIOSPHERE)
 
     if (db := "Ecoinvent 3.9.1") not in bw2data.databases:
-        import_simapro_csv(join("databases", EI391), db)
+        import_simapro_csv(EI391, db)
     else:
         print(f"{db} already imported")
 
     if (db := "Ecoinvent 3.10") not in bw2data.databases:
-        import_simapro_csv(join("databases", EI310), db)
+        import_simapro_csv(EI310, db)
 
     else:
         print(f"{db} already imported")
