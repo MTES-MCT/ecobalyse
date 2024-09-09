1a01c8acd396580f9156e7243921fe61f5f9977a
Christophe Combelles <ccomb@free.fr>
2024-09-09 10:38:47 +0200

    avoid to pollute cache with root owned files

--- data/docker/entrypoint.sh
+++ data/docker/entrypoint.sh
@@ -3,13 +3,12 @@
 ECOBALYSE_ID=$(ls -lnd /home/jovyan/ecobalyse|awk '{print $3}')
 JOVYAN_ID=$(id -u jovyan)
 
-chown -R 1000:100 "/home/jovyan/.cache"
 if [ $ECOBALYSE_ID -ne $JOVYAN_ID ]; then
     usermod -u $ECOBALYSE_ID jovyan
 fi
 
 pushd /home/jovyan/${ECOBALYSE:=ecobalyse}/data
-pip install -e .
+gosu jovyan pip install -e .
 popd
 
 ldconfig
