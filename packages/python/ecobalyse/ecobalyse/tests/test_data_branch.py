8addea72b8f92e5b9ce16bffe416ca3a910574a4
Vincent Jousse <vincent@jousse.org>
2024-09-09 16:40:44 +0200

    fix: look for ecobalys-private instead of ecobalys_data

--- packages/python/ecobalyse/ecobalyse/tests/test_data_branch.py
+++ packages/python/ecobalyse/ecobalyse/tests/test_data_branch.py
@@ -2,25 +2,27 @@ from ecobalyse.github import extract_branch_name
 
 
 def test_extract_data_branch_name(client):
-    branch_name = extract_branch_name("ecobalyse_data: test_branch_name")
+    branch_name = extract_branch_name("ecobalyse-private: test_branch_name")
     assert branch_name == "test_branch_name"
 
-    branch_name = extract_branch_name("nstnsnecobalyse_data: test_branch_name nstnstn")
+    branch_name = extract_branch_name(
+        "nstnsnecobalyse-private: test_branch_name nstnstn"
+    )
     assert branch_name == "test_branch_name"
 
     branch_name = extract_branch_name("""tnsrtntntsr
-    nstnsnecobalyse_data: test_branch_name nstnstn
+    nstnsnecobalyse-private: test_branch_name nstnstn
 
     eaiuiuea""")
     assert branch_name == "test_branch_name"
 
     branch_name = extract_branch_name(
-        "nstnsnecobalyse_data: test_branch_name/9./_- nstnstn"
+        "nstnsnecobalyse-private: test_branch_name/9./_- nstnstn"
     )
     assert branch_name == "test_branch_name/9./_-"
 
     # We should not include invalid characters in the match
     branch_name = extract_branch_name(
-        "nstnsnecobalyse_data: test_bran<ch_name/9./_- nstnstn"
+        "nstnsnecobalyse-private: test_bran<ch_name/9./_- nstnstn"
     )
     assert branch_name == "test_bran"
