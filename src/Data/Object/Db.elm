8e6331b213759503a6091f888c4ae830c6e3b029
Nicolas Perriault <nicolas@perriault.net>
2024-09-26 12:14:32 +0200

    chore: decode object examples, introduce Query.

--- src/Data/Object/Db.elm
+++ src/Data/Object/Db.elm
@@ -3,24 +3,31 @@ module Data.Object.Db exposing
     , buildFromJson
     )
 
+import Data.Example as Example exposing (Example)
 import Data.Impact as Impact
 import Data.Object.Process as Process exposing (Process)
+import Data.Object.Query as Query exposing (Query)
 import Json.Decode as Decode
 import Result.Extra as RE
 
 
 type alias Db =
-    { processes : List Process }
+    { examples : List (Example Query)
+    , processes : List Process
+    }
 
 
-buildFromJson : String -> Result String Db
-buildFromJson objectProcessesJson =
+buildFromJson : String -> String -> Result String Db
+buildFromJson objectExamplesJson objectProcessesJson =
     objectProcessesJson
         |> Decode.decodeString (Process.decodeList Impact.decodeImpacts)
         |> Result.mapError Decode.errorToString
         |> Result.andThen
             (\processes ->
                 Ok Db
-                    -- FIXME: add more stuff to Db, eg. examples
+                    |> RE.andMap
+                        (objectExamplesJson
+                            |> Example.decodeListFromJsonString Query.decode
+                        )
                     |> RE.andMap (Ok processes)
             )
