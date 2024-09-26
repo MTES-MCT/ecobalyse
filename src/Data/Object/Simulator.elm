1711f583cfbdae6bc7e846114e2085607d20da1c
Nicolas Perriault <nicolas@perriault.net>
2024-09-26 13:16:02 +0200

    feat: add Simulator for objects and tests.

---
+++ src/Data/Object/Simulator.elm
@@ -0,0 +1,26 @@
+module Data.Object.Simulator exposing (compute)
+
+import Data.Impact as Impact exposing (Impacts)
+import Data.Object.Db exposing (Db)
+import Data.Object.Process as Process
+import Data.Object.Query as Query exposing (Item, Query)
+import Quantity
+import Result.Extra as RE
+
+
+compute : Db -> Query -> Result String Impacts
+compute db query =
+    query.items
+        |> List.map (computeItemImpacts db)
+        |> RE.combine
+        |> Result.map Impact.sumImpacts
+
+
+computeItemImpacts : Db -> Item -> Result String Impacts
+computeItemImpacts db { amount, processId } =
+    processId
+        |> Process.findById db.processes
+        |> Result.map
+            (.impacts
+                >> Impact.mapImpacts (\_ -> Quantity.multiplyBy (Query.amountToFloat amount))
+            )
