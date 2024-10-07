1adc1e55bcd7867a5fe4cdff3c45276048c50d02
Nicolas Perriault <nicolas@perriault.net>
2024-10-07 17:12:33 +0200

    fix(api,food): nullable fields weren't nullable anymore.

--- src/Data/Common/DecodeUtils.elm
+++ src/Data/Common/DecodeUtils.elm
@@ -5,8 +5,9 @@ import Json.Decode.Extra as DE
 
 
 {-| A stricter Decode.maybe using Json.Decode.Extra's optionalField here because we want
-a failure when a Maybe decoded field value is invalid.
+a failure when a Maybe decoded field value is invalid, while we still want to treat `null`
+as an acepted value.
 -}
 strictOptional : String -> Decoder a -> Decoder (Maybe a -> b) -> Decoder b
 strictOptional field decoder =
-    DE.andMap (DE.optionalField field decoder)
+    DE.andMap (DE.optionalNullableField field decoder)
