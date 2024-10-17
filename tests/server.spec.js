7d47835e409bbc0db00c696a5de51ed876710ff3
paulboosz <paulboosz@gmail.com>
2024-10-17 18:32:30 +0200

    set tolerance to 1e-12

--- tests/server.spec.js
+++ tests/server.spec.js
@@ -554,11 +554,11 @@ describe("API", () => {
 
           // Add tolerance check for impacts
           Object.entries(impacts).forEach(([key, value]) => {
-            expect(response.body.results.total[key]).toBeCloseTo(value, 6);
+            expect(response.body.results.total[key]).toBeCloseTo(value, 12);
           });
 
           Object.entries(scoring).forEach(([key, value]) => {
-            expect(response.body.results.scoring[key]).toBeCloseTo(value, 6);
+            expect(response.body.results.scoring[key]).toBeCloseTo(value, 12);
           });
         });
       }
