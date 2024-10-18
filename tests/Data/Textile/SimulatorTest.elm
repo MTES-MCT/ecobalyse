08780c11a7161024107fe275b4659605f330bd6e
Christophe Combelles <ccomb@free.fr>
2024-10-18 15:41:23 +0200

    adapted test

--- tests/Data/Textile/SimulatorTest.elm
+++ tests/Data/Textile/SimulatorTest.elm
@@ -50,7 +50,7 @@ suite =
                 [ { tShirtCotonFrance
                     | countrySpinning = Nothing
                   }
-                    |> expectImpact db ecs 1342.8981764232133
+                    |> expectImpact db ecs 1340.2966903797258
                     |> asTest "should compute a simulation ecs impact"
                 , describe "disabled steps"
                     [ { tShirtCotonFrance | disabledSteps = [ Label.Ennobling ] }
