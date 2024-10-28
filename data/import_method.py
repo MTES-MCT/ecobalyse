24a175507ce2276b90a9d962e0d46c208c47acc3
Christophe Combelles <ccomb@free.fr>
2024-10-28 18:47:35 +0100

    try to use the same strategies

--- data/import_method.py
+++ data/import_method.py
@@ -28,11 +28,11 @@ METHODNAME = "Environmental Footprint 3.1 (adapted) patch wtu"  # defined inside
 METHODPATH = join("..", "..", "dbfiles", METHODNAME + ".CSV.zip")
 
 # excluded strategies and migrations
-EXCLUDED_FOOD = [
+EXCLUDED = [
     "normalize_simapro_biosphere_names",
     "normalize_biosphere_names",
     "fix_localized_water_flows",
-    # "simapro-water",
+    "simapro-water",
 ]
 
 
@@ -54,41 +54,15 @@ def import_method(project, datapath=METHODPATH, biosphere=BIOSPHERE):
         unzipped,
         biosphere=biosphere,
         normalize_biosphere=True,
-        # normalize_biosphere=False if project == "textile" else True,
         # normalize_biosphere to align the categories between LCI and LCIA
     )
     os.unlink(unzipped)
     ef.statistics()
 
     # exclude strategies/migrations in EXCLUDED
-    if project == "default":
-        ef.strategies = [
-            s for s in ef.strategies if not any([e in repr(s) for e in EXCLUDED_FOOD])
-        ]
-    if project == "textile":
-        ef.strategies = [
-            normalize_units,
-            set_biosphere_type,
-            # fix_localized_water_flows,  # adding it leads to 60m3
-            drop_unspecified_subcategories,
-            # functools.partial(normalize_biosphere_categories, lcia=True),
-            functools.partial(normalize_simapro_biosphere_names),
-            functools.partial(normalize_biosphere_names),
-            # functools.partial(migrate_exchanges, migration="simapro-water"),
-            normalize_simapro_biosphere_categories,
-            # normalize_simapro_biosphere_names,  # removing avoid multiple CFs
-            functools.partial(
-                link_iterable_by_fields,
-                other=(
-                    obj
-                    for obj in bw2data.Database(ef.biosphere_name)
-                    if obj.get("type") == "emission"
-                ),
-                # fields=("name", "unit", "categories"),
-                kind="biosphere",
-            ),
-            functools.partial(match_subcategories, biosphere_db_name=ef.biosphere_name),
-        ]
+    ef.strategies = [
+        s for s in ef.strategies if not any([e in repr(s) for e in EXCLUDED])
+    ]
     ef.apply_strategies()
 
     # ef.write_excel(METHODNAME)
