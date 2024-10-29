2a5dc8574ddef776434d371ae1054fa81f867321
Christophe Combelles <ccomb@free.fr>
2024-10-29 22:11:23 +0100

    make command to create activities

---
+++ data/create_activities.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+import os
+
+import bw2data
+from common.import_ import add_created_activities
+
+if __name__ == "__main__":
+    """Add additional processes"""
+
+    if "Ecobalyse" in bw2data.databases:
+        del bw2data.databases["Ecobalyse"]
+
+    if (db := "Ecobalyse") not in bw2data.databases:
+        for vertical in ("object", "food", "textile"):
+            file = f"{vertical}/activities_to_create.json"
+            if os.path.exists(file):
+                add_created_activities(db, file)
+    else:
+        print(f"{db} already imported")
