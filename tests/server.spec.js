262315352ce755125f32b47d9bbf4c0449efb8ce
Christophe Combelles <ccomb@free.fr>
2024-11-06 11:22:28 +0100

    changed precision

--- tests/server.spec.js
+++ tests/server.spec.js
@@ -562,11 +562,11 @@ describe("API", () => {
 
           // Add tolerance check for impacts
           Object.entries(impacts).forEach(([key, value]) => {
-            expect(response.body.results.total[key]).toBeCloseTo(value, 12);
+            expect(response.body.results.total[key]).toBeCloseTo(value, 10);
           });
 
           Object.entries(scoring).forEach(([key, value]) => {
-            expect(response.body.results.scoring[key]).toBeCloseTo(value, 12);
+            expect(response.body.results.scoring[key]).toBeCloseTo(value, 10);
           });
         });
       }
