8b96b2ec1c536e1a32384383887469f18f8e8289
Nicolas Perriault <nicolas@perriault.net>
2024-11-26 16:35:11 +0100

    feat: introduce linkable components.

--- src/Data/Object/Component.elm
+++ src/Data/Object/Component.elm
@@ -1,4 +1,11 @@
-module Data.Object.Component exposing (Component)
+module Data.Object.Component exposing
+    ( Component
+    , ComponentItem
+    , ProcessItem
+    , amountToFloat
+    , decodeList
+    , quantityToInt
+    )
 
 import Data.Object.Process as Process exposing (Process)
 import Data.Uuid as Uuid exposing (Uuid)
@@ -15,6 +22,11 @@ type alias Component =
     { id : Id
     , name : String
     , processes : List ProcessItem
+    }
+
+
+type alias ComponentItem =
+    { id : Id
     , quantity : Quantity
     }
 
@@ -38,22 +50,23 @@ amountToFloat (Amount float) =
     float
 
 
-quantity : Int -> Quantity
-quantity int =
-    Quantity int
-
-
-quantityToInt : Quantity -> Int
-quantityToInt (Quantity int) =
-    int
-
-
 decode : Decoder Component
 decode =
     Decode.succeed Component
         |> JDP.required "id" (Decode.map Id Uuid.decoder)
         |> JDP.required "name" Decode.string
         |> JDP.required "processes" (Decode.list decodeProcessItem)
+
+
+decodeList : Decoder (List Component)
+decodeList =
+    Decode.list decode
+
+
+decodeComponentItem : Decoder ComponentItem
+decodeComponentItem =
+    Decode.succeed ComponentItem
+        |> JDP.required "id" (Decode.map Id Uuid.decoder)
         |> JDP.required "quantity" (Decode.map Quantity Decode.int)
 
 
@@ -62,3 +75,48 @@ decodeProcessItem =
     Decode.map2 ProcessItem
         (Decode.field "amount" (Decode.map Amount Decode.float))
         (Decode.field "process_id" Process.decodeId)
+
+
+encode : Component -> Encode.Value
+encode item =
+    Encode.object
+        [ ( "id", item.id |> extractUuid |> Uuid.encoder )
+        , ( "name", item.name |> Encode.string )
+        , ( "processes", item.processes |> Encode.list encodeProcessItem )
+        ]
+
+
+encodeComponentItem : ComponentItem -> Encode.Value
+encodeComponentItem componentItem =
+    Encode.object
+        [ ( "id", componentItem.id |> idToString |> Encode.string )
+        , ( "quantity", componentItem.quantity |> quantityToInt |> Encode.int )
+        ]
+
+
+encodeProcessItem : ProcessItem -> Encode.Value
+encodeProcessItem processItem =
+    Encode.object
+        [ ( "amount", processItem.amount |> amountToFloat |> Encode.float )
+        , ( "process_id", Process.encodeId processItem.processId )
+        ]
+
+
+extractUuid : Id -> Uuid
+extractUuid (Id uuid) =
+    uuid
+
+
+idToString : Id -> String
+idToString (Id uuid) =
+    Uuid.toString uuid
+
+
+quantity : Int -> Quantity
+quantity int =
+    Quantity int
+
+
+quantityToInt : Quantity -> Int
+quantityToInt (Quantity int) =
+    int
