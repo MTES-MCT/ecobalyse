9f921ce7bbb8e12c8af9df0cfa100c3aa59e5d6e
Vincent Jousse <vincent@jousse.org>
2024-12-09 17:38:40 +0100

    fix: deploy needs an old version of `ecobalyse-private`

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -2,8 +2,14 @@
 
 git clone git@github.com:MTES-MCT/ecobalyse-private.git
 
+# Hack to deploy the hotfix here https://github.com/MTES-MCT/ecobalyse/pull/851
+# As he current `ecobalyse-private` repo is not compatible anymore with the code
+# of the 2.7.0
+cd ecobalyse-private
+git checkout 83aa9c8f682e1c9bb874bb13d8b6440f568d7922
+cd ..
 # Scalingo should set the $SOURCE_VERSION variable
-./bin/checkout-ecobalyse-private-branch.sh $SOURCE_VERSION
+# ./bin/checkout-ecobalyse-private-branch.sh $SOURCE_VERSION
 
 ./bin/download_github_releases.py
 
