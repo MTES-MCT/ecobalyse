ce44c450fbd018b816bdb3740ba9332922ceef0f
Nicolas Perriault <nicolas@perriault.net>
2024-12-23 14:40:03 +0100

    refactor: add TestUtils.expectResultWithin.

--- tests/Data/ComponentTest.elm
+++ tests/Data/ComponentTest.elm
@@ -1,29 +1,29 @@
 module Data.ComponentTest exposing (..)
 
 import Data.Component as Component
+import Data.Impact as Impact
+import Data.Impact.Definition as Definition
+import Data.Unit as Unit
 import Expect
 import Json.Decode as Decode
 import Test exposing (..)
 import TestUtils exposing (asTest, suiteWithDb)
 
 
+getEcsImpact : Component.Results -> Float
+getEcsImpact =
+    Component.extractImpacts >> (Impact.getImpact Definition.Ecs >> Unit.impactToFloat)
+
+
 sampleJsonComponentsItems : String
 sampleJsonComponentsItems =
-    """[
-        {
-          "id": "64fa65b3-c2df-4fd0-958b-83965bd6aa08",
-          "quantity": 4
-        },
-        {
-          "id": "ad9d7f23-076b-49c5-93a4-ee1cd7b53973",
-          "quantity": 1
-        },
-        {
-          "id": "eda5dd7e-52e4-450f-8658-1876efc62bd6",
-          "quantity": 1
-        }
-      ]
-      """
+    """
+    [
+      { "id": "64fa65b3-c2df-4fd0-958b-83965bd6aa08", "quantity": 4 },
+      { "id": "ad9d7f23-076b-49c5-93a4-ee1cd7b53973", "quantity": 1 },
+      { "id": "eda5dd7e-52e4-450f-8658-1876efc62bd6", "quantity": 1 }
+    ]
+    """
 
 
 suite : Test
@@ -35,7 +35,8 @@ suite =
                     |> Decode.decodeString (Decode.list Component.decodeComponentItem)
                     |> Result.mapError Decode.errorToString
                     |> Result.andThen (Component.compute db.object)
-                    |> Expect.ok
+                    |> Result.map getEcsImpact
+                    |> TestUtils.expectResultWithin (Expect.Absolute 1) 422
                     |> asTest "should compute results from decoded component items"
                 ]
             ]
