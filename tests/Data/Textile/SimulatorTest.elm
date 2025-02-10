d4dc4096f5b1d66b00e2452423f669de4851271a
Nicolas Perriault <nicolas@perriault.net>
2025-02-10 18:21:50 +0100

    chore: update impl. and tests.

--- tests/Data/Textile/SimulatorTest.elm
+++ tests/Data/Textile/SimulatorTest.elm
@@ -51,7 +51,7 @@ suite =
                 [ { tShirtCotonFrance
                     | countrySpinning = Nothing
                   }
-                    |> expectImpact db ecs 1451.273729407401
+                    |> expectImpact db ecs 1579.78
                     |> asTest "should compute a simulation ecs impact"
                 , describe "disabled steps"
                     [ { tShirtCotonFrance | disabledSteps = [ Label.Ennobling ] }
