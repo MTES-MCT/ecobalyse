55840e91ef5a810f35810f9df07a182b8a9ba2f2
Nicolas Perriault <nicolas@perriault.net>
2025-02-18 10:32:34 +0100

    @vjousse typo review
    Co-authored-by: Vincent Jousse <vincent@jousse.org>

--- src/Data/Textile/Dyeing.elm
+++ src/Data/Textile/Dyeing.elm
@@ -44,7 +44,7 @@ fromString string =
             Ok Discontinuous
 
         _ ->
-            Err <| "Type de teinture inconnu: " ++ string
+            Err <| "Type de teinture inconnu : " ++ string
 
 
 toProcess : WellKnown -> Maybe ProcessType -> Process
