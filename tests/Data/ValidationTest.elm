4b66dca2ed16586fdf19b03c1f170c78207b9fc3
Nicolas Perriault <nicolas@perriault.net>
2025-03-04 18:14:38 +0100

    refactor: introduce optional value validation helper

--- tests/Data/ValidationTest.elm
+++ tests/Data/ValidationTest.elm
@@ -11,27 +11,40 @@ type alias Foo =
     { x : Int, y : Int }
 
 
+type alias Bar =
+    { x : Int, y : Maybe Int }
+
+
 suite : Test
 suite =
     describe "Validation"
-        [ Ok Foo
-            |> Validation.with "x" (Ok 1)
-            |> Validation.with "y" (Ok 2)
-            |> Expect.equal (Ok { x = 1, y = 2 })
-            |> asTest "should validate an Ok result"
-        , Ok Foo
-            |> Validation.with "x" (Ok 1)
-            |> Validation.with "y" (Err "y is bad")
-            |> Expect.equal (Err (Dict.fromList [ ( "y", "y is bad" ) ]))
-            |> asTest "should invalidate exposing an error dict, 1 error"
-        , Ok Foo
-            |> Validation.with "x" (Err "x is bad")
-            |> Validation.with "y" (Ok 2)
-            |> Expect.equal (Err (Dict.fromList [ ( "x", "x is bad" ) ]))
-            |> asTest "should invalidate exposing an error dict, 1 error, whatever the order is"
-        , Ok Foo
-            |> Validation.with "x" (Err "x is bad")
-            |> Validation.with "y" (Err "y is bad")
-            |> Expect.equal (Err (Dict.fromList [ ( "x", "x is bad" ), ( "y", "y is bad" ) ]))
-            |> asTest "should invalidate exposing an error dict, 2 errors"
+        [ describe "required"
+            [ Ok Foo
+                |> Validation.required "x" (Ok 1)
+                |> Validation.required "y" (Ok 2)
+                |> Expect.equal (Ok { x = 1, y = 2 })
+                |> asTest "should validate an Ok result"
+            , Ok Foo
+                |> Validation.required "x" (Ok 1)
+                |> Validation.required "y" (Err "y is bad")
+                |> Expect.equal (Err (Dict.fromList [ ( "y", "y is bad" ) ]))
+                |> asTest "should invalidate exposing an error dict, 1 error"
+            , Ok Foo
+                |> Validation.required "x" (Err "x is bad")
+                |> Validation.required "y" (Ok 2)
+                |> Expect.equal (Err (Dict.fromList [ ( "x", "x is bad" ) ]))
+                |> asTest "should invalidate exposing an error dict, 1 error, whatever the order is"
+            , Ok Foo
+                |> Validation.required "x" (Err "x is bad")
+                |> Validation.required "y" (Err "y is bad")
+                |> Expect.equal (Err (Dict.fromList [ ( "x", "x is bad" ), ( "y", "y is bad" ) ]))
+                |> asTest "should invalidate exposing an error dict, 2 errors"
+            ]
+        , describe "optional"
+            [ Ok Bar
+                |> Validation.required "x" (Ok 1)
+                |> Validation.optional "y" Nothing (always <| Err "y is bad")
+                |> Expect.equal (Ok { x = 1, y = Nothing })
+                |> asTest "should allow optional value"
+            ]
         ]
