fd5ff490062cda522436f06caa22868c3dc2ad00
Nicolas Perriault <nicolas@perriault.net>
2025-04-09 17:59:54 +0200

    chore: clean code

--- src/Request/Component.elm
+++ src/Request/Component.elm
@@ -19,9 +19,7 @@ apiBaseUrl =
 
 deleteComponent : Session -> (WebData String -> msg) -> Component -> Cmd msg
 deleteComponent _ event component =
-    -- curl -X PATCH https://ecobalyse-data-pr65.osc-fr1.scalingo.io/api/components/8ca2ca05-8aec-4121-acaa-7cdcc03150a9 -d '{"name":"Tissu pour joli canapé"}' --header "Content-Type: application/json"
-    -- {"id":"8ca2ca05-8aec-4121-acaa-7cdcc03150a9","name":"Tissu pour joli canapé","elements":[{"amount":1.0,"material":"62a4d6fb-3276-4ba5-93a3-889ecd3bff84","transforms":["9c478d79-ff6b-45e1-9396-c3bd897faa1d","da9d1c32-a166-41ab-bac6-f67aff0cf44a"]},{"amount":1.0,"material":"9dba0e95-0c35-4f8b-9267-62ddf47d4984","transforms":["9c478d79-ff6b-45e1-9396-c3bd897faa1d","ae9cbbad-7982-4f3c-9220-edf27946d347"]}]}
-    -- FIXME: use session to secure access?
+    -- FIXME: use session token to secure access?
     Http.deleteWithConfig { defaultConfig | headers = [] }
         (apiBaseUrl ++ "/components/" ++ Component.idToString component.id)
         event
@@ -30,7 +28,7 @@ deleteComponent _ event component =
 
 getComponents : Session -> (WebData (List Component) -> msg) -> Cmd msg
 getComponents _ event =
-    -- FIXME: use session to secure access?
+    -- FIXME: use session token to secure access?
     Component.decodeList Scope.all
         |> Http.getWithConfig { defaultConfig | headers = [] }
             (apiBaseUrl ++ "/components")
@@ -39,9 +37,7 @@ getComponents _ event =
 
 patchComponent : Session -> (WebData Component -> msg) -> Component -> Cmd msg
 patchComponent _ event component =
-    -- curl -X PATCH https://ecobalyse-data-pr65.osc-fr1.scalingo.io/api/components/8ca2ca05-8aec-4121-acaa-7cdcc03150a9 -d '{"name":"Tissu pour joli canapé"}' --header "Content-Type: application/json"
-    -- {"id":"8ca2ca05-8aec-4121-acaa-7cdcc03150a9","name":"Tissu pour joli canapé","elements":[{"amount":1.0,"material":"62a4d6fb-3276-4ba5-93a3-889ecd3bff84","transforms":["9c478d79-ff6b-45e1-9396-c3bd897faa1d","da9d1c32-a166-41ab-bac6-f67aff0cf44a"]},{"amount":1.0,"material":"9dba0e95-0c35-4f8b-9267-62ddf47d4984","transforms":["9c478d79-ff6b-45e1-9396-c3bd897faa1d","ae9cbbad-7982-4f3c-9220-edf27946d347"]}]}
-    -- FIXME: use session to secure access?
+    -- FIXME: use session token to secure access?
     Http.patchWithConfig { defaultConfig | headers = [] }
         (apiBaseUrl ++ "/components/" ++ Component.idToString component.id)
         event
