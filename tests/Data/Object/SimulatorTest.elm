7c798ba72d09dac4e52c915facc240db77e38105
Christophe Combelles <ccomb@free.fr>
2025-04-23 17:43:23 +0200

    adapt tests

--- tests/Data/Object/SimulatorTest.elm
+++ tests/Data/Object/SimulatorTest.elm
@@ -31,13 +31,13 @@ suite =
                     |> Example.findByName "Chaise"
                     |> Result.andThen (.query >> getEcsImpact db)
                     |> Result.withDefault 0
-                    |> Expect.within (Expect.Absolute 1) 293
+                    |> Expect.within (Expect.Absolute 1) 287
                     |> asTest "should compute impact for an example chair"
                 , db.object.examples
                     |> Example.findByName "Table"
                     |> Result.andThen (.query >> getEcsImpact db)
                     |> Result.withDefault 0
-                    |> Expect.within (Expect.Absolute 1) 3974
+                    |> Expect.within (Expect.Absolute 1) 3880
                     |> asTest "should compute impact for an example table"
                 ]
             ]
