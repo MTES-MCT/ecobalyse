4dbee28bc3ed9a3257747fca7a4a71d16447a41b
Vincent Jousse <vincent@jousse.org>
2025-04-23 17:25:03 +0200

    chore: simplify homepage

--- src/Page/Home.elm
+++ src/Page/Home.elm
@@ -9,6 +9,7 @@ module Page.Home exposing
 
 import Browser.Events
 import Browser.Navigation as Nav
+import Data.Dataset as Dataset exposing (Dataset)
 import Data.Env as Env
 import Data.Key as Key
 import Data.Scope as Scope exposing (Scope)
@@ -81,7 +82,7 @@ viewHero { enabledSections } modal =
                     text ""
                 , if enabledSections.objects then
                     a [ class "btn btn-lg btn-outline-primary", Route.href (Route.ObjectSimulatorHome Scope.Object) ]
-                        [ text "Calculateur objet"
+                        [ text "Calculateur objets"
                         , Html.cite [ class "fw-normal fs-7 d-block" ] [ text "Simulateur en construction" ]
                         ]
 
@@ -104,26 +105,10 @@ viewInfo =
             ]
             []
         , div [ class "d-flex flex-column gap-2" ]
-            [ h3 [ class "mb-1" ] [ text "Un coût environnemental pour informer les consommateurs" ]
-            , blockquote [ class "d-inline-block fw-bold fs-5 mx-5 mt-3 mb-2" ]
-                [ p [ class "mb-0" ]
-                    [ text "«\u{00A0}74%\u{00A0}des Français aimeraient avoir plus d’informations sur l’impact environnemental et sociétal des produits qu’ils achètent.\u{00A0}»" ]
-                , Html.cite [ class "fw-normal fs-7 text-muted mb-5" ]
-                    [ text "14ème baromètre de la consommation responsable Greenflex et ADEME - 2021" ]
-                ]
-            , h3 [ class "my-3" ] [ text "Inscrit dans la loi Climat et Résilience de 2021" ]
-            , p [] [ text "“Un affichage destiné à apporter au consommateur une information relative aux impacts environnementaux (...) d'un bien, d'un service ou d'une catégorie de biens ou de services mis sur le marché national est rendu obligatoire” — Article L.541-9-11 du code de l’environnement" ]
-            , h3 [ class "my-3" ] [ text "Les secteurs Textile et Alimentaire, premiers concernés" ]
-            , """Un projet de méthodologie est élaboré à partir de **29 expérimentations** qui se sont tenues
-                     en 2021 et 2022, des travaux d’un **conseil scientifique alimentaire** et d’un **comité d’experts
-                     textile**, de nombreux échanges avec les parties prenantes depuis la mise en ligne d’Ecobalyse…"""
-                |> Markdown.simple []
-            , """Pour le secteur textile, la méthodologie aujourd’hui présentée est un **premier projet de
-                     référentiel technique**. Elle est soumise à concertation et **toute contribution est [la bienvenue]({url}).**"""
-                |> String.replace "{url}" Env.communityUrl
-                |> Markdown.simple []
-            , """Le projet de calculateur pour le produits alimentaires a été temporairement retiré, dans
-                     l’attente de la présentation d’une première proposition de méthode complète."""
+            [ h3 [ class "mb-1" ] [ text "En savoir plus sur les données et les impacts\u{202F}?" ]
+            , """Vous pouvez en savoir plus sur nos données sources et nos modélisations en vous rendant dans [«\u{202F}l'explorateur\u{202F}»]({url_explorer}). Consultez également le détail des impacts environnementaux de vos simulations en [«\u{202F}créant votre compte Ecobalyse\u{202F}»]({url_account})."""
+                |> String.replace "{url_explorer}" (Route.toString <| Route.Explore Scope.Textile (Dataset.TextileExamples Nothing))
+                |> String.replace "{url_account}" (Route.toString <| Route.Auth { authenticated = False })
                 |> Markdown.simple []
             ]
         ]
@@ -132,129 +117,51 @@ viewInfo =
 viewTools : Session -> Html Msg
 viewTools { enabledSections } =
     Container.centered []
-        [ h4 [ class "fw-normal mb-5 lh-base" ]
-            [ text "Afin d’amorcer la transition vers un modèle de production plus durable"
-            , br [] []
-            , h2 [ class "h4" ] [ text "Écobalyse met à la disposition des entreprises : " ]
-            ]
-        , div [ class "row d-flex mb-5" ]
-            [ div [ class "col-md-4 mb-3 mb-md-0" ]
-                [ div
-                    [ class "card d-flex flex-warp align-content-between text-decoration-none h-100"
-                    , attribute "role" "button"
-                    ]
-                    [ img
-                        [ class "w-100"
-                        , src "img/img_outil_calculateur.png"
-                        , style "width" "450px"
-                        , style "height" "auto"
-                        , style "aspect-ratio" "auto 450 / 254"
-                        , alt "Capture d'écran du calculateur alimentaire"
-                        ]
-                        []
-                    , div [ class "card-body p-4 pb-0 fs-7" ]
-                        [ h3 [ class "h5 fw-bold" ] [ text "Calculateur de coût environnemental" ]
-                        , """Un calculateur gratuit qui permet de modéliser le coût environnemental d’un produit, et
-                             donc ses différents impacts, sur la base de critères simples et accessibles aux marques."""
-                            |> Markdown.simple []
-                        ]
-                    , div [ class "card-footer bg-white border-top-0 text-end fw-bold fs-5 px-4" ] [ text "→" ]
-                    ]
+        [ h3 [ class "mb-2" ] [ text "Les dessous du coût environnemental" ]
+        , """Le coût environnemental s'appuie sur la méthodologie d'analyse du cycle de vie PEF (Product Environmental Footprint) complétée sur les aspects qu'elle ne couvre pas encore. Il est issu du travail des pouvoirs publics (ADEME, Ministère de la transition écologique, ...) en s'appuyant sur des experts et des parties prenantes mobilisées notamment lors de phase de concertation. Ce cadre méthodologique est explicité."""
+            |> String.replace "{url_explorer}" (Route.toString <| Route.Explore Scope.Textile (Dataset.TextileExamples Nothing))
+            |> String.replace "{url_account}" (Route.toString <| Route.Auth { authenticated = False })
+            |> Markdown.simple []
+        , div [ class "d-flex mt-4 gap-3" ]
+            [ Link.external
+                [ class "btn btn-primary"
+                , href <| Env.gitbookUrl
                 ]
-            , div [ class "col-md-4 mb-3 mb-md-0" ]
-                [ a
-                    [ class "card d-flex flex-warp align-content-between text-decoration-none link-dark h-100"
-                    , href Env.gitbookUrl
-                    ]
-                    [ img
-                        [ class "w-100"
-                        , src "img/img_outil_methode.png"
-                        , style "width" "450px"
-                        , style "height" "auto"
-                        , style "aspect-ratio" "auto 450 / 254"
-                        , alt "Capture d'écran de la documentation"
-                        ]
-                        []
-                    , div [ class "card-body p-4 pb-0 fs-7" ]
-                        [ h3 [ class "h5 fw-bold" ] [ text "Support de travail sur la méthode" ]
-                        , p [] [ text "Les orientations présentées participent à la construction de la future méthodologie réglementaire." ]
-                        , div []
-                            [ em [] [ text "Écobalyse, c’est aussi un mode de collaboration ouvert à la critique et aux suggestions, en vue d’aider à élaborer la future méthode réglementaire française (contribuez!)" ]
-                            ]
-                        ]
-                    , div [ class "card-footer bg-white border-top-0 text-end fw-bold fs-5 px-4" ] [ text "→" ]
-                    ]
-                ]
-            , div [ class "col-md-4 mb-3 mb-md-0" ]
-                [ a
-                    [ class "card d-flex flex-warp align-content-between text-decoration-none link-dark h-100"
-                    , href "https://ecobalyse.beta.gouv.fr/#/api"
-                    ]
-                    [ img
-                        [ class "w-100"
-                        , src "img/img_outil_api.png"
-                        , style "width" "450px"
-                        , style "height" "auto"
-                        , style "aspect-ratio" "auto 450 / 254"
-                        , alt "Engrenages représentant une API"
-                        ]
-                        []
-                    , div [ class "card-body p-4 pb-0 fs-7" ]
-                        [ h3 [ class "h5 fw-bold" ] [ text "API ouverte" ]
-                        , div [] [ text "Une interface de programmation applicative (API) permet de connecter le calculateur Écobalyse à tout autre service numérique : gestion d’entreprises (ERP), bases de données de produits (PIM), services SaaS… " ]
-                        ]
-                    , div [ class "card-footer bg-white border-top-0 text-end fw-bold fs-5 px-4" ] [ text "→" ]
-                    ]
-                ]
+                [ text "Consulter la méthodologie du coût environnemental" ]
             ]
         ]
 
 
 viewContribution : Html Msg
 viewContribution =
-    Container.centered [ class "Contribution" ]
-        [ div [ class "row d-flex align-items-start" ]
-            [ div [ class "col-sm-4 bg-info-dark text-white px-3 py-5" ]
-                [ img
-                    [ src "img/picto_bulle.png"
-                    , class "pb-4"
-                    , style "width" "60px"
-                    , style "height" "auto"
-                    , style "aspect-ratio" "auto 60 / 60"
-                    , alt "Picto d'une bulle de conversation"
-                    ]
-                    []
-                , p [] [ q [] [ text "Lorsque j’applique la méthode sur mes produits, je suis surpris du résultat\u{00A0}!" ] ]
-                , p [] [ q [] [ text "Comment pourrait-on prendre en compte les labels dont je bénéficie\u{00A0}?" ] ]
-                , p [] [ q [] [ text "Comment dois-je calculer ma largeur de gamme si je suis distribué sur une plateforme\u{00A0}?" ] ]
+    Container.centered []
+        [ h3 [ class "mb-2" ] [ text "Des questions sur nos outils ou la méthode\u{202F}?" ]
+        , """Ecobalyse est un outil ouvert et gratuit. Vos retours sur notre méthode ou sur notre outil sont riches.""" |> Markdown.simple []
+        , div [ class "d-flex mt-4 gap-3" ]
+            [ Link.external
+                [ class "btn btn-primary"
+                , href <| Env.communityUrl
                 ]
-            , div [ class "col-sm-8 flex-columns gap-5 bg-light mt-5 mb-5 p-5" ]
-                [ h3 [ class "h5 fw-bold mb-3" ] [ text "Contribuez à la concertation en cours sur la méthode de calcul du coût environnemental" ]
-                , """La méthode présentée aujourd’hui dans Ecobalyse est [soumise à concertation]({url}).
-                     Les retours de chacun, positifs ou critiques, seront utiles\u{00A0}!
-
-                     Vous êtes une **marque**, un **producteur**, un **bureau d’étude** ou un **distributeur**\u{00A0}:
-
-- **Faites préciser** les aspects qui devraient l’être
-- **Partagez** les résultats de calcul appliqués à vos produits
-- **Suggérez** une amélioration de la méthodologie ou du calculateur
-                 """
-                    |> String.replace "{url}" Env.communityUrl
-                    |> Markdown.simple []
-                , div [ class "d-flex justify-content-center mt-4 gap-3" ]
-                    [ Link.external
-                        [ class "btn btn-primary"
-                        , href <| Env.communityUrl
-                        ]
-                        [ text "Rejoignez la communauté" ]
-                    , Link.external
-                        [ class "btn btn-outline-primary"
-                        , href "https://fabrique-numerique.gitbook.io/ecobalyse/textile/nous-contacter"
-                        ]
-                        [ text "Contactez l’équipe Écobalyse" ]
-                    ]
+                [ text "Rejoignez la communauté" ]
+            , Link.external
+                [ class "btn btn-outline-primary"
+                , href "https://fabrique-numerique.gitbook.io/ecobalyse/textile/nous-contacter"
                 ]
+                [ text "Contactez l’équipe Écobalyse" ]
             ]
+        ]
+
+
+viewApi : Html Msg
+viewApi =
+    Container.centered []
+        [ h3 [ class "mb-2" ] [ text "Accéder au coût environnemental à travers notre API" ]
+        , """[L'API HTTP Ecobalyse]({api_url}) permet de calculer les impacts environnementaux des différents produits. Elle est expérimentale et donc ne garantit pas de continuité de service à ce stade."""
+            |> String.replace "{api_url}" (Route.toString <| Route.Api)
+            |> Markdown.simple []
+        , """Des questions\u{202F}? Consultez notre [«\u{202F}FAQ API Ecobalyse\u{202F}»]({api_faq_url})."""
+            |> String.replace "{api_faq_url}" (Route.toString <| Route.Editorial "api-faq")
+            |> Markdown.simple []
         ]
 
 
@@ -265,10 +172,12 @@ view session { modal } =
             [ div [ class "bg-light pt-5" ]
                 [ viewHero session modal ]
             , viewInfo
-            , div [ class "bg-light pt-5" ]
+            , div [ class "bg-light pt-5 pb-5" ]
                 [ viewTools session ]
-            , div [ class "pt-5" ]
+            , div [ class "pt-5 mb-5" ]
                 [ viewContribution ]
+            , div [ class "bg-light pt-5 pb-5" ]
+                [ viewApi ]
             ]
       ]
     )
