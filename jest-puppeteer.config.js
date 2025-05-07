ec9a4da3837bb68efd87e0e50fbbd394dcc196f8
Vincent Jousse <vincent@jousse.org>
2025-05-07 17:06:31 +0200

    test: use puppeteer to test JS loading

---
+++ jest-puppeteer.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  launch: {
+    headless: process.env.HEADLESS !== "false",
+  },
+  browserContext: "default", // Use "incognito" if you want isolated sessions per test
+  server: {
+    command: 'concurrently "npm run start:parcel" "npm run server:start"',
+    port: 1234,
+    launchTimeout: 50000,
+    debug: true,
+  },
+};
