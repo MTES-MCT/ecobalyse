34b9f19427c247ddd7035aaac55900e87fde44fa
Nicolas Perriault <nicolas@perriault.net>
2025-05-14 09:25:33 +0200

    feat: user signup form

--- src/Request/Auth2.elm
+++ src/Request/Auth2.elm
@@ -1,5 +1,31 @@
-module Request.Auth2 exposing (..)
+module Request.Auth2 exposing
+    ( requestAuthEmail
+    , signup
+    )
+
+import Data.Session exposing (Session)
+import Data.User2 as User exposing (SignupForm, User)
+import Http
+import Json.Encode as Encode
 
 
-toto =
-    2
+{-| Request an authentication email
+-}
+requestAuthEmail : Session -> (Result Http.Error () -> msg) -> String -> Cmd msg
+requestAuthEmail session event email =
+    Http.post
+        { body = Http.jsonBody <| Encode.object [ ( "email", email |> Encode.string ) ]
+        , expect = Http.expectWhatever event
+        , url = session.backendApiUrl ++ "access/magic_link/login"
+        }
+
+
+{-| Signup a new user
+-}
+signup : Session -> (Result Http.Error User -> msg) -> SignupForm -> Cmd msg
+signup session event signupForm =
+    Http.post
+        { body = Http.jsonBody <| User.encodeSignupForm signupForm
+        , expect = Http.expectJson event User.decodeUser
+        , url = session.backendApiUrl ++ "access/magic_link/signup"
+        }
