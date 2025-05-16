4fa440aed29c57dc6fbdd43e0e8a744c4c06d50c
Nicolas Perriault <nicolas@perriault.net>
2025-05-16 10:13:31 +0200

    feat: introduce custom http handlers for the backend api

--- src/Request/Auth2.elm
+++ src/Request/Auth2.elm
@@ -9,6 +9,7 @@ import Data.Session exposing (Session)
 import Data.User2 as User exposing (AccessTokenData, SignupForm, User)
 import Http
 import Json.Encode as Encode
+import Request.BackendHttp as BackendHttp
 
 
 endpoint : { a | backendApiUrl : String } -> String -> String
@@ -18,32 +19,32 @@ endpoint { backendApiUrl } path =
 
 {-| Request an authentication email
 -}
-askLoginEmail : Session -> (Result Http.Error () -> msg) -> String -> Cmd msg
+askLoginEmail : Session -> (Result BackendHttp.Error () -> msg) -> String -> Cmd msg
 askLoginEmail session event email =
     Http.post
         { body = Http.jsonBody <| Encode.object [ ( "email", email |> Encode.string ) ]
-        , expect = Http.expectWhatever event
+        , expect = BackendHttp.expectApiWhatever event
         , url = endpoint session "access/magic_link/login"
         }
 
 
 {-| Logs the user in
 -}
-login : Session -> (Result Http.Error AccessTokenData -> msg) -> String -> String -> Cmd msg
+login : Session -> (Result BackendHttp.Error AccessTokenData -> msg) -> String -> String -> Cmd msg
 login session event email token =
     Http.get
-        { expect = Http.expectJson event User.decodeAccessTokenData
+        { expect = BackendHttp.expectApiJson event User.decodeAccessTokenData
         , url = endpoint session "access/login" ++ "?email=" ++ email ++ "&token=" ++ token
         }
 
 
 {-| Retrieve user profile
 -}
-profile : Session -> (Result Http.Error User -> msg) -> String -> Cmd msg
+profile : Session -> (Result BackendHttp.Error User -> msg) -> String -> Cmd msg
 profile session event accessToken =
     Http.request
         { body = Http.emptyBody
-        , expect = Http.expectJson event User.decodeUser
+        , expect = BackendHttp.expectApiJson event User.decodeUser
         , headers = [ Http.header "Authorization" ("Bearer " ++ accessToken) ]
         , method = "GET"
         , timeout = Nothing
@@ -54,46 +55,10 @@ profile session event accessToken =
 
 {-| Signup a new user
 -}
-signup : Session -> (Result Http.Error User -> msg) -> SignupForm -> Cmd msg
+signup : Session -> (Result BackendHttp.Error User -> msg) -> SignupForm -> Cmd msg
 signup session event signupForm =
     Http.post
         { body = Http.jsonBody <| User.encodeSignupForm signupForm
-        , expect = Http.expectJson event User.decodeUser
+        , expect = BackendHttp.expectApiJson event User.decodeUser
         , url = endpoint session "access/magic_link/signup"
         }
-
-
-
--- FIXME: we should be able to access error details by inspecting a bad status response json body
--- type alias ErrorResponse =
---     { detail : String, status_code : Int }
--- expectCustomResponse : Decode.Decoder value -> (Result Http.Error value -> msg) -> Http.Expect msg
--- expectCustomResponse decoder toMsg =
---     Http.expectStringResponse toMsg <|
---         \response ->
---             case response of
---                 Http.BadStatus_ metadata body ->
---                     let
---                         errorDecoder =
---                             Decode.map2 ErrorResponse
---                                 (Decode.field "detail" Decode.string)
---                                 (Decode.field "status_code" Decode.int)
---                     in
---                     case Decode.decodeString errorDecoder body of
---                         Err _ ->
---                             Err <| Http.BadStatus metadata.statusCode
---                         Ok decodedError ->
---                             -- How to use decoded error?
---                             Err <| Http.BadStatus metadata.statusCode
---                 Http.BadUrl_ url ->
---                     Err (Http.BadUrl url)
---                 Http.GoodStatus_ _ body ->
---                     case Decode.decodeString decoder body of
---                         Err err ->
---                             Err <| Http.BadBody (Decode.errorToString err)
---                         Ok value ->
---                             Ok value
---                 Http.NetworkError_ ->
---                     Err Http.NetworkError
---                 Http.Timeout_ ->
---                     Err Http.Timeout
