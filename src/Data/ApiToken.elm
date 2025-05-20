1ae1ab5518278a29eb9b65c0bf90a5d7cb742f33
Nicolas Perriault <nicolas@perriault.net>
2025-05-20 18:57:47 +0200

    feat: list api tokens

---
+++ src/Data/ApiToken.elm
@@ -0,0 +1,33 @@
+module Data.ApiToken exposing
+    ( CreatedToken
+    , Token
+    , decodeCreatedToken
+    , decodeToken
+    )
+
+import Data.Common.DecodeUtils as DU
+import Json.Decode as Decode exposing (Decoder)
+import Json.Decode.Pipeline as JDP
+
+
+type alias CreatedToken =
+    { id : String
+    , lastAccessedAt : Maybe String
+    }
+
+
+type Token
+    = Token String
+
+
+decodeToken : Decoder Token
+decodeToken =
+    Decode.map Token Decode.string
+
+
+decodeCreatedToken : Decoder CreatedToken
+decodeCreatedToken =
+    Decode.succeed CreatedToken
+        |> JDP.required "id" Decode.string
+        -- TODO: parse datetime
+        |> DU.strictOptional "lastAccessedAt" Decode.string
