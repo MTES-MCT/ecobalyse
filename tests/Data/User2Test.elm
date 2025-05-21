e9e2d22d4a3cf3125c0165c43182e1a9b5fd8674
Nicolas Perriault <nicolas@perriault.net>
2025-05-21 07:59:42 +0200

    chore: add user tests

--- tests/Data/User2Test.elm
+++ tests/Data/User2Test.elm
@@ -1,6 +1,7 @@
 module Data.User2Test exposing (..)
 
 import Data.User2 as User
+import Dict
 import Expect
 import Json.Decode as Decode
 import Json.Encode as Encode
@@ -27,6 +28,79 @@ suite =
                     |> Result.andThen (Decode.decodeString User.decodeUser)
                     |> Result.map .email
                     |> Expect.equal (Ok "user@tld.org")
+                )
+            ]
+        , describe "validateEmail"
+            [ it "should validate a well-formed email"
+                (User.validateEmail "user@tld.org"
+                    |> Expect.equal Dict.empty
+                )
+            , it "should invalidate an email with no @ symbol"
+                (User.validateEmail "user.tld.org"
+                    |> Expect.equal (Dict.singleton "email" "L'adresse e-mail est invalide")
+                )
+            , it "should invalidate an email with a space"
+                (User.validateEmail "user@tld. org"
+                    |> Expect.equal (Dict.singleton "email" "L'adresse e-mail est invalide")
+                )
+            , it "should invalidate an email with spaces"
+                (User.validateEmail "  @  .  "
+                    |> Expect.equal (Dict.singleton "email" "L'adresse e-mail est invalide")
+                )
+            , it "should invalidate an email without ext"
+                (User.validateEmail "user@tld"
+                    |> Expect.equal (Dict.singleton "email" "L'adresse e-mail est invalide")
+                )
+            , it "should invalidate a totally invalid email"
+                (User.validateEmail "invalid-email"
+                    |> Expect.equal (Dict.singleton "email" "L'adresse e-mail est invalide")
+                )
+            ]
+        , let
+            validForm =
+                { email = "user@tld.org"
+                , firstName = "John"
+                , lastName = "Doe"
+                , organization = "Ecobalyse"
+                , termsAccepted = True
+                }
+          in
+          describe "validateSignupForm"
+            [ it "should validate a well-formed signup form"
+                (User.validateSignupForm validForm
+                    |> Expect.equal Dict.empty
+                )
+            , it "should invalidate a signup form with an invalid email"
+                (User.validateSignupForm { validForm | email = "user.tld.org" }
+                    |> Expect.equal (Dict.singleton "email" "L'adresse e-mail est invalide")
+                )
+            , it "should invalidate a signup form with an empty first name"
+                (User.validateSignupForm { validForm | firstName = "" }
+                    |> Expect.equal (Dict.singleton "firstName" "Le champ est obligatoire")
+                )
+            , it "should invalidate a signup form with an empty last name"
+                (User.validateSignupForm { validForm | lastName = "" }
+                    |> Expect.equal (Dict.singleton "lastName" "Le champ est obligatoire")
+                )
+            , it "should invalidate a signup form with an empty organization"
+                (User.validateSignupForm { validForm | organization = "" }
+                    |> Expect.equal (Dict.singleton "organization" "Le champ est obligatoire")
+                )
+            , it "should invalidate a signup form with termsAccepted set to False"
+                (User.validateSignupForm { validForm | termsAccepted = False }
+                    |> Expect.equal (Dict.singleton "termsAccepted" "Les CGU doivent être acceptées")
+                )
+            , it "should invalidate a signup form with several erroneous field values"
+                (User.validateSignupForm User.emptySignupForm
+                    |> Expect.equal
+                        (Dict.fromList
+                            [ ( "email", "L'adresse e-mail est invalide" )
+                            , ( "firstName", "Le champ est obligatoire" )
+                            , ( "lastName", "Le champ est obligatoire" )
+                            , ( "organization", "Le champ est obligatoire" )
+                            , ( "termsAccepted", "Les CGU doivent être acceptées" )
+                            ]
+                        )
                 )
             ]
         ]
