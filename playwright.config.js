900f9257af8875bb0e67261d1b183430194e986c
Nicolas Perriault <nicolas@perriault.net>
2025-06-03 14:48:38 +0200

    fix: ci

--- playwright.config.js
+++ playwright.config.js
@@ -28,18 +28,10 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
 
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: process.env.CI
-    ? ["github", "html"]
-    : [
-        ["list"],
-        [
-          "html",
-          {
-            open: "never",
-            outputFolder: "playwright-report",
-          },
-        ],
-      ],
+  reporter: [
+    [process.env.CI ? "github" : "list"],
+    ["html", { outputFolder: "playwright-report", open: "never" }],
+  ],
 
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
