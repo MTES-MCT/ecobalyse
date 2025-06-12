686e911e6b11b6e54cc30bef12162bd308ff6bf5
Vincent Jousse <vincent@jousse.org>
2025-06-12 11:43:55 +0200

    feat: return scopes

--- backend/src/app/domain/components/deps.py
+++ backend/src/app/domain/components/deps.py
@@ -7,6 +7,7 @@ from app.lib.deps import create_service_provider
 
 provide_components_service = create_service_provider(
     ComponentService,
+    load=[m.ComponentModel.scopes],
 )
 
 provide_scopes_service = create_service_provider(
