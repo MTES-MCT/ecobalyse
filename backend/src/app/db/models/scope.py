837c4aa0bcb82d0f176d1da7fecbbb70024cba0e
Vincent Jousse <vincent@jousse.org>
2025-06-13 16:19:16 +0200

    feat: add scopes update

--- backend/src/app/db/models/scope.py
+++ backend/src/app/db/models/scope.py
@@ -1,16 +1,18 @@
 from __future__ import annotations
 
+from collections.abc import Hashable
 from typing import TYPE_CHECKING
 
 from advanced_alchemy.base import UUIDAuditBase
-from sqlalchemy import Table
+from advanced_alchemy.mixins import UniqueMixin
+from sqlalchemy import ColumnElement, Table
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 if TYPE_CHECKING:
     from .component import ComponentModel
 
 
-class Scope(UUIDAuditBase):
+class Scope(UUIDAuditBase, UniqueMixin):
     __tablename__ = "scope"
     value: Mapped[str] = mapped_column(unique=True, index=True, nullable=False)
 
@@ -21,6 +23,17 @@ class Scope(UUIDAuditBase):
         secondary=lambda: _component_scope(),
         back_populates="scopes",
     )
+
+    @classmethod
+    def unique_hash(cls, value: str) -> Hashable:
+        return value
+
+    @classmethod
+    def unique_filter(
+        cls,
+        value: str,
+    ) -> ColumnElement[bool]:
+        return cls.value == value
 
 
 def _component_scope() -> Table:
