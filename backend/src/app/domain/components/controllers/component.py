a174d93e3fcc61aaefb723fdbdf5361a97aa851f
Vincent Jousse <vincent@jousse.org>
2025-06-13 16:19:16 +0200

    feat: add scopes update

--- backend/src/app/domain/components/controllers/component.py
+++ backend/src/app/domain/components/controllers/component.py
@@ -51,13 +51,13 @@ class ComponentController(Controller):
     async def list_scopes(
         self,
         scopes_service: ScopeService,
-    ) -> list[Scope]:
+    ) -> list[DbScope]:
         """List scopes."""
         results = await scopes_service.list()
 
         return convert(
             obj=results,
-            type=list[Scope],  # type: ignore[valid-type]
+            type=list[DbScope],  # type: ignore[valid-type]
             from_attributes=True,
         )
 
@@ -73,11 +73,7 @@ class ComponentController(Controller):
             OrderBy(field_name="name", sort_order="asc"), uniquify=True
         )
 
-        return convert(
-            obj=results,
-            type=list[Component],  # type: ignore[valid-type]
-            from_attributes=True,
-        )
+        return components_service.from_list_db_to_response(results)
 
     @post(
         operation_id="CreateComponent",
@@ -89,6 +85,7 @@ class ComponentController(Controller):
         data: ComponentCreate,
         current_user: m.User,
         components_service: ComponentService,
+        scopes_service: ScopeService,
     ) -> Component:
         """Create a component."""
 
@@ -97,10 +94,10 @@ class ComponentController(Controller):
 
         component = await components_service.create(data=data)
 
-        component_with_scopes = await components_service.get_one(id=component.id)
-        return components_service.to_schema(
-            component_with_scopes, schema_type=Component
-        )
+        # Force reload from db to get scopes
+        created_component = await components_service.get_one(id=component.id)
+
+        return components_service.from_db_to_response(created_component)
 
     @patch(
         operation_id="UpdateComponent",
@@ -125,9 +122,8 @@ class ComponentController(Controller):
         component = await components_service.update(item_id=component_id, data=data)
 
         component_with_scopes = await components_service.get_one(id=component.id)
-        return components_service.to_schema(
-            component_with_scopes, schema_type=Component
-        )
+
+        return components_service.from_db_to_response(component_with_scopes)
 
     @delete(
         operation_id="DeleteComponent",
@@ -160,7 +156,7 @@ class ComponentController(Controller):
         """Get a component."""
 
         component = await components_service.get(component_id)
-        return components_service.to_schema(component, schema_type=Component)
+        return components_service.from_db_to_response(component)
 
     @patch(
         operation_id="BulkUpdateComponent",
@@ -213,8 +209,4 @@ class ComponentController(Controller):
             OrderBy(field_name="name", sort_order="asc"), uniquify=True
         )
 
-        return convert(
-            obj=updated_components,
-            type=list[Component],  # type: ignore[valid-type]
-            from_attributes=True,
-        )
+        return components_service.from_list_db_to_response(updated_components)
