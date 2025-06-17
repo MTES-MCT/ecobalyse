32426e9b47a6e13391098af4c1b496332d33634d
Vincent Jousse <vincent@jousse.org>
2025-06-17 16:33:01 +0200

    fix: avoid SA warning abou component not present in session
    see https://github.com/sqlalchemy/sqlalchemy/discussions/12049

--- backend/src/app/db/models/scope.py
+++ backend/src/app/db/models/scope.py
@@ -1,28 +1,16 @@
 from __future__ import annotations
 
 from collections.abc import Hashable
-from typing import TYPE_CHECKING
 
 from advanced_alchemy.base import UUIDAuditBase
 from advanced_alchemy.mixins import UniqueMixin
 from sqlalchemy import ColumnElement, Table
-from sqlalchemy.orm import Mapped, mapped_column, relationship
-
-if TYPE_CHECKING:
-    from .component import ComponentModel
+from sqlalchemy.orm import Mapped, mapped_column
 
 
 class Scope(UUIDAuditBase, UniqueMixin):
     __tablename__ = "scope"
     value: Mapped[str] = mapped_column(unique=True, index=True, nullable=False)
-
-    # -----------
-    # ORM Relationships
-    # ------------
-    components: Mapped[list[ComponentModel]] = relationship(
-        secondary=lambda: _component_scope(),
-        back_populates="scopes",
-    )
 
     @classmethod
     def unique_hash(cls, value: str) -> Hashable:
