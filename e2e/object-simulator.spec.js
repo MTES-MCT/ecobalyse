678aaddf8d2018ce9d9641eef534f98ca4fc5059
Nicolas Perriault <nicolas@perriault.net>
2025-06-22 14:48:12 +0200

    feat: add object basic e2e test

---
+++ e2e/object-simulator.spec.js
@@ -0,0 +1,37 @@
+import { test, expect } from "@playwright/test";
+
+test("Object simulator", async ({ page }) => {
+  await page.goto("/");
+  await page.getByLabel("Menu principal").getByRole("link", { name: "Objets" }).click();
+  await page.getByRole("button", { name: "Ajouter un composant" }).click();
+  await page.getByRole("option", { name: "Dossier plastique (PP)" }).click();
+  await page.getByRole("button", { name: "▶" }).click();
+  await page
+    .getByRole("row", { name: "▼ Dossier plastique (PP) 0," })
+    .getByRole("spinbutton")
+    .fill("2");
+  await page
+    .getByRole("row", { name: "kg Plastique granulé (PP) 0," })
+    .getByRole("spinbutton")
+    .fill("0.734065");
+  await page.getByRole("button", { name: "Plastique granulé (PP)" }).click();
+  await page.getByRole("option", { name: "Fibre de viscose" }).click();
+  await page.getByRole("button", { name: "Ajouter une transformation" }).click();
+  await page.getByRole("option", { name: "Lavage (fibres synthétiques)" }).click();
+  await page.getByRole("button", { name: "Ajouter une transformation" }).click();
+  await page.getByRole("option", { name: "Mercerisage" }).click();
+  await page.getByRole("button", { name: "Ajouter un élément" }).click();
+  await page.getByRole("option", { name: "Bois d'industrie" }).click();
+  await page.getByRole("button", { name: "Ajouter une transformation" }).nth(1).click();
+  await page.getByRole("option", { name: "Désencollage" }).click();
+  await page.getByRole("button", { name: "Ajouter un composant" }).click();
+  await page.getByRole("option", { name: "Assise plastique (PP)" }).click();
+  await page.getByRole("button", { name: "▶" }).click();
+  await page.getByRole("button", { name: "Ajouter une transformation" }).nth(2).click();
+  await page.getByRole("option", { name: "Moulage par injection" }).click();
+
+  await expect(page.getByTestId("score-card")).toBeVisible();
+
+  await expect(page.getByRole("row", { name: "Matières premières 70,20 %" })).toBeVisible();
+  await expect(page.getByRole("row", { name: "Transformation 29,80 %" })).toBeVisible();
+});
