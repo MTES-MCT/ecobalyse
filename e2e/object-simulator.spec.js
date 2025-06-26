dfb7aa4be32637fae947b58c81c70ffd881ce5c3
Nicolas Perriault <nicolas@perriault.net>
2025-06-26 17:29:30 +0200

    fix: update e2e tests

--- e2e/object-simulator.spec.js
+++ e2e/object-simulator.spec.js
@@ -3,32 +3,29 @@ import { test, expect } from "@playwright/test";
 test("Object simulator", async ({ page }) => {
   await page.goto("/");
   await page.getByLabel("Menu principal").getByRole("link", { name: "Objets" }).click();
+
+  // Add a component to the simulation
   await page.getByRole("button", { name: "Ajouter un composant" }).click();
   await page.getByRole("option", { name: "Dossier plastique (PP)" }).click();
+
+  // Open details
   await page.getByRole("button", { name: "▶" }).click();
-  await page
-    .getByRole("row", { name: "▼ Dossier plastique (PP) 0," })
-    .getByRole("spinbutton")
-    .fill("2");
-  await page
-    .getByRole("row", { name: "kg Plastique granulé (PP) 0," })
-    .getByRole("spinbutton")
-    .fill("0.734065");
+
+  // Change default element material, select steel
   await page.getByRole("button", { name: "Plastique granulé (PP)" }).click();
-  await page.getByRole("option", { name: "Fibre de viscose" }).click();
+  await page.getByRole("option", { name: "Acier (non allié)" }).click();
+
+  // Add a transformation to the steel element
   await page.getByRole("button", { name: "Ajouter une transformation" }).click();
-  await page.getByRole("option", { name: "Moulage par injection" }).click();
-  await page.getByRole("button", { name: "Ajouter une transformation" }).click();
-  await page.getByRole("option", { name: /Transformation plastique/ }).click();
+  await page.getByRole("option", { name: "Transformation métallique (" }).click();
+
+  // Add a second element, select plastic
   await page.getByRole("button", { name: "Ajouter un élément" }).click();
-  await page.getByRole("option", { name: "Acier", exact: true }).click();
+  await page.getByRole("option", { name: "Production de PET, granulés," }).click();
+
+  // Add a textile material only transformation
   await page.getByRole("button", { name: "Ajouter une transformation" }).nth(1).click();
-  await page.getByRole("option", { name: "Transformation métallique (moyenne)" }).click();
-  await page.getByRole("button", { name: "Ajouter un composant" }).click();
-  await page.getByRole("option", { name: "Assise plastique (PP)" }).click();
-  await page.getByRole("button", { name: "▶" }).click();
-  await page.getByRole("button", { name: "Ajouter une transformation" }).nth(2).click();
-  await page.getByRole("option", { name: "Moulage par injection" }).click();
+  await page.getByRole("option", { name: "Filage (40 Nm)" }).click();
 
   await expect(page.getByTestId("score-card")).toBeVisible();
 
