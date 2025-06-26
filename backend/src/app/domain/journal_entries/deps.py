a91fd0cda4eb1ab7736e19d75e7cdd2b8ab57410
Vincent Jousse <vincent@jousse.org>
2025-06-26 17:39:30 +0200

    chore: optimize DB query

--- backend/src/app/domain/journal_entries/deps.py
+++ backend/src/app/domain/journal_entries/deps.py
@@ -3,12 +3,12 @@ from __future__ import annotations
 from app.db import models as m
 from app.domain.journal_entries.services import JournalEntryService
 from app.lib.deps import create_service_provider
-from sqlalchemy.orm import joinedload, selectinload
+from sqlalchemy.orm import joinedload
 
 provide_journal_entries_service = create_service_provider(
     JournalEntryService,
     load=[
-        selectinload(m.JournalEntry.user).options(
+        joinedload(m.JournalEntry.user, innerjoin=True).options(
             joinedload(m.User.profile, innerjoin=True)
         ),
     ],
