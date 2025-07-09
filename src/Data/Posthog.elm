1564efc18068580371e740edfb3edf66b364726e
Nicolas Perriault <nicolas@perriault.net>
2025-07-09 17:23:14 +0200

    feat: collect authn events

--- src/Data/Posthog.elm
+++ src/Data/Posthog.elm
@@ -8,7 +8,12 @@ import Ports
 
 
 type Event
-    = PageView
+    = AuthApiTokenCreated
+    | AuthLoginOK
+    | AuthMagicLinkSent
+    | AuthProfileUpdated
+    | AuthSignup
+    | PageView
     | SelectDetailedImpact Trigram
 
 
@@ -16,10 +21,26 @@ send : Event -> Cmd msg
 send event =
     Ports.sendPosthogEvent <|
         case event of
+            AuthApiTokenCreated ->
+                { name = "AuthApiTokenCreated", properties = [] }
+
+            AuthLoginOK ->
+                { name = "AuthLoginOK", properties = [] }
+
+            AuthMagicLinkSent ->
+                { name = "AuthMagicLinkSent", properties = [] }
+
+            AuthProfileUpdated ->
+                { name = "AuthProfileUpdated", properties = [] }
+
+            AuthSignup ->
+                { name = "AuthSignup", properties = [] }
+
             PageView ->
+                --  Note: $pageview is a special event handled by posthog
                 { name = "$pageview", properties = [] }
 
             SelectDetailedImpact trigram ->
-                { name = "select_detailed_impact"
+                { name = "SelectDetailedImpact"
                 , properties = [ ( "trigram", Definition.toString trigram ) ]
                 }
