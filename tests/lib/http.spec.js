cb069bf5e04b1e6dc12ae3109d09083f5a3fb7c1
Nicolas Perriault <nicolas@perriault.net>
2025-07-16 16:51:30 +0200

    refactor: improve impl and tests

--- tests/lib/http.spec.js
+++ tests/lib/http.spec.js
@@ -3,19 +3,29 @@ const { createCSPDirectives, extractTokenFromHeaders } = require("../../lib/http
 describe("lib.http", () => {
   describe("createCSPDirectives", () => {
     test("should create a CSP directives object", () => {
-      const env = {
+      const sampleEnv = {
         MATOMO_HOST: "matomo.example.com",
         POSTHOG_HOST: "https://posthog.example.com",
       };
-      const directives = createCSPDirectives(env);
-      expect(directives["frame-src"]).toEqual(["'self'", "https://matomo.example.com"]);
+      const directives = createCSPDirectives(sampleEnv);
       expect(directives["connect-src"]).toEqual(["'self'", "https://posthog.example.com"]);
+      expect(directives["frame-src"]).toEqual(["'self'", "https://matomo.example.com"]);
       expect(directives["script-src"]).toEqual([
         "'self'",
         "'unsafe-inline'",
         "https://matomo.example.com",
         "https://posthog.example.com",
       ]);
+      expect(directives["worker-src"]).toEqual(["'self'", "https://posthog.example.com"]);
+    });
+
+    test("should create a CSP directives object with no tracker hosts", () => {
+      const sampleEnv = {};
+      const directives = createCSPDirectives(sampleEnv);
+      expect(directives["connect-src"]).toEqual(["'self'"]);
+      expect(directives["frame-src"]).toEqual(["'self'"]);
+      expect(directives["script-src"]).toEqual(["'self'", "'unsafe-inline'"]);
+      expect(directives["worker-src"]).toEqual(["'self'"]);
     });
   });
 
