f28757fd1f9d1ca482267fcab6b4c88e20964ca8
Nicolas Perriault <nicolas@perriault.net>
2025-07-19 13:05:35 +0200

    chore: improve ui/ux

--- src/Views/Admin.elm
+++ src/Views/Admin.elm
@@ -2,15 +2,15 @@ module Views.Admin exposing (header)
 
 import Html exposing (..)
 import Html.Attributes exposing (..)
-import Page.Admin.Section as Section exposing (Section)
+import Page.Admin.Section as AdminSection exposing (Section(..))
 import Route exposing (Route)
 
 
 all : List ( Section, Route, Bool )
 all =
-    [ ( Section.AccountSection, Route.Admin Section.AccountSection, True )
-    , ( Section.ComponentSection, Route.Admin Section.ComponentSection, True )
-    , ( Section.ProcessSection, Route.Admin Section.ProcessSection, False )
+    [ ( AccountSection, Route.Admin AccountSection, True )
+    , ( ComponentSection, Route.Admin ComponentSection, True )
+    , ( ProcessSection, Route.Admin ProcessSection, False )
     ]
 
 
@@ -20,7 +20,7 @@ header currentSection =
         [ div [ class "col-md-6 col-lg-8" ]
             [ h1 [ class "mb-0" ]
                 [ text "Administration"
-                , small [ class "h3 text-muted" ] [ text <| " des " ++ toString currentSection ]
+                , small [ class "h3 text-muted" ] [ text <| " des " ++ AdminSection.toLabel currentSection ]
                 ]
             ]
         , div [ class "col-md-6 col-lg-4 d-flex justify-content-end align-items-end" ]
@@ -43,23 +43,10 @@ menu currenSection =
                         ]
                     , Route.href route
                     ]
-                    [ text (toString section) ]
+                    [ text (AdminSection.toLabel section) ]
             )
         |> nav
             [ class "btn-group w-100 w-md-auto mt-2"
             , attribute "role" "group"
             , attribute "aria-label" "Sections du back-office"
             ]
-
-
-toString : Section -> String
-toString section =
-    case section of
-        Section.AccountSection ->
-            "Comptes"
-
-        Section.ComponentSection ->
-            "Composants"
-
-        Section.ProcessSection ->
-            "Procédés (à venir)"
