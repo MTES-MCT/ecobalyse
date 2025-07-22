133f07b6849a21aec438e57abab4b699e6310425
paulboosz <paulboosz@gmail.com>
2025-07-22 14:55:34 +0200

    fix: update test with impact change

--- tests/Data/Textile/SimulatorTest.elm
+++ tests/Data/Textile/SimulatorTest.elm
@@ -51,7 +51,7 @@ suite =
                 [ { tShirtCotonFrance
                     | countrySpinning = Nothing
                   }
-                    |> expectImpact db ecs 1228.3951
+                    |> expectImpact db ecs 1199.3593
                     |> asTest "should compute a simulation ecs impact"
                 , describe "disabled steps"
                     [ { tShirtCotonFrance | disabledSteps = [ Label.Ennobling ] }
