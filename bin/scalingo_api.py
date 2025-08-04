4c21bac4567b102a98af5a6e39ffd579a1f2ad36
Vincent Jousse <vincent@jousse.org>
2025-08-04 15:56:06 +0200

    feat: download logs from date range

--- bin/scalingo_api.py
+++ bin/scalingo_api.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
 import logging
 import os
+from datetime import UTC, datetime, timedelta
 from enum import Enum
+from pathlib import Path
+from typing import Optional
 
+import requests
 import typer
 from ecobalyse import logging_config as logging_config
 from ecobalyse import scalingo
@@ -18,7 +22,7 @@ class LoggingLevels(str, Enum):
     WARNING = "WARNING"
 
 
-app = typer.Typer(help="Interact with the scalingo HTTP API")
+app = typer.Typer(help="Interact with the scalingo HTTP API")
 
 logger = logging.getLogger(__name__)
 
@@ -33,15 +37,43 @@ def main(
 
 
 @app.command()
-def log_archives():
+def log_archives(
+    download_dir: Annotated[
+        Optional[Path],
+        typer.Argument(help="The output CSV file."),
+    ] = Path("./archives_logs"),
+):
     """
     Get the list of archived logs
     """
 
+    # Create dir if it doesn’t exist
+    os.makedirs(download_dir, exist_ok=True)
+
+    now = datetime.now(UTC)
     api_token = os.getenv("SCALINGO_API_TOKEN")
     bearer_token = scalingo.get_bearer_token(api_token=api_token)
-    logs = scalingo.list_logs_archives(bearer_token=bearer_token)
-    print(logs)
+
+    archives = scalingo.list_logs_archives_for_range(
+        start_date=now, end_date=now - timedelta(days=14), bearer_token=bearer_token
+    )
+
+    logger.info(f"-> Got {len(archives)} archives")
+
+    for archive in archives:
+        url = archive["url"]
+
+        filename = url.rsplit("/", 1)[1].rsplit("?", 1)[0]
+        dest_file_path = os.path.join(download_dir, filename)
+
+        # Download only if file is not present on disk
+        if not Path(dest_file_path).is_file():
+            response = requests.get(url, allow_redirects=True)
+            open(dest_file_path, "wb").write(response.content)
+        else:
+            logger.info(
+                f"File `{filename}` already present in `{download_dir}`, skipping download"
+            )
 
 
 if __name__ == "__main__":
--- packages/python/ecobalyse/ecobalyse/scalingo.py
+++ packages/python/ecobalyse/ecobalyse/scalingo.py
@@ -1,6 +1,8 @@
 import logging
+from datetime import datetime
 
 import requests
+from dateutil import parser
 from requests.auth import HTTPBasicAuth
 
 # Tell ruff to not delete the unused import by rexporting it using as
@@ -20,10 +22,17 @@ def get_bearer_token(api_token: str) -> str:
     return response.json()["token"]
 
 
-def list_logs_archives(bearer_token: str, application: str = "ecobalyse") -> dict:
-    logging.info("-> Listing log archives")
+def parse_archive_datetime(date_string: str) -> datetime:
+    date_string_clean: str = date_string.replace(" UTC", "")
+    return parser.parse(date_string_clean)
 
-    endpoint = f"https://api.osc-fr1.scalingo.com/v1/apps/{application}/logs_archives"
+
+def list_logs_archives(
+    bearer_token: str, cursor: str = "1", application: str = "ecobalyse"
+) -> dict:
+    logging.info(f"-> Listing log archives for cursor {cursor}")
+
+    endpoint = f"https://api.osc-fr1.scalingo.com/v1/apps/{application}/logs_archives?cursor={cursor}"
     headers = {
         "Accept": "application/json",
         "Authorization": f"Bearer {bearer_token}",
@@ -31,3 +40,52 @@ def list_logs_archives(bearer_token: str, application: str = "ecobalyse") -> dic
     }
     response = requests.get(endpoint, headers=headers)
     return response.json()
+
+
+def list_logs_archives_for_range(
+    start_date: datetime,
+    end_date: datetime,
+    bearer_token: str,
+    application: str = "ecobalyse",
+) -> list[dict]:
+    logging.info(f"-> Listing log archives from {start_date} to {end_date}")
+
+    cursor = 1
+    archives_logs = list_logs_archives(bearer_token=bearer_token, cursor=cursor)
+    archives = archives_logs["archives"]
+
+    if len(archives) == 0:
+        logger.info("-> No more archives, returning")
+        return
+
+    first_archive = archives[0]
+    first_archive_from_date = parse_archive_datetime(first_archive["from"])
+
+    last_archive = archives[-1]
+    last_archive_to_date = parse_archive_datetime(last_archive["to"])
+
+    while first_archive_from_date > start_date and archives_logs["has_more"]:
+        cursor = archives_logs["next_cursor"]
+        archives_logs = list_logs_archives(bearer_token=bearer_token, cursor=cursor)
+        archives = archives_logs["archives"]
+
+        if len(archives) == 0:
+            logger.info("-> No more archives, returning")
+            return
+
+        first_archive = archives[0]
+        first_archive_from_date = parse_archive_datetime(first_archive["from"])
+
+    while last_archive_to_date > end_date and archives_logs["has_more"]:
+        cursor = archives_logs["next_cursor"]
+        archives_logs = list_logs_archives(bearer_token=bearer_token, cursor=cursor)
+        archives += archives_logs["archives"]
+
+        if len(archives) == 0:
+            logger.info("-> No more archives, returning")
+            return
+
+        last_archive = archives[-1]
+        last_archive_to_date = parse_archive_datetime(last_archive["to"])
+
+    return archives
