d444f525036292c9503e3243f71014d187dc2af9
Nicolas Perriault <nicolas@perriault.net>
2025-09-16 14:55:00 +0200

    feat(object,ui): add support for component comments

--- src/Page/Explore/Components.elm
+++ src/Page/Explore/Components.elm
@@ -90,6 +90,10 @@ table db { detailed, scope } =
                                     )
                                 |> ul [ class "m-0 px-2" ]
           }
+        , { label = "Commentaire"
+          , toValue = Table.StringValue .comment
+          , toCell = .comment >> text
+          }
         , { label = "Coût environnemental"
           , toValue = Table.FloatValue <| getComponentEcoscore scopedProcesses >> Result.withDefault 0
           , toCell =
