b3a7f972fb20b58f21e779b20fa99420539a09b8
Nicolas Perriault <nicolas@perriault.net>
2025-10-09 16:40:44 +0200

    fix: validate version number param (#1426)
    Fixes #1424

--- e2e/versions.spec.js
+++ e2e/versions.spec.js
@@ -6,10 +6,10 @@ test.beforeEach(async ({ page }) => {
   await page.route("https://api.github.com/repos/MTES-MCT/ecobalyse/releases", async (route) => {
     const json = [
       {
-        html_url: "https://github.com/MTES-MCT/ecobalyse/releases/tag/test_build",
-        tag_name: "test_build",
+        html_url: "https://github.com/MTES-MCT/ecobalyse/releases/tag/v0.0.1",
+        tag_name: "v0.0.1",
         target_commitish: "master",
-        name: "test_build",
+        name: "v0.0.1",
         draft: false,
         prerelease: false,
         body: "",
@@ -31,7 +31,7 @@ test.describe("versions", () => {
 
     const options = page.getByTestId("version-selector").locator("option");
 
-    await expect(options).toHaveText(["Unreleased", "test_build"]);
+    await expect(options).toHaveText(["Unreleased", "v0.0.1"]);
   });
 
   test("should redirect to the correct page version", async ({ page }) => {
@@ -40,10 +40,10 @@ test.describe("versions", () => {
     await page.goto("/");
 
     // Single selection matching the value or label
-    await page.getByTestId("version-selector").selectOption("test_build");
-    await page.waitForURL("**/versions/test_build/#");
+    await page.getByTestId("version-selector").selectOption("v0.0.1");
+    await page.waitForURL("**/versions/v0.0.1/#");
 
-    expect(page.url()).toBe("http://localhost:1234/versions/test_build/#");
+    expect(page.url()).toBe("http://localhost:1234/versions/v0.0.1/#");
 
     await expect(page.getByText("Calculer l’impact de l’alimentation")).toBeVisible();
 
