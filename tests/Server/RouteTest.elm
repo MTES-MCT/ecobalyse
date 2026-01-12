9b6843806d0568e299ef7762d229068bb81e8b9c
Vincent Jousse <vincent@jousse.org>
2026-01-12 16:55:49 +0100

    chore: tests should compile

--- tests/Server/RouteTest.elm
+++ tests/Server/RouteTest.elm
@@ -4,7 +4,7 @@ import Data.Component as Component
 import Data.Country as Country
 import Data.Example as Example
 import Data.Food.Preparation as Preparation
-import Data.Food.Query as FoodQuery
+import Data.Food.Query as FoodQuery exposing (PackagingAmount(..))
 import Data.Split as Split
 import Data.Textile.Material as Material
 import Data.Textile.Product as Product
@@ -93,7 +93,7 @@ foodEndpoints db =
                     |> asTest "validate a transform mass"
                 , FoodQuery.encode
                     { royalPizza
-                        | packaging = royalPizza.packaging |> List.map (\p -> { p | mass = Mass.grams -1 })
+                        | packaging = royalPizza.packaging |> List.map (\p -> { p | amount = MassAmount <| Mass.grams -1 })
                     }
                     |> testFoodEndpoint db
                     |> expectFoodValidationError "packaging" "La masse doit être supérieure à zéro"
