946123d95b42321fc0e7736b5ebec77a99602551
Christophe Combelles <ccomb@free.fr>
2026-02-09 18:19:44 +0100

    Revert "adapt tests"
    This reverts commit 08855fe6e0c8bf8d37516f2c358ad169570ac8c3.

--- tests/Data/Food/RecipeTest.elm
+++ tests/Data/Food/RecipeTest.elm
@@ -144,7 +144,7 @@ suite =
 
                                     Ok result ->
                                         Unit.impactToFloat result
-                                            |> Expect.within (Expect.Absolute 0.1) 138.2
+                                            |> Expect.within (Expect.Absolute 0.1) 139.3
                                 )
                              , asTest "should have the ingredients' total ecs impact with the complement taken into account"
                                 (case royalPizzaResults |> Result.map (Tuple.second >> .recipe >> .ingredientsTotal >> Impact.getImpact Definition.Ecs) of
@@ -153,7 +153,7 @@ suite =
 
                                     Ok result ->
                                         Unit.impactToFloat result
-                                            |> Expect.within (Expect.Absolute 0.1) 113.3
+                                            |> Expect.within (Expect.Absolute 0.1) 114.4
                                 )
                              , describe "Scoring"
                                 (case royalPizzaResults |> Result.map (Tuple.second >> .scoring) of
@@ -164,13 +164,13 @@ suite =
 
                                     Ok scoring ->
                                         [ Unit.impactToFloat scoring.all
-                                            |> Expect.within (Expect.Absolute 0.01) 460.65
+                                            |> Expect.within (Expect.Absolute 0.01) 463.88
                                             |> asTest "should properly score total impact"
                                         , Unit.impactToFloat scoring.allWithoutComplements
                                             |> Expect.within (Expect.Absolute 0.01) 460.66
                                             |> asTest "should properly score total impact without complements"
                                         , Unit.impactToFloat scoring.complements
-                                            |> Expect.within (Expect.Absolute 0.01) 0.0
+                                            |> Expect.within (Expect.Absolute 0.01) -3.21
                                             |> asTest "should properly score complement impact"
                                         , (Unit.impactToFloat scoring.allWithoutComplements - Unit.impactToFloat scoring.complements)
                                             |> Expect.within (Expect.Absolute 0.0001) (Unit.impactToFloat scoring.all)
