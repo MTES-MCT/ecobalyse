5351c2d2f2789c21d0c383ec860f8d0e268b024f
Vincent Jousse <vincent@jousse.org>
2026-02-11 17:30:46 +0100

    chore: remove legacy version code

--- bin/build-specific-app-version.sh
+++ bin/build-specific-app-version.sh
@@ -16,11 +16,7 @@ fi
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 ROOT_DIR=$( dirname $SCRIPT_DIR )
 ECOBALYSE_GIT_REPO="git@github.com:MTES-MCT/ecobalyse.git"
-ECOBALYSE_DATA_DIR_GIT_REPO="git@github.com:MTES-MCT/ecobalyse-private.git"
-VERSION_DIR=$ROOT_DIR/versions/$COMMIT_OR_TAG
-GIT_CLONE_DIR=$ROOT_DIR/git_build
 PUBLIC_GIT_CLONE_DIR=$ROOT_DIR/git_build/public
-DATA_DIR_GIT_CLONE_DIR=$ROOT_DIR/git_build/private
 
 # Custom error codes
 ERR_INVALID_COMMIT=2
@@ -93,92 +89,19 @@ else
   PUBLIC_GIT_CLONE_DIR=.
 fi
 
+if [ -z "$TRANSCRYPT_KEY" ]; then
 
-
-# Check if detailed impacts are present in the directory, if not an ECOBALYSE_DATA_DIR env variable need to be set
-# and a commit hash for the private repo specified
-# We check at the same time that this not a the new version > 5.0.1 having only one file for the detailed processes
-
-if [[ ! -f "$PUBLIC_GIT_CLONE_DIR/public/data/textile/processes_impacts.json" ]] && [[ ! -f "$PUBLIC_GIT_CLONE_DIR/public/data/processes_impacts.json" ]]; then
-
-  if [ -z "$ECOBALYSE_DATA_DIR_COMMIT" ]; then
-    echo ""
-    echo "🚨 This version of the application requires data files from the Ecobalyse data dir. You need to specify the ECOBALYSE_DATA_DIR_COMMIT as a second parameter."
-    echo "   The corresponding data dir version will be cloned locally."
-    echo ""
-    echo "Usage: $0 <public_commit_hash> <data_dir_commit_hash>"
-    echo ""
-    exit
-  fi
-
-  TEXTILE_DETAILED_IMPACTS_FILE="$DATA_DIR_GIT_CLONE_DIR/data/textile/processes_impacts.json"
-  FOOD_DETAILED_IMPACTS_FILE="$DATA_DIR_GIT_CLONE_DIR/data/textile/processes_impacts.json"
-
-
-  if [ -d $DATA_DIR_GIT_CLONE_DIR ]; then
-    echo "⚠️ Data dir Git destination directory exists, deleting."
-    echo "-> Deleting '$DATA_DIR_GIT_CLONE_DIR'."
-    rm -rf $DATA_DIR_GIT_CLONE_DIR
-  fi
-
-  mkdir -p $DATA_DIR_GIT_CLONE_DIR
-
-  echo "-> Cloning $ECOBALYSE_DATA_DIR_GIT_REPO to $DATA_DIR_GIT_CLONE_DIR"
   echo ""
-
-  git clone $ECOBALYSE_DATA_DIR_GIT_REPO $DATA_DIR_GIT_CLONE_DIR
-
-  cd $DATA_DIR_GIT_CLONE_DIR
-
-  # Use custom error handler to avoid set -eo to be triggred before displaying the error message
-  git checkout $ECOBALYSE_DATA_DIR_COMMIT || error_handler $ERR_INVALID_COMMIT
-
-  cp $DATA_DIR_GIT_CLONE_DIR/data/textile/processes_impacts.json $PUBLIC_GIT_CLONE_DIR/public/data/textile/processes_impacts.json
-  cp $DATA_DIR_GIT_CLONE_DIR/data/food/processes_impacts.json $PUBLIC_GIT_CLONE_DIR/public/data/food/processes_impacts.json
-
-  export ECOBALYSE_DATA_DIR=$DATA_DIR_GIT_CLONE_DIR
-else
-    if [ -z "$TRANSCRYPT_KEY" ]; then
-
-      echo ""
-      echo "🚨 This version of the application requires a TRANSCRYPT_KEY environment var to be set to be able to decrypt the detailed impacts."
-      echo ""
-      exit
-
-    fi
-
-    $ROOT_DIR/bin/run-transcrypt.sh
+  echo "🚨 This version of the application requires a TRANSCRYPT_KEY environment var to be set to be able to decrypt the detailed impacts."
+  echo ""
+  exit
 
 fi
 
-ELM_VERSION_FILE=$PUBLIC_GIT_CLONE_DIR"/src/Request/Version.elm"
-INDEX_JS_FILE=$PUBLIC_GIT_CLONE_DIR"/index.js"
+$ROOT_DIR/bin/run-transcrypt.sh
 
-# We should not patch if we are building the current version
-if [ -z "$BUILD_CURRENT_VERSION" ]; then
-  # Patch old versions of the app so that it gets the version file using relative path in Elm
-  # Otherwise serving the app from /versions will not display the good version number
-  # Also patch the local storage key to avoid messing things up between versions
-  rm -f $PUBLIC_GIT_CLONE_DIR"/pyproject.toml"
-
-  if [ "$COMMIT_OR_TAG" = "v6.0.0" ]; then
-    uv run $ROOT_DIR/bin/patch_files_for_versions_compat.py double-slash $INDEX_JS_FILE $ROOT_DIR/packages/python/ecobalyse/ecobalyse/0002-patch-index-js-client-path-backend.patch $PUBLIC_GIT_CLONE_DIR
-  fi
-  uv run $ROOT_DIR/bin/patch_files_for_versions_compat.py elm-version $ELM_VERSION_FILE
-
-  if printf '%s\n' $COMMIT_OR_TAG 'v5.0.1' | sort -V -C; then
-      # Only patch the local storage key used to log user prior to the v6.0.0, it allows to keep bookmarks and so on,
-      # will being sure that the user is disconnected
-      # We don’t want to do it for the versions starting with v6.0.0 as they are all using the new auth system
-      echo "$COMMIT_OR_TAG is <= to v5.0.1, patching the storage key"
-      uv run $ROOT_DIR/bin/patch_files_for_versions_compat.py local-storage-key $INDEX_JS_FILE $COMMIT_OR_TAG
-  fi
-  uv run $ROOT_DIR/bin/patch_files_for_versions_compat.py version-selector $ROOT_DIR/packages/python/ecobalyse/ecobalyse/0001-feat-patch-homepage-link-and-inject-and-inject-versi.patch $PUBLIC_GIT_CLONE_DIR
-  uv run $ROOT_DIR/bin/patch_files_for_versions_compat.py prerelease $PUBLIC_GIT_CLONE_DIR
-fi
 
 cd $PUBLIC_GIT_CLONE_DIR
-
 
 # Installing node stuff
 # We need to specify dev as the env to avoid errors with needed dev packages at build time like
@@ -193,37 +116,7 @@ export NODE_ENV=production
 npm run build
 npm run server:build
 
-
-# We should not patch if we are building the current version
-if [ -z "$BUILD_CURRENT_VERSION" ]; then
-  # Always put the tag name in the version.json file to help debugging if needed later on
-  # If TAG is defined
-  if [[ ! -z "$TAG" ]]; then
-    uv run $ROOT_DIR/bin/patch_files_for_versions_compat.py add-entry-to-version dist/version.json tag $TAG
-  fi
-
-
-  # If a data dir commit was specified, put it in the version file if needed
-  # it will to keep track of the commit used to build the version
-  if [[ ! -z "$ECOBALYSE_DATA_DIR_COMMIT" ]]; then
-    uv run $ROOT_DIR/bin/patch_files_for_versions_compat.py add-entry-to-version dist/version.json dataDirHash $ECOBALYSE_DATA_DIR_COMMIT
-
-  fi
-
-  # We need to send the referer to python in order to properly redirect after login
-  # so we need to patch the html files that don't have it
-  uv run $ROOT_DIR/bin/patch_files_for_versions_compat.py patch-cross-origin dist/index.html
-fi
-
 cd $ROOT_DIR
-
-# Clean dir for specific commit if it already exists
-if [ -d $VERSION_DIR ]; then
-  rm -rf $VERSION_DIR
-fi
-
-mkdir -p $VERSION_DIR
-
 
 if [[ -f "$PUBLIC_GIT_CLONE_DIR/public/data/processes_impacts.json" ]]; then
   # New version with all the impacts in the same file
@@ -231,21 +124,6 @@ if [[ -f "$PUBLIC_GIT_CLONE_DIR/public/data/processes_impacts.json" ]]; then
 
   # Never ship detailed impacts
   rm -f -- $PUBLIC_GIT_CLONE_DIR/dist/data/processes_impacts.json
-else
-  # Old versions <= 5.0.1 having separate files for the processes
-  npm run encrypt $PUBLIC_GIT_CLONE_DIR/public/data/textile/processes_impacts.json $PUBLIC_GIT_CLONE_DIR/dist/processes_impacts_textile.json.enc
-  npm run encrypt $PUBLIC_GIT_CLONE_DIR/public/data/food/processes_impacts.json $PUBLIC_GIT_CLONE_DIR/dist/processes_impacts_food.json.enc
-
-  # Objects are not present in old versions
-  if [[ -f "$PUBLIC_GIT_CLONE_DIR/public/data/object/processes_impacts.json" ]]; then
-    npm run encrypt $PUBLIC_GIT_CLONE_DIR/public/data/object/processes_impacts.json $PUBLIC_GIT_CLONE_DIR/dist/processes_impacts_object.json.enc
-  fi
-
-  # Never ship detailed impacts
-  rm -f -- $PUBLIC_GIT_CLONE_DIR/dist/data/textile/processes_impacts.json
-  rm -f -- $PUBLIC_GIT_CLONE_DIR/dist/data/food/processes_impacts.json
-  rm -f -- $PUBLIC_GIT_CLONE_DIR/dist/data/object/processes_impacts.json
-
 fi
 
 cp $PUBLIC_GIT_CLONE_DIR/server-app.js $PUBLIC_GIT_CLONE_DIR/dist
@@ -255,12 +133,6 @@ cp $PUBLIC_GIT_CLONE_DIR/openapi.yaml $PUBLIC_GIT_CLONE_DIR/dist
 cd $PUBLIC_GIT_CLONE_DIR
 tar czvf $COMMIT_OR_TAG-dist.tar.gz dist
 
-if [ -z "$BUILD_CURRENT_VERSION" ]; then
-  mv $COMMIT_OR_TAG-dist.tar.gz $ROOT_DIR/
+mv $COMMIT_OR_TAG-dist.tar.gz $ROOT_DIR
 
-  # Move the standalone app into the version directory
-  mv $PUBLIC_GIT_CLONE_DIR/dist/* $VERSION_DIR
-fi
-
-
-echo "✅ $COMMIT_OR_TAG-dist.tar.gz successfully created"
+echo "✅ $ROOT_DIR/$COMMIT_OR_TAG-dist.tar.gz successfully created"
