1728da32f39c4f595c2119d8b01263f4722d15b5
Cedric Rossi <cedric.rossi@beta.gouv.fr>
2026-03-24 14:24:49 +0100

    chore: add scope 'food2' to everything with scope 'food' (#1922)
    ## :wrench: Problem
    
    Fixes https://github.com/MTES-MCT/ecobalyse/issues/1799
    
    See also https://github.com/MTES-MCT/ecobalyse-data/pull/237
    
    ---------
    
    Co-authored-by: Nicolas Perriault <nicolas@perriault.net>

--- src/Page/Explore.elm
+++ src/Page/Explore.elm
@@ -219,6 +219,7 @@ scopesMenuView { enabledSections } model =
         [ label [ class "fw-bold d-none d-sm-block", for "scope-selector" ]
             [ text "Secteur" ]
         , [ ( Scope.Food, enabledSections.food )
+          , ( Scope.Food2, True )
           , ( Scope.Object, enabledSections.objects )
           , ( Scope.Textile, True )
           , ( Scope.Veli, enabledSections.veli )
