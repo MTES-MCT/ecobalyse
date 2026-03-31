a86d60f42f2351e5aa31dd0d6d1a344626a3461b
Cedric Rossi <cedric.rossi@beta.gouv.fr>
2026-03-31 19:36:52 +0200

    chore: fix tests

--- tests/Data/Food/RecipeTest.elm
+++ tests/Data/Food/RecipeTest.elm
@@ -166,13 +166,13 @@ suite =
 
                                     Ok scoring ->
                                         [ Unit.impactToFloat scoring.all
-                                            |> Expect.within (Expect.Absolute 0.01) 463.88
+                                            |> Expect.within (Expect.Absolute 0.01) 463.9
                                             |> asTest "should properly score total impact"
                                         , Unit.impactToFloat scoring.allWithoutComplements
                                             |> Expect.within (Expect.Absolute 0.01) 460.66
                                             |> asTest "should properly score total impact without complements"
                                         , Unit.impactToFloat scoring.complements
-                                            |> Expect.within (Expect.Absolute 0.01) -3.21
+                                            |> Expect.within (Expect.Absolute 0.01) -3.23
                                             |> asTest "should properly score complement impact"
                                         , (Unit.impactToFloat scoring.allWithoutComplements - Unit.impactToFloat scoring.complements)
                                             |> Expect.within (Expect.Absolute 0.0001) (Unit.impactToFloat scoring.all)
