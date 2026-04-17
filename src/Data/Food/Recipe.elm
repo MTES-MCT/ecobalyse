52518b699fc35f6264db23f75222c2ae9b8ad0b0
Vincent Jousse <vincent@jousse.org>
2026-04-17 13:42:27 +0200

    chore: refactor
    Co-authored-by: Nicolas Perriault <nicolas@perriault.net>

--- src/Data/Food/Recipe.elm
+++ src/Data/Food/Recipe.elm
@@ -434,9 +434,8 @@ computeIngredientTransport db { country, ingredient, mass, planeTransport } =
                         -- This corresponds to the stage "3. RECETTE" in the
                         -- [transport documentation](https://fabrique-numerique.gitbook.io/ecobalyse/alimentaire/transport#circuits-consideres)
                         -- https://github.com/MTES-MCT/ecobalyse/issues/1975
-                        Transport.addRoadWithCooling
-                            (Length.kilometers 500)
-                            (ingredient.transportCooling == Ingredient.AlwaysCool || ingredient.transportCooling == Ingredient.CoolOnceTransformed)
+                        Transport.addRoadWithCooling (Length.kilometers 500)
+                            (List.member ingredient.transportCooling [ Ingredient.AlwaysCool, Ingredient.CoolOnceTransformed ])
                             t
 
         transport =
