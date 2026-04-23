0079fc9d83c844c14346a2d2397ef986c019d5ef
Nicolas Perriault <nicolas@perriault.net>
2026-04-23 13:41:07 +0200

    fix(textile): expose picking/kg for product categories

--- src/Page/Explore/TextileProducts.elm
+++ src/Page/Explore/TextileProducts.elm
@@ -10,19 +10,15 @@ import Data.Split as Split
 import Data.Textile.Economics as Economics
 import Data.Textile.Fabric as Fabric
 import Data.Textile.Formula as Formula
-import Data.Textile.LifeCycle as LifeCycle
 import Data.Textile.MakingComplexity as MakingComplexity
 import Data.Textile.Product as Product exposing (Product)
-import Data.Textile.Query as TextileQuery
-import Data.Textile.Simulator as Simulator
-import Data.Textile.Stage.Label as Label
 import Data.Unit as Unit
 import Duration
 import Energy
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import Mass
 import Page.Explore.Table as Table exposing (Table)
-import Quantity
 import Route
 import Views.Format as Format
 import Volume
@@ -34,7 +30,7 @@ withTitle str =
 
 
 table : Session -> { detailed : Bool, scope : Scope } -> Table Product String msg
-table { componentConfig, db } { detailed, scope } =
+table { db } { detailed, scope } =
     { filename = "products"
     , toId = .id >> Product.idToString
     , toRoute = .id >> Just >> Dataset.TextileProducts >> Route.Explore scope
@@ -116,36 +112,14 @@ table { componentConfig, db } { detailed, scope } =
           , toCell = .fabric >> Fabric.toLabel >> text
           }
         , let
-            picking product surfaceMass ys =
-                let
-                    outputMass =
-                        TextileQuery.default
-                            |> TextileQuery.updateProduct product
-                            |> Simulator.compute db componentConfig
-                            |> Result.map (.lifeCycle >> LifeCycle.getStageProp Label.Fabric .outputMass Quantity.zero)
-                            |> Result.withDefault Quantity.zero
-
-                    outputSurface =
-                        Unit.surfaceMassToSurface surfaceMass outputMass
-
-                    threadDensity =
-                        Formula.computeThreadDensity surfaceMass ys
-                in
-                outputSurface
-                    |> Formula.computePicking threadDensity
+            -- Compute picking per kg of end product
+            picking { surfaceMass, yarnSize } =
+                Unit.surfaceMassToSurface surfaceMass Mass.kilogram
+                    |> Formula.computePicking (Formula.computeThreadDensity surfaceMass yarnSize)
           in
-          { label = "Duites.m**"
-          , toValue =
-                Table.FloatValue <|
-                    \({ surfaceMass, yarnSize } as product) ->
-                        picking product surfaceMass yarnSize
-                            |> Unit.pickPerMeterToFloat
-          , toCell =
-                \({ surfaceMass, yarnSize } as product) ->
-                    div [ classList [ ( "text-center", not detailed ) ] ]
-                        [ picking product surfaceMass yarnSize
-                            |> Format.picking
-                        ]
+          { label = "Duites.m pour (1kg)**"
+          , toValue = Table.FloatValue <| picking >> Unit.pickPerMeterToFloat
+          , toCell = picking >> Format.picking
           }
         , { label = "Stocks dormants"
           , toValue = Table.FloatValue (Split.toPercent Env.defaultDeadStock |> always)
