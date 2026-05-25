6b4322d7e6d3163990163714570cf03c4f854954
Nicolas Perriault <nicolas@perriault.net>
2026-05-25 11:10:55 +0200

    feat(food1): publish regulatory food section. (#2312)
    Fixes #2275

--- src/Page/Home.elm
+++ src/Page/Home.elm
@@ -110,7 +110,7 @@ viewHero { enabledSections } =
                 )
               , ( enabledSections.food
                 , { label = "Calculer l’impact de l’alimentation"
-                  , subLabel = Just "Méthodologie en concertation"
+                  , subLabel = Just "Méthodologie validée"
                   , callToAction = False
                   , link = RouteLink Route.FoodBuilderHome
                   , testId = "food-callout-button"
