c8f1282ebb408676f0b54636770a90b8e05c83a6
Nicolas Perriault <nicolas@perriault.net>
2026-06-24 15:15:15 +0200

    refactor: cleaner code

--- src/Views/Spinner.elm
+++ src/Views/Spinner.elm
@@ -12,27 +12,36 @@ view progress =
         ]
         [ div [ class "spinner-border text-primary", attribute "role" "status" ] []
         , div [ class "text-muted" ]
-            [ case ( progress, progress |> Maybe.map (.total >> (\t -> t > 0)) |> Maybe.withDefault False ) of
-                ( Just { loaded, total }, True ) ->
-                    div [ class "d-flex flex-column gap-1" ]
-                        [ text <| "Récupération des données\u{00A0}: " ++ String.fromInt loaded ++ "/" ++ String.fromInt total
-                        , div
-                            [ class "progress w-100"
-                            , attribute "role" "progressbar"
-                            , attribute "aria-label" "Progression du chargement des données"
-                            , attribute "aria-valuenow" (String.fromInt loaded)
-                            , attribute "aria-valuemin" "0"
-                            , attribute "aria-valuemax" (String.fromInt total)
-                            ]
-                            [ div
-                                [ class "progress-bar"
-                                , style "width" (String.fromFloat (toFloat loaded * 100 / toFloat total) ++ "%")
+            [ case progress of
+                Just { loaded, total } ->
+                    if total > 0 then
+                        div [ class "d-flex flex-column gap-1" ]
+                            [ text <| "Récupération des données\u{00A0}: " ++ String.fromInt loaded ++ "/" ++ String.fromInt total
+                            , div
+                                [ class "progress w-100"
+                                , attribute "role" "progressbar"
+                                , attribute "aria-label" "Progression du chargement des données"
+                                , attribute "aria-valuenow" (String.fromInt loaded)
+                                , attribute "aria-valuemin" "0"
+                                , attribute "aria-valuemax" (String.fromInt total)
                                 ]
-                                []
+                                [ div
+                                    [ class "progress-bar"
+                                    , style "width" (String.fromFloat (toFloat loaded * 100 / toFloat total) ++ "%")
+                                    ]
+                                    []
+                                ]
                             ]
-                        ]
 
-                _ ->
-                    text "Chargement…"
+                    else
+                        defaultText
+
+                Nothing ->
+                    defaultText
             ]
         ]
+
+
+defaultText : Html msg
+defaultText =
+    text "Chargement…"
