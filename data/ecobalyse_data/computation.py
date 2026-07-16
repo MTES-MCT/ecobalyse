57e17b56f2cb5217e5d646037cbbdc7c8db8eaf7
paulboosz <paulboosz@gmail.com>
2026-07-16 16:41:16 +0200

    add transported cooled tags to relevant ingredients

--- data/ecobalyse_data/computation.py
+++ data/ecobalyse_data/computation.py
@@ -17,6 +17,7 @@ from common import (
     fix_unit,
     spproject,
 )
+from common.infer_metadata import infer_transported_cooled
 from config import settings
 from ecobalyse_data.bw.search import cached_search_one
 from ecobalyse_data.logging import logger
@@ -438,7 +439,9 @@ def activity_to_process_with_impacts(
             "name", "This process is not linked to a Brightway activity"
         ),
         bw_activity=bw_activity,
-        categories=eco_activity.get("categories", bw_activity.get("categories", [])),
+        categories=infer_transported_cooled(
+            eco_activity.get("categories", bw_activity.get("categories", []))
+        ),
         comment=comment,
         computed_by=computed_by,
         # Default to bw_activity name if no display name is given
