2f3fa0fdde43d6190aa914877882dc1e8f3d514b
Vincent Jousse <vincent@jousse.org>
2026-08-27 10:43:30 +0200

    chore: remove dead code

--- bin/json_formatter.py
+++ bin/json_formatter.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env -S uv run --script
 
 
-import difflib
 import json
 from pathlib import Path
 from typing import Annotated
@@ -10,30 +9,6 @@ import typer
 from data.config import settings
 from ecobalyse.json import CompactJSONEncoder, activities_processes_sort_key
 from ecobalyse.logging import logger
-
-
-# Taken from https://gist.github.com/KaoruNishikawa/55781991af9d50494dd85ac6db0cad89
-def diff(a: str, b: str) -> str:
-    line_color = {"+": 32, "-": 31}
-
-    diffs = difflib.ndiff(a.splitlines(keepends=True), b.splitlines(keepends=True))
-    diff_list = list(diffs)
-    styled: list[str] = []
-    for prev, next in zip(diff_list, diff_list[1:] + [""]):
-        color = line_color.get(prev[0], 0)
-        match prev[0]:
-            case " ":
-                styled.append(prev)
-            case "+" | "-":
-                index = [i for i, c in enumerate(next) if c == "^"]
-                _prev = list(prev)
-                for idx in index:
-                    _prev[idx] = f"\x1b[97;{color + 10};1m{_prev[idx]}\x1b[0;{color}m"
-                styled.append(f"\x1b[{color}m{''.join(_prev)}\x1b[0m")
-            case "?":
-                continue
-    return "".join(styled)
-
 
 EXCLUDED_PATHS: list[str] = [
     "/.git",
