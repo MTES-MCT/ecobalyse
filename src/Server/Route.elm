94d2d3d7060ae2e9fe5a8b4275dd8c3c39d8cf75
Nicolas Perriault <nicolas@perriault.net>
2026-09-08 08:50:10 +0200

    chore: add server routes

--- src/Server/Route.elm
+++ src/Server/Route.elm
@@ -3,17 +3,20 @@ module Server.Route exposing
     , endpoint
     )
 
+import Data.Component as Component
 import Data.Db exposing (Db)
 import Data.Food.Query as FoodQuery
 import Data.Food.Validation as FoodValidation
 import Data.Impact as Impact
 import Data.Impact.Definition as Definition
+import Data.Scope as Scope exposing (GenericScope)
 import Data.Textile.Query as TextileQuery
 import Data.Textile.Validation as TextileValidation
 import Data.Validation as Validation
 import Json.Decode as Decode
 import Json.Encode as Encode
 import Server.Request exposing (Request)
+import Static.Json as StaticJson
 import Url
 import Url.Parser as Parser exposing ((</>), Parser, s)
 
@@ -39,6 +42,20 @@ type Route
       --     Food recipe builder (POST, JSON body)
     | FoodPostRecipe (Result Validation.Errors FoodQuery.Query)
       --
+      -- Generic Routes
+      --   GET
+    | GenericGetAssemblyList GenericScope
+    | GenericGetCategoryList GenericScope
+    | GenericGetComponentList GenericScope
+    | GenericGetConsumptionList GenericScope
+    | GenericGetCountryList GenericScope
+    | GenericGetDistributionList GenericScope
+    | GenericGetMaterialList GenericScope
+    | GenericGetPackagingList GenericScope
+    | GenericGetTransformList GenericScope
+      --   POST
+    | GenericPostSimulator GenericScope (Result Validation.Errors Component.Query)
+      --
       -- Textile Routes
       --   GET
       --     Textile country list
@@ -58,44 +75,35 @@ type Route
     | TextilePostSimulatorSingle (Result Validation.Errors TextileQuery.Query) Definition.Trigram
 
 
-parser : Db -> Encode.Value -> Parser (Route -> a) a
-parser db body =
-    Parser.oneOf
-        [ -- Food
-          (s "GET" </> s "food" </> s "countries")
-            |> Parser.map FoodGetCountryList
-        , (s "GET" </> s "food" </> s "ingredients")
-            |> Parser.map FoodGetIngredientList
-        , (s "GET" </> s "food" </> s "transforms")
-            |> Parser.map FoodGetTransformList
-        , (s "GET" </> s "food" </> s "packagings")
-            |> Parser.map FoodGetPackagingList
-        , (s "POST" </> s "food")
-            |> Parser.map (FoodPostRecipe (decodeFoodQueryBody db body))
-
-        -- Textile
-        , (s "GET" </> s "textile" </> s "countries")
-            |> Parser.map TextileGetCountryList
-        , (s "GET" </> s "textile" </> s "materials")
-            |> Parser.map TextileGetMaterialList
-        , (s "GET" </> s "textile" </> s "products")
-            |> Parser.map TextileGetProductList
-        , (s "GET" </> s "textile" </> s "trims")
-            |> Parser.map TextileGetTrimList
-        , (s "POST" </> s "textile" </> s "simulator")
-            |> Parser.map (TextilePostSimulator (decodeTextileQueryBody db body))
-        , (s "POST" </> s "textile" </> s "simulator" </> s "detailed")
-            |> Parser.map (TextilePostSimulatorDetailed (decodeTextileQueryBody db body))
-        , (s "POST" </> s "textile" </> s "simulator" </> Impact.parseTrigram)
-            |> Parser.map (TextilePostSimulatorSingle (decodeTextileQueryBody db body))
-        ]
-
-
 decodeFoodQueryBody : Db -> Encode.Value -> Result Validation.Errors FoodQuery.Query
 decodeFoodQueryBody db =
     Decode.decodeValue FoodQuery.decode
         >> Result.mapError Validation.fromDecodingError
         >> Result.andThen (FoodValidation.validate db)
+
+
+decodeGenericQueryBody : Db -> GenericScope -> Encode.Value -> Result Validation.Errors Component.Query
+decodeGenericQueryBody db genericScope body =
+    Decode.decodeValue Component.decodeQuery body
+        |> Result.mapError Validation.fromDecodingError
+        |> Result.andThen
+            (\query ->
+                -- FIXME: investigate how we could already have the config here; I'd rather expect
+                -- it to be readily parsed before even decoding route payloads, and to have already
+                -- failed starting the server well before reaching to this point
+                Component.parseConfig db StaticJson.componentConfigJson
+                    |> Result.mapError Validation.fromErrorString
+                    |> Result.andThen
+                        (\config ->
+                            query
+                                |> Component.validateQuery
+                                    { config = config
+                                    , db = db
+                                    , scope = Scope.Generic genericScope
+                                    }
+                                |> Result.mapError Validation.fromErrorString
+                        )
+            )
 
 
 decodeTextileQueryBody : Db -> Encode.Value -> Result Validation.Errors TextileQuery.Query
@@ -113,3 +121,62 @@ endpoint db { body, method, url } =
     -- - We update the path appending the HTTP method to it, for simpler, cheaper route parsing.
     Url.fromString ("http://x/" ++ method ++ url)
         |> Maybe.andThen (Parser.parse (parser db body))
+
+
+genericGet : String -> (GenericScope -> Route) -> Parser (Route -> a) a
+genericGet segment toRoute =
+    Parser.map toRoute
+        (s "GET" </> Scope.parseGeneric </> s segment)
+
+
+parser : Db -> Encode.Value -> Parser (Route -> a) a
+parser db body =
+    Parser.oneOf
+        [ -- Food
+          (s "GET" </> s "food" </> s "countries")
+            |> Parser.map FoodGetCountryList
+        , (s "GET" </> s "food" </> s "ingredients")
+            |> Parser.map FoodGetIngredientList
+        , (s "GET" </> s "food" </> s "transforms")
+            |> Parser.map FoodGetTransformList
+        , (s "GET" </> s "food" </> s "packagings")
+            |> Parser.map FoodGetPackagingList
+        , (s "POST" </> s "food")
+            |> Parser.map (FoodPostRecipe (decodeFoodQueryBody db body))
+
+        -- Generic
+        , genericGet "assemblies" GenericGetAssemblyList
+        , genericGet "categories" GenericGetCategoryList
+        , genericGet "components" GenericGetComponentList
+        , genericGet "consumptions" GenericGetConsumptionList
+        , genericGet "countries" GenericGetCountryList
+        , genericGet "distributions" GenericGetDistributionList
+        , genericGet "materials" GenericGetMaterialList
+        , genericGet "packagings" GenericGetPackagingList
+        , genericGet "transforms" GenericGetTransformList
+        , (s "POST" </> Scope.parseGeneric </> s "simulator")
+            |> Parser.map
+                (\genericScope ->
+                    body
+                        -- FIXME: as commented in decodeGenericQueryBody, investigate passing
+                        -- the config here; maybe reuse the `Requirements db config` pattern
+                        |> decodeGenericQueryBody db genericScope
+                        |> GenericPostSimulator genericScope
+                )
+
+        -- Textile
+        , (s "GET" </> s "textile" </> s "countries")
+            |> Parser.map TextileGetCountryList
+        , (s "GET" </> s "textile" </> s "materials")
+            |> Parser.map TextileGetMaterialList
+        , (s "GET" </> s "textile" </> s "products")
+            |> Parser.map TextileGetProductList
+        , (s "GET" </> s "textile" </> s "trims")
+            |> Parser.map TextileGetTrimList
+        , (s "POST" </> s "textile" </> s "simulator")
+            |> Parser.map (TextilePostSimulator (decodeTextileQueryBody db body))
+        , (s "POST" </> s "textile" </> s "simulator" </> s "detailed")
+            |> Parser.map (TextilePostSimulatorDetailed (decodeTextileQueryBody db body))
+        , (s "POST" </> s "textile" </> s "simulator" </> Impact.parseTrigram)
+            |> Parser.map (TextilePostSimulatorSingle (decodeTextileQueryBody db body))
+        ]
