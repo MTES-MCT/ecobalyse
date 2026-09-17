b703a88f8eac36d937af2a91db0dc4180446a12d
nicolasp <nicolas.p@kandeel.co>
2026-09-17 16:18:36 +0200

    update battery material type and recycling processes

--- src/Data/Process/Category.elm
+++ src/Data/Process/Category.elm
@@ -37,7 +37,10 @@ type Category
 
 type Material
     = Aluminium
-    | BatteryCell
+    | BatteryCellNMC811
+    | BatteryCellNMC622
+    | BatteryCellLFP
+    | BatteryAlkaline
     | Cereals
     | Composites
     | Containerboard
@@ -170,8 +173,17 @@ materialTypeFromString string =
         "aluminium" ->
             Ok Aluminium
 
-        "battery_cell" ->
-            Ok BatteryCell
+        "battery_cell_nmc811" ->
+            Ok BatteryCellNMC811
+
+        "battery_cell_nmc622" ->
+            Ok BatteryCellNMC622
+
+        "battery_cell_lfp" ->
+            Ok BatteryCellLFP
+
+        "battery_alkaline" ->
+            Ok BatteryAlkaline
 
         "composites" ->
             Ok Composites
@@ -261,8 +273,17 @@ materialTypeToLabel material =
         Aluminium ->
             "Aluminium"
 
-        BatteryCell ->
-            "Cellule de batteries"
+        BatteryCellNMC811 ->
+            "Cellule de batteries NMC811"
+
+        BatteryCellNMC622 ->
+            "Cellule de batteries NMC622"
+
+        BatteryCellLFP ->
+            "Cellule de batteries LFP"
+
+        BatteryAlkaline ->
+            "Pile Alcaline"
 
         Cereals ->
             "Cereals"
@@ -349,8 +370,17 @@ materialTypeToString material =
         Aluminium ->
             "aluminium"
 
-        BatteryCell ->
-            "battery_cell"
+        BatteryCellNMC811 ->
+            "battery_cell_nmc811"
+
+        BatteryCellNMC622 ->
+            "battery_cell_nmc622"
+
+        BatteryCellLFP ->
+            "battery_cell_lfp"
+
+        BatteryAlkaline ->
+            "battery_alkaline"
 
         Cereals ->
             "cereals"
